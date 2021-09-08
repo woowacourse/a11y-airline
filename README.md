66f38eb10da072572806be3ddf2c3e65e446d9d0
임동준 <im1@woowahan.com>
2021-09-08 10:16:24 +0900

    feat: init a11y-airline project

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# a11y-airline
+<p align="middle" >
+  <img width="100px;" src="./src/images/airplane.png"/>
+</p>
+<h2 align="middle">누구나 접근할 수 있는 항공사 웹사이트</h2>
+<p align="middle">항공사 웹사이트 컴포넌트의 스크린 리더 접근성 높이기</p>
+<br/>
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import SpinButton from "./components/SpinButton";
+
+function App() {
+  return (
+    <div className="App">
+      <SpinButton />
+    </div>
+  );
+}
+
+export default App;
