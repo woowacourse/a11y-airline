db265824d7562b47593a9fb11f30baf3c44f2534
imgwang-yeol <mkitigy@gmail.com>
2021-09-28 19:44:26 +0900

    feat: 스테퍼에 screen reader 적용

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,11 +5,9 @@ import ROUTE from '../constants/route';
 const Home = () => (
   <section>
     <h1>누구나 접근할 수 있는 항공사 웹사이트</h1>
-    <ul>
-      <li>
-        <Link to={ROUTE.MISSION1.PATH}>Mission 1</Link>
-      </li>
-    </ul>
+    <nav>
+      <Link to={ROUTE.MISSION1.PATH}>Mission 1</Link>
+    </nav>
   </section>
 );
 
--- src/pages/Mission1.js
+++ src/pages/Mission1.js
@@ -1,45 +1,58 @@
 import React, { useState } from 'react';
 
+const PASSENGER_MIN_COUNT = 1;
 const PASSENGER_MAX_COUNT = 3;
+const initialPassengerCount = {
+  prev: PASSENGER_MIN_COUNT,
+  current: PASSENGER_MIN_COUNT,
+};
 
 const Mission1 = () => {
-  const [passengerCount, setPassengerCount] = useState(0);
+  const [passengerCount, setPassengerCount] = useState(initialPassengerCount);
+  const { prev, current } = passengerCount;
 
   const decrementPassengerCount = () => {
-    if (passengerCount === 0) return;
+    if (current <= PASSENGER_MIN_COUNT) return;
 
-    setPassengerCount(passengerCount - 1);
+    setPassengerCount({ prev: current, current: current - 1 });
   };
 
   const incrementPassengerCount = () => {
-    if (passengerCount === PASSENGER_MAX_COUNT) return;
+    if (current >= PASSENGER_MAX_COUNT) return;
 
-    setPassengerCount(passengerCount + 1);
+    setPassengerCount({ prev: current, current: current + 1 });
   };
 
   const submitForm = (event) => {
     event.preventDefault();
 
-    if (passengerCount === 0) {
-      alert('추가할 탑승객이 없습니다.');
-
-      return;
-    }
-
-    if (window.confirm('탑승객을 추가하시겠습니까?')) {
+    if (window.confirm('탑승자를 추가하시겠습니까?')) {
       alert('추가 되었습니다.');
-      setPassengerCount(0);
+      setPassengerCount(initialPassengerCount);
     }
   };
 
   return (
     <section>
       <form onSubmit={submitForm}>
-        <button type="button" onClick={decrementPassengerCount}>
+        <button
+          type="button"
+          aria-label="성인 탑승자 한명 줄이기"
+          onClick={decrementPassengerCount}
+        >
           -
         </button>
-        <input value={passengerCount} readOnly />
-        <button type="button" onClick={incrementPassengerCount}>
+        <label htmlFor="passengerCount">성인</label>
+        <input type="tel" id="passengerCount" value={current} readOnly={true} />
+        <div aria-live="assertive" aria-atomic={true}>
+          {prev > current && <p>성인 승객 감소 {current}</p>}
+          {prev < current && <p>성인 승객 증가 {current}</p>}
+        </div>
+        <button
+          type="button"
+          aria-label="성인 탑승자 한명 늘리기"
+          onClick={incrementPassengerCount}
+        >
           +
         </button>
         <button>추가</button>
