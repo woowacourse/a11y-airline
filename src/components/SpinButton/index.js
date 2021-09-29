57c321d56609b90a173dd7ce705b67899f8c7335
Min Kyung Kang <mnk918p@gmail.com>
2021-09-29 23:38:37 +0900

    feat: 접근성을 위한 form, input id 추가

--- src/components/SpinButton/index.js
+++ src/components/SpinButton/index.js
@@ -25,53 +25,53 @@ const SpinButton = () => {
     <section>
       <h2>승객 선택</h2>
 
-      <h3>
-        성인
-        <span role='tooltip' className='tooltip'>
-          ?
-        </span>
-      </h3>
-      <button
-        type='button'
-        onClick={decreaseCount}
-        aria-live='assertive'
-        aria-label='성인 탑승자 한 명 줄이기 버튼'
-        className='decrease default-btn'
-        disabled={passenger <= 0 ? true : false}
-      >
-        -
-      </button>
+      <h3>성인</h3>
+      <form>
+        <button
+          type='button'
+          onClick={decreaseCount}
+          aria-live='assertive'
+          aria-label='성인 탑승자 한 명 줄이기 버튼'
+          className='decrease default-btn'
+          disabled={passenger <= 0 ? true : false}
+        >
+          -
+        </button>
 
-      <label
-        id='passenger-count-input'
-        htmlFor='passenger-count-input'
-        className='passenger-count-label'
-        aria-live='assertive'
-        aria-label={`성인 ${passenger} 텍스트 숫자만 수정`}
-      ></label>
+        <label
+          className='hidden'
+          id='passenger-count-input'
+          htmlFor='passenger-count-input-label'
+          className='passenger-count-label'
+          aria-live='assertive'
+          aria-label={`성인 ${passenger} 텍스트 숫자만 수정`}
+        >
+          승객 수
+        </label>
 
-      <input
-        className='passenger-count-input'
-        name='passenger-count-input'
-        type='number'
-        min='0'
-        max='3'
-        value={passenger}
-        aria-labelledby='passenger-count-input'
-        readOnly
-      />
+        <input
+          className='passenger-count-input'
+          name='passenger-count-input'
+          id='passenger-count-input-label'
+          type='number'
+          min='0'
+          max='3'
+          value={passenger}
+          aria-labelledby='passenger-count-input'
+          readOnly
+        />
 
-      <button
-        type='button'
-        onClick={increaseCount}
-        aria-live='assertive'
-        aria-label='성인 탑승자 한 명 늘리기 버튼'
-        className='increase default-btn'
-        disabled={passenger >= 3 ? true : false}
-      >
-        +
-      </button>
-
+        <button
+          type='button'
+          onClick={increaseCount}
+          aria-live='assertive'
+          aria-label='성인 탑승자 한 명 늘리기 버튼'
+          className='increase default-btn'
+          disabled={passenger >= 3 ? true : false}
+        >
+          +
+        </button>
+      </form>
       <span className='hidden' aria-live='assertive'>{`성인 승객 추가 ${passenger}명`}</span>
     </section>
   );
