ecda8c51f9c306e63e13ea4ea6d38b92b84a8c65
권세진 <0307kwon@gmail.com>
2021-09-29 20:40:14 +0900

    feat: 승객 수 입력기능 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { useState } from "react";
 
+import "./App.css";
+
 function App() {
   const [count, setCount] = useState(1);
 
   const handleChangeCount = (event) => {
     const input = event.target.valueAsNumber;
 
-    if(input < 1 || input > 3) {
+    if (input < 1 || input > 3) {
       return;
     }
 
@@ -14,15 +16,21 @@ function App() {
   };
 
   const handleMinusCount = () => {
-    if (count > 1) {
-      setCount(count - 1);
+    if (count <= 1) {
+      alert(`승객 수는 ${1}명 이상이어야 합니다.`);
+      return;
     }
+
+    setCount(count - 1);
   };
 
   const handlePlusCount = () => {
-    if (count < 3) {
-      setCount(count + 1);
+    if (count >= 3) {
+      alert(`승객 수는 ${3}명 이하여야 합니다.`);
+      return;
     }
+
+    setCount(count + 1);
   };
 
   return (
@@ -30,15 +38,31 @@ function App() {
       <h1>접근성 높이기 미션</h1>
       <section>
         <h2>승객 수 입력하기</h2>
-        <button onClick={handleMinusCount}>-</button>
+        <button
+          type="button"
+          onClick={handleMinusCount}
+          aria-label="성인 탑승자 한명 줄이기"
+        >
+          -
+        </button>
         <input
           type="number"
           min="1"
           max="3"
           value={count}
           onChange={handleChangeCount}
+          aria-describedby="passenger-count"
         />
-        <button onClick={handlePlusCount}>+</button>
+        <button
+          type="button"
+          onClick={handlePlusCount}
+          aria-label="성인 탑승자 한명 늘리기"
+        >
+          +
+        </button>
+        <span id="passenger-count" role="status" className="hidden">
+          성인 {count}명
+        </span>
       </section>
     </>
   );
