d3794e0429353966e71364b81fec50938cd57d9d
Shim MunSeong <Puterism.K@gmail.com>
2021-09-30 01:34:31 +0900

    feat: 메인 페이지에 `SpinButton` 컴포넌트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { useState } from 'react';
+import SpinButton from './components/SpinButton/SpinButton';
 import './App.css';
 
 const App = () => {
-  return <div className="App"></div>;
+  const valueState = useState<string | number>(1);
+
+  return (
+    <div className="App">
+      <h1>승객 선택</h1>
+      <SpinButton
+        label="성인"
+        valueState={valueState}
+        min={0}
+        max={3}
+        increaseButtonLabel="성인 탑승자 한 명 늘리기"
+        decreaseButtonLabel="성인 탑승자 한 명 줄이기"
+        increaseMessage="성인 승객 추가"
+        decreaseMessage="성인 승객 감소"
+      />
+    </div>
+  );
 };
 
 export default App;
