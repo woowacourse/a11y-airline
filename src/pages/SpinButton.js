dce6065ecf59f532bab89e4b164db0c9ae5527e7
jum0 <wnsah052@naver.com>
2021-09-30 01:13:29 +0900

    feat: SpinButton 구현

--- src/pages/SpinButton.js
+++ src/pages/SpinButton.js
@@ -1,3 +1,75 @@
-const SpinButton = () => <div>spin button page</div>;
+import styled from "@emotion/styled";
+import { useState } from "react";
+
+const H1 = styled.span`
+  font-size: 2rem;
+  font-weight: 700;
+  border-bottom: 1px solid grey;
+`;
+
+const SpinButtonSection = styled.section`
+  display: flex;
+`;
+
+const Input = styled.input`
+  all: unset;
+  border-bottom: 1px solid black;
+  font-weight: 600;
+  text-align: center;
+  margin: 0 1rem;
+  padding-left: 0.8rem;
+  width: 2rem;
+`;
+
+const CurrentPassenger = styled.span`
+  width: 0;
+  height: 0;
+  opacity: 0;
+`;
+
+const SpinButton = () => {
+  const [passengerCount, setPassengerCount] = useState(1);
+
+  const handleIncreaseCount = () => {
+    if (passengerCount < 3) {
+      setPassengerCount(passengerCount + 1);
+    }
+  };
+
+  const handleDecreaseCount = () => {
+    if (passengerCount > 1) setPassengerCount(passengerCount - 1);
+  };
+
+  return (
+    <>
+      <H1>🎯 미션1 Spin Button: 승객수 입력하기</H1>
+      <h2>승객 선택</h2>
+      <h4>성인</h4>
+      <SpinButtonSection>
+        <button
+          aria-label="성인 탑승자 한 명 줄이기"
+          onClick={handleDecreaseCount}
+        >
+          -
+        </button>
+        <Input
+          type="number"
+          aria-label="성인"
+          value={passengerCount}
+          readOnly
+        />
+        <CurrentPassenger role="status">
+          현재 승객 인원 {passengerCount}
+        </CurrentPassenger>
+        <button
+          aria-label="성인 탑승자 한명 늘리기"
+          onClick={handleIncreaseCount}
+        >
+          +
+        </button>
+      </SpinButtonSection>
+    </>
+  );
+};
 
 export default SpinButton;
