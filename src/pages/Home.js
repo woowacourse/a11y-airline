2b8bf7466b5d9de366c35242e1dde71e02a348c3
jum0 <wnsah052@naver.com>
2021-09-30 11:10:26 +0900

    fix: header 태그 수정 및 Link 태그 변경

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import PATH from "../constants/path";
 
 const Home = () => (
   <>
-    <h2>🚀 항공사 웹사이트의 컴포넌트 접근성 높이기</h2>
-    <Link to={PATH.SPIN_BUTTON}>
-      <button> 요구 사항 1 - Spin Button: 승객수 입력하기</button>
-    </Link>
+    <h1>🚀 항공사 웹사이트의 컴포넌트 접근성 높이기</h1>
+    <NavLink to={PATH.SPIN_BUTTON}>
+      요구 사항 1 - Spin Button: 승객수 입력하기
+    </NavLink>
   </>
 );
 
