8e0d7db15d8bddccb311d0ee0afb4ca73fac5e89
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-10-02 14:34:22 +0900

    fix: ssr 오류 해결 및 lang 속성 적용

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,14 @@ import '../styles/global.css';
 
 const App = ({ Component, pageProps }) => {
   return (
-    <html lang="ko">
+    <>
       <Head>
         <title>접근성 미션</title>
         <meta name="description" content="웹 접근성 미션" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
-    </html>
+    </>
   );
 };
 
