86149af805de1f32f8743413121f4180b8592477
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-11-25 13:57:49 +0900

    feat: Carousel, Nabigation 컴포넌트 생성

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Carousel = () => {
-  return <div>캐러셀 페이지입니다.</div>;
+  useEffect(() => {
+    document.title = "웹 접근성 미션 | 캐러셀";
+  }, []);
+
+  return <div>캐러셀 영역입니다.</div>;
 };
 
 export default Carousel;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Navigation = () => {
+  useEffect(() => {
+    document.title = "웹 접근성 미션 | 네비게이션";
+  }, []);
+
   return <div>네비게이션 영역입니다.</div>;
 };
 
