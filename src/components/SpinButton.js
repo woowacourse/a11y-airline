2dcbcd9477adae4391819e07c2d0577d69dbcb8e
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-11-25 13:57:00 +0900

    refactor: 스핀버튼 웹 스트린리더 UX 향상

--- src/components/SpinButton.js
+++ src/components/SpinButton.js
@@ -1,8 +1,13 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import styled from "@emotion/styled";
+
+const PASSENGER_MAX_COUNT = 3;
+const PASSENGER_MIN_COUNT = 1;
 
 const SpinButton = () => {
-  const PASSENGER_MAX_COUNT = 3;
-  const PASSENGER_MIN_COUNT = 1;
+  useEffect(() => {
+    document.title = "웹 접근성 미션 | 스핀버튼";
+  }, []);
 
   const [adultPassengerCount, setAdultPassengerCount] = useState(
     PASSENGER_MIN_COUNT,
@@ -20,70 +25,100 @@ const SpinButton = () => {
     setAdultPassengerCount((count) => count + 1);
   };
 
+  const onUpdateAdultPassengerCount = (event) => {
+    setAdultPassengerCount(event.target.value);
+  };
+
   return (
-    <section>
-      <h1>승객 선택</h1>
+    <Section>
+      <h2>승객 선택</h2>
 
-      <h2>성인</h2>
-      <label>
-        <input
-          type="button"
-          aria-label="성인 탑승자 한명 줄이기"
-          value="-"
-          disabled={adultPassengerCount < PASSENGER_MIN_COUNT}
+      <label htmlFor="adultPassenger">성인</label>
+      <div aria-label="성인 승객 수">
+        <Button
           onClick={onDecreaseCount}
-          style={{
-            fontSize: "2rem",
-            padding: "0 1rem",
-            margin: "0.5rem 0",
-            boxSizing: "border-box",
-          }}
+          aria-label="성인 탑승자 한명 줄이기"
+          disabled={adultPassengerCount < PASSENGER_MIN_COUNT}
+        >
+          -
+        </Button>
+        <Input
+          id="adultPassenger"
+          type="number"
+          min={PASSENGER_MIN_COUNT - 1}
+          max={PASSENGER_MAX_COUNT}
+          value={adultPassengerCount}
+          onChange={onUpdateAdultPassengerCount}
         />
-
-        <span aria-atomic="true" aria-live="assertive">
-          <span
-            style={{
-              display: "inline-block",
-              width: "1px",
-              height: "1px",
-              margin: "-1px 0 0",
-              overflow: "hidden",
-            }}
-          >
-            성인 승객 {adultPassengerCount} 명
-          </span>
-          <input
-            style={{
-              fontSize: "2rem",
-              padding: "0 1rem",
-              margin: "0.5rem 0",
-              boxSizing: "border-box",
-            }}
-            value={adultPassengerCount}
-            onChange={({ target: { value } }) => setAdultPassengerCount(value)}
-            type="number"
-            min={PASSENGER_MIN_COUNT}
-            max={PASSENGER_MAX_COUNT}
-            readOnly
-          />
-        </span>
-
-        <input
-          type="button"
-          aria-label="성인 탑승자 한명 늘리기"
-          value="+"
-          disabled={adultPassengerCount >= PASSENGER_MAX_COUNT}
+        <VisibilityHidden role="alert" aria-atomic="true" aria-live="polite">
+          성인 {adultPassengerCount}명
+        </VisibilityHidden>
+        <Button
           onClick={onIncreaseCount}
-          style={{
-            fontSize: "2rem",
-            padding: "0 1rem",
-            margin: "0.5rem 0",
-            boxSizing: "border-box",
-          }}
-        />
-      </label>
-    </section>
+          aria-label="성인 탑승자 한 명 늘리기"
+          disabled={adultPassengerCount >= PASSENGER_MAX_COUNT}
+        >
+          +
+        </Button>
+      </div>
+    </Section>
   );
 };
 
 export default SpinButton;
+
+const Section = styled.section`
+  width: fit-content;
+  padding: 2rem;
+  margin: 1rem auto;
+  border: 1px solid #333333;
+  border-radius: 0.5rem;
+
+  h2 {
+    margin-bottom: 1rem;
+  }
+
+  > div {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const Button = styled.button`
+  width: 2.5rem;
+  height: 2.5rem;
+  margin: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-size: 2rem;
+
+  border-radius: 50%;
+  border: 1px solid #333333;
+
+  :hover {
+    font-weight: 600;
+    border: 2px solid #333333;
+  }
+`;
+
+const Input = styled.input`
+  width: 5rem;
+  height: 3rem;
+
+  font-size: 2rem;
+  text-align: center;
+
+  border: none;
+  border-bottom: 2px solid #333333;
+`;
+
+const VisibilityHidden = styled.p`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  overflow: hidden;
+  clip: react(1px, 1px, 1px, 1px);
+  white-space: nowrap;
+`;
