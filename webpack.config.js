545d15c4962b6e53188d3db97a7241c58268fa84
Yo Wook Kim <uk960214@gmail.com>
2022-09-30 15:26:18 +0900

    chore: 웹팩과 기본 프로젝트 설정

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "main.[fullhash].js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".ts"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./index.html"),
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    open: true,
+    historyApiFallback: true,
+    compress: true,
+    port: 3000,
+  },
+};
