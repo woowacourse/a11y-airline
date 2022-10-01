fecda5f0e1b744ee757c0e393056150886457441
jswith <typingjs@gmail.com>
2022-10-01 16:59:22 +0900

    refactor: 카운터 range 1부터 3까지로 변경

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -2,25 +2,25 @@ const passengerCountInput = document.querySelector(".passenger__count");
 const passengerAddButton = document.querySelector(".-sub");
 const passengerSubButton = document.querySelector(".-add");
 
-const handleInputNumberLimitFromZeroToThree = (e) => {
-  e.target.value = e.target.value.replace(/[^0-3]/g, "0");
+const handleInputNumberLimitFromOneToThree = (e) => {
+  e.target.value = e.target.value.replace(/[^1-3]/g, "1");
 };
 
 const subtractPassengerCount = () => {
-  if (passengerCountInput.value >= 1) {
+  if (passengerCountInput.value > 1) {
     passengerCountInput.value--;
   }
 };
 
 const addPassengerCount = () => {
-  if (passengerCountInput.value <= 2) {
+  if (passengerCountInput.value < 3) {
     passengerCountInput.value++;
   }
 };
 
 passengerCountInput.addEventListener(
   "input",
-  handleInputNumberLimitFromZeroToThree
+  handleInputNumberLimitFromOneToThree
 );
 passengerAddButton.addEventListener("click", subtractPassengerCount);
 passengerSubButton.addEventListener("click", addPassengerCount);
