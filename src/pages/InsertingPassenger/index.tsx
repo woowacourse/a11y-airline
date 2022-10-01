0aed716f654917b1aedb14cb9c5ed9bd39a5aee4
JASUN LEE <liswktjs@gmail.com>
2022-10-01 15:58:29 +0900

    feat: 0명 미만의 숫자나, 3명 초과하는 숫자를 선택하지 못하도록 한다

--- src/pages/InsertingPassenger/index.tsx
+++ src/pages/InsertingPassenger/index.tsx
@@ -1,9 +1,30 @@
+import { useState } from 'react';
+
 import { Button } from '@/common/Button/Button.styles';
-import ChipButton from '@/common/ChipButton/ChipButton';
 import { Input } from '@/common/Input/Input.styles';
+import ChipButton from '@/common/ChipButton/ChipButton';
+
 import * as S from '@/pages/InsertingPassenger/index.styles';
 
 const InsertingPassenger = () => {
+	const [peopleCount, setPeopleCount] = useState<number>(0);
+
+	const onMinusButtonClick = () => {
+		if (peopleCount === 0) {
+			window.alert('0명 미만의 인원은 선택하실 수 없습니다');
+			return;
+		}
+		setPeopleCount(peopleCount - 1);
+	};
+
+	const onPlusButtonClick = () => {
+		if (peopleCount === 3) {
+			window.alert('3명을 초과하는 인원은 선택하실 수 없습니다');
+			return;
+		}
+		setPeopleCount(peopleCount + 1);
+	};
+
 	return (
 		<S.Container>
 			<S.Title>승객 선택</S.Title>
@@ -13,9 +34,13 @@ const InsertingPassenger = () => {
 					<ChipButton />
 				</S.SubInfo>
 				<S.InputContainer>
-					<Button aria-label="성인 탑승자 한명 줄이기 버튼">-</Button>
-					<Input type="number" min={0} max={3} />
-					<Button aria-label="성인 탑승자 한명 늘리기 버튼">+</Button>
+					<Button aria-label="성인 탑승자 한명 줄이기 버튼" onClick={onMinusButtonClick}>
+						-
+					</Button>
+					<Input type="number" min={0} max={3} value={peopleCount} />
+					<Button aria-label="성인 탑승자 한명 늘리기 버튼" onClick={onPlusButtonClick}>
+						+
+					</Button>
 				</S.InputContainer>
 			</S.ContentContainer>
 		</S.Container>
