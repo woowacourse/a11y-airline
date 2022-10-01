eff84888164c7ac49721b29c2dd37efb96e3cf66
kimsanglok <evertree6031@gmail.com>
2022-10-01 18:16:51 +0900

    feat: entry 포인트 파일(index.tsx) 추가

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+const container = document.getElementById('root');
+const root = createRoot(container as Element);
+
+root.render(<App />);
+
+export default App;
