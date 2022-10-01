3d059d414bbc50fadcdcc277ee830d05d6368aab
kimsanglok <evertree6031@gmail.com>
2022-10-01 18:28:52 +0900

    refactor: 매직넘버 상수화 적용

--- src/pages/FirstMissionPage.tsx
+++ src/pages/FirstMissionPage.tsx
@@ -3,6 +3,24 @@ import FlexContainer from '../components/FlexContainer/FlexContainer';
 
 import { useMessageState } from '../hooks/useMessageState';
 
+const ADULT = {
+  label: '성인',
+  min: 0,
+  max: 3,
+} as const;
+
+const INFANT = {
+  label: '소아',
+  min: 0,
+  max: 3,
+} as const;
+
+const CHILD = {
+  label: '유아',
+  min: 0,
+  max: 10,
+} as const;
+
 const FirstMissionPage = () => {
   const { message } = useMessageState();
 
@@ -10,9 +28,9 @@ const FirstMissionPage = () => {
     <div>
       <h1>승객 선택</h1>
       <FlexContainer flexDirection="row" alignItems="center" gap="large">
-        <PassengerCounter label="성인" min={0} max={3} />
-        <PassengerCounter label="소아" min={0} max={3} />
-        <PassengerCounter label="유아" min={0} max={3} />
+        <PassengerCounter label={ADULT.label} min={ADULT.min} max={ADULT.max} />
+        <PassengerCounter label={INFANT.label} min={INFANT.min} max={INFANT.max} />
+        <PassengerCounter label={CHILD.label} min={CHILD.min} max={CHILD.max} />
         <div aria-live="polite">{message}</div>
       </FlexContainer>
     </div>
