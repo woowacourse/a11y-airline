b13a9f776adf04a538a8aed4046950547597e2af
JASUN LEE <liswktjs@gmail.com>
2022-10-01 14:25:47 +0900

    feat: production 모드 추가 및 불필요한 cleanwebpack 삭제

--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,55 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const {
-  CleanWebpackPlugin,
-} = require("clean-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.tsx",
-  output: {
-    path: path.join(
-      __dirname,
-      "../dist"
-    ),
-    filename: "[name].bundle.js",
-    publicPath: "/",
-    clean: true,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(
-        __dirname,
-        "/src"
-      ),
-    },
-    modules: ["node_modules"],
-    extensions: [".js", ".ts", ".tsx"],
-  },
-  devServer: {
-    historyApiFallback: true,
-    port: 3000,
-    hot: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|ts|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-        generator: {
-          filename:
-            "[name].[contenthash].js",
-        },
-      },
-      {
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          "css-loader",
-        ],
-        generator: {
-          filename:
-            "[name].[contenthash].css",
-        },
-      },
-      {
-        test: /\.(png)$/,
-        exclude: /node_modules/,
-        type: "asset/resource",
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-  ],
-  optimization: {
-    minimize: true,
-  },
+	mode: 'production',
+	entry: './src/index.tsx',
+	output: {
+		path: path.join(__dirname, '../dist'),
+		filename: '[name].bundle.js',
+		publicPath: '/',
+		clean: true,
+	},
+	devtool: 'source-map',
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, '/src'),
+		},
+		modules: ['node_modules'],
+		extensions: ['.js', '.ts', '.tsx'],
+	},
+	devServer: {
+		historyApiFallback: true,
+		port: 3000,
+		hot: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx|ts|tsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+				generator: {
+					filename: '[name].[contenthash].js',
+				},
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+				generator: {
+					filename: '[name].[contenthash].css',
+				},
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+		}),
+	],
+	optimization: {
+		minimize: true,
+	},
 };
