4d52273df2e0431c52d10399e9e0b09904f7e337
JASUN LEE <liswktjs@gmail.com>
2022-10-01 15:52:58 +0900

    style: Input 스타일 지정

---
+++ src/pages/InsertingPassenger/index.styles.tsx
@@ -0,0 +1,31 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.main`
+	display: flex;
+	flex-direction: column;
+
+	width: 100vw;
+	height: 100vh;
+	justify-content: center;
+	align-items: center;
+`;
+
+export const Title = styled.h2``;
+
+export const ContentContainer = styled.section``;
+
+export const SubInfo = styled.div`
+	display: flex;
+	gap: 15px;
+
+	align-items: center;
+	justify-content: center;
+`;
+
+export const PeopleType = styled.h3``;
+
+export const InputContainer = styled.div`
+	display: flex;
+	gap: 15px;
+	margin-top: 50px;
+`;
