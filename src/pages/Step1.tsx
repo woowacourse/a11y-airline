f3a705032e2174396336265adbafbda4f9cb1f25
moonheekim0118 <hellomooneekim@gmail.com>
2022-10-03 03:12:37 +0900

    feat: Step1 페이지에 스타일링을 적용한다.

--- src/pages/Step1.tsx
+++ src/pages/Step1.tsx
@@ -1,10 +1,12 @@
 import PassengerCounter from "@components/PassengerCounter";
 const Step1 = () => {
   return (
-    <div>
-      <h1>탑승 인원을 정해주세요.</h1>
+    <article className="flex flex-col gap-10 mt-2.5 justify-center items-center">
+      <h1 className="text-xl font-extrabold">연령별 탑승 인원 선택</h1>
       <PassengerCounter ageGroup="성인" />
-    </div>
+      <PassengerCounter ageGroup="청소년" />
+      <PassengerCounter ageGroup="영유아" />
+    </article>
   );
 };
 
