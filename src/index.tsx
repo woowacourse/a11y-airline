1f32443584d29c8a8d0bb321dec1fb4f497861e0
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-10-02 16:30:08 +0900

    chore: 개발 환경(react, typescript) 설정

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+
+  root.render(<App />);
+} else {
+  console.error("no root Element");
+}
