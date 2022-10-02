9133c789edb8e5cea35113e6edf3980a5362e53b
moonheekim0118 <hellomooneekim@gmail.com>
2022-10-03 04:39:53 +0900

    refactor: useNavigate -> Link로 수정한다.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,20 +1,12 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-  const navigate = useNavigate();
-
-  const handlePushToPreviousPage = () => {
-    navigate("/");
-  };
-
   return (
     <article className="flex flex-col gap-10 mt-2.5 justify-center items-center">
       <h1 className="text-xl font-extrabold">
         죄송합니다. 존재하지 않는 페이지 입니다.
       </h1>
-      <button type="button" onClick={handlePushToPreviousPage}>
-        메인 화면으로 가기
-      </button>
+      <Link to="/">메인 화면으로 가기</Link>
     </article>
   );
 };
