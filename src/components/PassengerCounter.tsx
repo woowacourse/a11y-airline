00cc1a3179b9b1691d74d5b561d71660b82d2c3a
moonheekim0118 <hellomooneekim@gmail.com>
2022-10-02 23:39:23 +0900

    feat: 최소,최대 승객을 초과 할 경우 버튼을 disalbe 되게 하지 않고, alert 메시지를 띄우도록 수정한다.

--- src/components/PassengerCounter.tsx
+++ src/components/PassengerCounter.tsx
@@ -1,16 +1,23 @@
-import { useState } from "react";
+import { useState, ChangeEvent } from "react";
 
 interface Props {
   ageGroup: "성인" | "청소년" | "유아";
 }
 
 const DEFAULT_COUNT = 1;
+const MINIMUM_COUNT = 1;
+const MAXIMUM_COUNT = 3;
+const COUNT_RANGE_ERROR_MESSAGE = "최소 1명, 최대 3명까지 선택 가능합니다.";
 
-const Counter = ({ ageGroup }: Props) => {
+const PassengerCounter = ({ ageGroup }: Props) => {
   const [count, setCount] = useState(DEFAULT_COUNT);
   const [descriptionText, setDescriptionText] = useState("");
 
   const handleDecrementCount = () => {
+    if (count === MINIMUM_COUNT) {
+      alert(COUNT_RANGE_ERROR_MESSAGE);
+      return;
+    }
     setCount((prev) => {
       const result = prev - 1;
       setDescriptionText(`${ageGroup} 승객 감소 ${result}`);
@@ -19,6 +26,10 @@ const Counter = ({ ageGroup }: Props) => {
   };
 
   const handleIncrementCount = () => {
+    if (count === MAXIMUM_COUNT) {
+      alert(COUNT_RANGE_ERROR_MESSAGE);
+      return;
+    }
     setCount((prev) => {
       const result = prev + 1;
       setDescriptionText(`${ageGroup} 승객 증가 ${result}`);
@@ -26,7 +37,8 @@ const Counter = ({ ageGroup }: Props) => {
     });
   };
 
-  const handleChangeText = () => {
+  const handleChangeText = (event: ChangeEvent<HTMLInputElement>) => {
+    setCount(event.target.valueAsNumber);
     setDescriptionText(`${ageGroup} 승객 텍스트만 변경 ${count}`);
   };
 
@@ -35,7 +47,6 @@ const Counter = ({ ageGroup }: Props) => {
       <h2>{ageGroup}</h2>
       <button
         onClick={handleDecrementCount}
-        disabled={count === 0}
         aria-label={`${ageGroup} 탑승자 한명 줄이기`}
       >
         -
@@ -48,12 +59,13 @@ const Counter = ({ ageGroup }: Props) => {
         id="passengerCount"
         type="number"
         value={count}
+        min="1"
+        max="3"
         onChange={handleChangeText}
       />
 
       <button
         onClick={handleIncrementCount}
-        disabled={count === 3}
         aria-label={`${ageGroup} 탑승자 한명 늘리기`}
       >
         +
@@ -62,4 +74,4 @@ const Counter = ({ ageGroup }: Props) => {
   );
 };
 
-export default Counter;
+export default PassengerCounter;
