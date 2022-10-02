5912acc6e448602821ba87d0088117730156d330
moonheekim0118 <hellomooneekim@gmail.com>
2022-10-03 03:41:29 +0900

    feat: 404 페이지를 수정한다.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,15 +4,18 @@ const NotFound = () => {
   const navigate = useNavigate();
 
   const handlePushToPreviousPage = () => {
-    navigate(-1);
+    navigate("/");
   };
+
   return (
-    <div>
-      <h1>죄송합니다. 존재하지 않는 페이지 입니다.</h1>
+    <article className="flex flex-col gap-10 mt-2.5 justify-center items-center">
+      <h1 className="text-xl font-extrabold">
+        죄송합니다. 존재하지 않는 페이지 입니다.
+      </h1>
       <button type="button" onClick={handlePushToPreviousPage}>
-        뒤로가기
+        메인 화면으로 가기
       </button>
-    </div>
+    </article>
   );
 };
 
