3ea498c37319e88717db6512cc158329b324b8cf
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-10-02 22:05:37 +0900

    feat: 동작에 따른 리딩 기능 구현

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,17 @@ const helpBoxButton = document.querySelector("#help-box");
 const adultHelpContextButton = document.querySelector("#adult-help-context");
 const addPassengerButton = document.querySelector("#add-passenger");
 const reducePassengerButton = document.querySelector("#reduce-passenger");
+const announceElement = document.querySelector("#announce");
 const passengerAmount = document.querySelector(
   "#passenger-amount"
 ) as HTMLInputElement;
 
-const minPassengerAmount = 1;
-const maxPassengerAmount = 3;
+const MIN_PASSENGER_AMOUNT = 1;
+const MAX_PASSENGER_AMOUNT = 3;
+
+const announce = (message: string) => {
+  announceElement.textContent = message;
+};
 
 helpBoxButton.addEventListener("click", () => {
   if (adultHelpContextButton.classList.contains("hide")) {
@@ -21,13 +26,36 @@ helpBoxButton.addEventListener("click", () => {
 });
 
 addPassengerButton.addEventListener("click", () => {
-  if (Number(passengerAmount.value) < maxPassengerAmount) {
+  if (Number(passengerAmount.value) < MAX_PASSENGER_AMOUNT) {
     passengerAmount.value = (Number(passengerAmount.value) + 1).toString();
+    announce(`성인 승객 추가 ${passengerAmount.value}`);
   }
 });
 
 reducePassengerButton.addEventListener("click", () => {
-  if (Number(passengerAmount.value) > minPassengerAmount) {
+  if (Number(passengerAmount.value) > MIN_PASSENGER_AMOUNT) {
     passengerAmount.value = (Number(passengerAmount.value) - 1).toString();
+    announce(`성인 승객 추감 ${passengerAmount.value}`);
+  }
+});
+
+passengerAmount.addEventListener("input", (e: KeyboardEvent) => {
+  const inputValue = Number((e.target as HTMLInputElement).value);
+  if (inputValue === 0) {
+    return;
+  }
+
+  if (inputValue > MAX_PASSENGER_AMOUNT) {
+    announce(
+      `${inputValue}이 입력되어 최대 승객인 ${MAX_PASSENGER_AMOUNT}명으로 변경됩니다.`
+    );
+    (e.target as HTMLInputElement).value = MAX_PASSENGER_AMOUNT.toString();
+  }
+
+  if (inputValue < MIN_PASSENGER_AMOUNT) {
+    announce(
+      `${inputValue}이 입력되어 최소 승객인 ${MIN_PASSENGER_AMOUNT}명으로 변경됩니다.`
+    );
+    (e.target as HTMLInputElement).value = MIN_PASSENGER_AMOUNT.toString();
   }
 });
