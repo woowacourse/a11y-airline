6c99c56b2961c6b1a0d1812a8bb692ad935e0a2b
moonheekim0118 <hellomooneekim@gmail.com>
2022-10-03 03:09:32 +0900

    feat: HelpToolTip에 툴팁 컨텐츠를 알려줄 수 있는 aria label을 추가한다.

--- src/components/HelpToolTip.tsx
+++ src/components/HelpToolTip.tsx
@@ -7,10 +7,10 @@ interface Props {
 const HelpToolTip = ({ content }: Props) => {
   return (
     <button
-      className="tooltip w-9 p-1 border-2 rounded-full border-gray-200 text-gray-300"
+      className="tooltip w-9 p-1 border-2 border-dotted rounded-full border-gray-200 text-gray-300"
       type="button"
       role="tooltip"
-      aria-label="연령 기준 안내"
+      aria-label={`연령 기준 안내 ${content}`}
     >
       ?
       <div className="tooltip-content w-auto rounded bg-black text-white text-xs">
