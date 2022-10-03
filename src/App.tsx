6b43367fa769e891d4dd4d676ca6125d8bc050c4
이아현 <lah1203@naver.com>
2022-10-03 17:59:11 +0900

    feat: Step 1 페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Step1 from '@/pages/Step1';
+
+const baseURI = '/a11y-airline/';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path={baseURI} element={<Step1 />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
