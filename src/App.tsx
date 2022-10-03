d6e6d3a813edb635e00a08fb07142f7a9ddbd901
intae92 <kangit2013@gmail.com>
2022-10-04 02:37:35 +0900

    feat: counter 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,167 @@
+import { css } from '@emotion/react';
+import React, { useState } from 'react';
+
+const buttonStyle = css`
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  margin: 0;
+  padding: 0;
+  cursor: pointer;
+
+  text-align: center;
+  vertical-align: middle;
+`;
+
+const tooltip = css`
+  border: 2px solid #999999;
+  background-color: #ffffff;
+  height: 1.4rem;
+  width: 1.4rem;
+  font-size: 0.9em;
+  color: #999999;
+  border-radius: 1rem;
+`;
+
+const counterButton = css`
+  border: 1px solid #999999;
+  background-color: #ffffff;
+  height: 2rem;
+  width: 2rem;
+  border-radius: 1rem;
+  font-size: 1.4em;
+`;
+
+const inputStyle = css`
+  ::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+  ::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  text-align: center;
+  vertical-align: middle;
+  font-size: 2rem;
+  width: 2rem;
+  padding: 0 0.8rem;
+  border: none;
+  border-bottom: 2px solid black;
+`;
+
+const a11yHidden = css`
+  border: 0;
+  clip: rect(0 0 0 0);
+  clip-path: inset(50%);
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  white-space: nowrap;
+`;
+
+type Passengers = 1 | 2 | 3;
+
 function App() {
-  return <div className="App"></div>;
+  const [count, setCount] = useState<Passengers>(1);
+
+  const changCount = (newCount: Passengers) => {
+    try {
+      if (newCount < 1 || newCount > 3) {
+        throw new Error('인원수는 1~3명까지만 가능합니다.');
+      }
+
+      setCount(newCount);
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  const handleChangeCount = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newCount = (parseInt(e.target.value[e.target.value.length - 1], 10) || 1) as Passengers;
+    changCount(newCount);
+  };
+
+  const handleIncreaseCount = () => {
+    const newCount = (count + 1) as Passengers;
+    changCount(newCount);
+  };
+
+  const handleDecreaseCount = () => {
+    const newCount = (count - 1) as Passengers;
+    changCount(newCount);
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <h1>승객 선택</h1>
+      </header>
+      <section>
+        <div>
+          <div
+            css={css`
+              display: flex;
+              align-items: center;
+              gap: 16px;
+            `}
+          >
+            <h2>성인</h2>
+            <button type="button" css={[buttonStyle, tooltip]}>
+              <span>?</span>
+            </button>
+          </div>
+          <div
+            css={css`
+              display: flex;
+              align-items: center;
+              gap: 16px;
+            `}
+          >
+            <button
+              css={[buttonStyle, counterButton]}
+              type="button"
+              aria-label="성인 탑승자 한명 줄이기"
+              onClick={handleDecreaseCount}
+            >
+              -
+            </button>
+            <label css={a11yHidden} htmlFor="counter" aria-live="polite">
+              성인 {count}
+            </label>
+            <input
+              id="counter"
+              css={[inputStyle]}
+              type="number"
+              value={count}
+              min="1"
+              max="3"
+              onChange={handleChangeCount}
+            />
+            <button
+              css={[buttonStyle, counterButton]}
+              type="button"
+              aria-label="성인 탑승자 한명 늘리기"
+              onClick={handleIncreaseCount}
+            >
+              +
+            </button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default App;
+
+// 성인 탑승자 한명 줄이기 버튼
+// 성인 1 텍스트 숫자만 수정
+// 성인 탑승자 한명 늘리기 버튼
+// 성인 승객 추가 2
+// 성인 탑승자 한명 늘리기 버튼
+// 성인 승객 추가 3
