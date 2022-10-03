d4bb8d15ada5c1d5a4e209bfe3052841499f34a6
woose28 <dldntmdsla@naver.com>
2022-10-03 10:35:23 +0900

    feat: aria-live 및 aria-control 적용
    - 클릭 이벤트의 접근성을 높이기 위해 버튼 컴포넌트에 aria-control와 메시지 컴포넌트에 aria-live 적용

--- src/components/SpinButton/SpinButton.tsx
+++ src/components/SpinButton/SpinButton.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 const SpinButton = () => {
   const [value, setValue] = useState(1);
+  const [message, setMessage] = useState('');
 
   const handleClickDecrease = () => {
     if (value <= 0) {
@@ -10,6 +11,7 @@ const SpinButton = () => {
     }
 
     setValue((prevValue) => prevValue - 1);
+    setMessage(`승객 감소 ${value - 1}`);
   };
 
   const handleClickIncrease = () => {
@@ -18,28 +20,34 @@ const SpinButton = () => {
     }
 
     setValue((prevValue) => prevValue + 1);
+    setMessage(`승객 추가 ${value + 1}`);
   };
 
   return (
     <Wrapper>
-      <label id="성인">{'성인'}</label>
+      <label for="성인">{'성인'}</label>
       <ControlWrapper>
         <ControlButton
           type="button"
           onClick={handleClickDecrease}
-          aria-label="탑승자 한명 줄이기"
+          aria-label="성인 탑승자 한명 줄이기"
+          aria-controls="spinnerMessage"
         >
           -
         </ControlButton>
-        <Input name="성인" value={value} type="number" min={1} max={3} />
+        <Input value={value} type="text" maxLength={1} />
         <ControlButton
           type="button"
           onClick={handleClickIncrease}
           aria-label="성인 탑승자 한명 늘리기"
+          aria-controls="spinnerMessage"
         >
           +
         </ControlButton>
       </ControlWrapper>
+      <Message id="spinnerMessage" aria-live="polite">
+        {message}
+      </Message>
     </Wrapper>
   );
 };
@@ -58,3 +66,12 @@ const ControlWrapper = styled.div`
 const ControlButton = styled.button``;
 
 const Input = styled.input``;
+
+const Message = styled.span`
+  position: absolute;
+  left: -10000px;
+  top: auto;
+  width: 1px;
+  height: 1px;
+  overflow: hidden;
+`;
