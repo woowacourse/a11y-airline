51e74ff9a9422be9cdbc9ba433144e025b777d22
gytks4 <gytks4@naver.com>
2022-10-04 17:59:01 +0900

    feat: 버튼 클릭 시 메시지를 read 하는 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
 import styled from '@emotion/styled';
 import { useState } from 'react';
+import useAnnounceMessage from './useAnnounceMessage';
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const { announceMessage } = useAnnounceMessage();
 
   const handleClickPlusButton = () => {
     if (count >= 3) {
       return;
     }
-    setCount(count => count + 1);
+    const changedCount = count + 1;
+    setCount(changedCount);
+    announceMessage(`승객 추가 ${changedCount}`);
   };
 
   const handleClickMinusButton = () => {
     if (count <= 0) {
       return;
     }
-    setCount(count => count - 1);
+    const changedCount = count - 1;
+    announceMessage(`승객 감소 ${changedCount}`);
+    setCount(changedCount);
   };
 
   return (
@@ -26,7 +32,7 @@ const App = () => {
         <button onClick={handleClickMinusButton} aria-label='성인 탑승자 한명 줄이기'>
           -
         </button>
-        <span>{count}</span>
+        <input type='number' value={count} />
         <button onClick={handleClickPlusButton} aria-label='성인 탑승자 한명 늘리기'>
           +
         </button>
