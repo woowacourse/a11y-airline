73274c8e7c7bb1e96f73e46029be67e8e3123272
gytks4 <gytks4@naver.com>
2022-10-04 16:32:27 +0900

    feat: 카운터 기능 구현

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import styled from '@emotion/styled';
+import { useState } from 'react';
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClickPlusButton = () => {
+    if (count >= 3) {
+      return;
+    }
+    setCount(count => count + 1);
+  };
+
+  const handleClickMinusButton = () => {
+    if (count <= 0) {
+      return;
+    }
+    setCount(count => count - 1);
+  };
+
+  return (
+    <div>
+      <h1>승객 선택</h1>
+      <h2>성인</h2>?
+      <StyledCounter>
+        <button onClick={handleClickMinusButton}>-</button>
+        <span>{count}</span>
+        <button onClick={handleClickPlusButton}>+</button>
+      </StyledCounter>
+    </div>
+  );
+};
+
+export default App;
+
+const StyledCounter = styled.div`
+  display: flex;
+  gap: 20px;
+`;
