6e92aae7e8668dc0975b2b135602b362ceb12a3a
gytks4 <gytks4@naver.com>
2022-10-04 16:59:05 +0900

    feat: aria-label 대체 텍스트 등록

--- src/App.tsx
+++ src/App.tsx
@@ -23,9 +23,13 @@ const App = () => {
       <h1>승객 선택</h1>
       <h2>성인</h2>?
       <StyledCounter>
-        <button onClick={handleClickMinusButton}>-</button>
+        <button onClick={handleClickMinusButton} aria-label='성인 탑승자 한명 줄이기'>
+          -
+        </button>
         <span>{count}</span>
-        <button onClick={handleClickPlusButton}>+</button>
+        <button onClick={handleClickPlusButton} aria-label='성인 탑승자 한명 늘리기'>
+          +
+        </button>
       </StyledCounter>
     </div>
   );
