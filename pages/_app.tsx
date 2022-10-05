86fafdbba8df50685a31f10b41963e510ea28335
cks3066 <cks3066@naver.com>
2022-10-05 15:08:29 +0900

    feat: page 파일 정리

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,38 +1,31 @@
-import '../styles/globals.scss';
+import '../styles/_globals.scss';
 import type { AppProps } from 'next/app';
-import styles from '../styles/pages/_app.module.scss';
+import styles from '../styles/_app.module.scss';
 import Head from 'next/head';
-import Image from 'next/image';
+import Link from 'next/link';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>항공사 웹사이트 컴포넌트의 접근성 높이기</title>
+        <title>항공권 예약 | 우테코항공</title>
         <meta
           name="description"
-          content="우아한테크코스 레벨4 미션 항공사 웹사이트 컴포넌트의 접근성 높이기"
+          content="우테코 미션 - 항공사 웹사이트 컴포넌트의 접근성 높이기"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <header className={styles.header} role="banner">
+        <Link href="/">
+          <h1>우테코 항공 ✈️ - 웹접근성 미션</h1>
+        </Link>
+      </header>
+
       <main className={styles.main}>
         <Component {...pageProps} />
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
 import Link from 'next/link';
-import styles from '../styles/pages/Home.module.scss';
+import styles from '../styles/Home.module.scss';
 
 const Home: NextPage = () => {
   return (
