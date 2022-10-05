75fb64349fcf8d3dc02cdf1a589b8f44c6ebc150
Jeongmin Woo <greenblues1190@gmail.com>
2022-10-05 17:45:12 +0900

    chore: initial settings

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/prop-types': 'off',
+  },
+};
---
+++ webpack/webpack.common.js
@@ -0,0 +1,36 @@
+const { join, resolve } = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: join(__dirname, '../src/index.tsx'),
+  devtool: 'eval-source-map',
+  output: {
+    publicPath: '/',
+    path: resolve(__dirname, 'build/'),
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: join(__dirname, '../public/index.html'),
+    }),
+    new ESLintPlugin(),
+  ],
+};
