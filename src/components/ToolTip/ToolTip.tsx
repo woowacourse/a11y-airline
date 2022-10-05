795e3fb8d3a8f0b8046d1714c5b0b5ab7c10d8bd
(yunjin) <sion11290805@gmail.com>
2022-10-05 09:56:49 +0900

    feat: 퉅팁 aria 수정

--- src/components/ToolTip/ToolTip.tsx
+++ src/components/ToolTip/ToolTip.tsx
@@ -3,12 +3,10 @@ import styles from './ToolTip.module.css';
 const ToolTip = ({ text }: { text: string }) => {
   return (
     <div className={styles.toolTip}>
-      <button aria-describedby='TIP-TEL' aria-label={text} className={styles.toolTipButton}>
+      <button aria-label={text} className={styles.toolTipButton}>
         성인 탑승자 툴팁
       </button>
-      <p id='TIP-TEL' role='tooltip' className={styles.toolTipText}>
-        {text}
-      </p>
+      <p className={styles.toolTipText}>{text}</p>
     </div>
   );
 };
