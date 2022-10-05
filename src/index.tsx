3421dcc81d1238e635a01bf87a5c6dccee7825c5
(yunjin) <sion11290805@gmail.com>
2022-10-05 09:40:28 +0900

    feat: 승객 카운터 구현

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,4 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <>
-    <App />
-  </>
-);
+root.render(<App />);
