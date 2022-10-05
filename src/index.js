6e6bd9750481f020ef905676e8e2d1b479c528b9
InKyoJeong <viapolar@naver.com>
2022-10-06 02:17:30 +0900

    feat: 상태 메세지 추가

--- src/index.js
+++ src/index.js
@@ -5,22 +5,27 @@ const passengerMinusButton = document.querySelector(
 const passengerPlusButton = document.querySelector(
   ".passenger-count__plus-button"
 );
+const statusMessage = document.querySelector(".status-message");
 
 passengerInput.addEventListener("input", ({ target }) => {
   if (target.value < 1 || target.value > 3) {
     passengerInput.value = 1;
     return;
   }
+
+  statusMessage.textContent = `성인 ${passengerInput.value} 텍스트 숫자만 수정`;
 });
 
 passengerMinusButton.addEventListener("click", () => {
   const passengerCount = Number(passengerInput.value);
   if (passengerCount <= 1) return;
   passengerInput.value = passengerCount - 1;
+  statusMessage.textContent = `성인 승객 감소 ${passengerInput.value}`;
 });
 
 passengerPlusButton.addEventListener("click", () => {
   const passengerCount = Number(passengerInput.value);
   if (passengerCount >= 3) return;
   passengerInput.value = passengerCount + 1;
+  statusMessage.textContent = `성인 승객 추가 ${passengerInput.value}`;
 });
