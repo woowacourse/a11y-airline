17bf80b3c00ff97d51f9946bd279e86888095196
cks3066 <cks3066@naver.com>
2022-10-05 15:09:39 +0900

    feat: 소아, 유아 추가

--- pages/spin-button/index.tsx
+++ pages/spin-button/index.tsx
@@ -1,13 +1,17 @@
 import type { NextPage } from 'next';
-import Link from 'next/link';
-import Layout from '../../components/Layout';
-import styles from '../../styles/pages/spin-button.module.scss';
+import PassengerItem from '../../components/PassengerItem';
 
+//"미션1 Spin Button: 승객수 입력하기"
 const SpinButton: NextPage = () => {
   return (
-    <Layout title="미션1 Spin Button: 승객수 입력하기">
-      <h2>하하</h2>
-    </Layout>
+    <section>
+      <h2>승객 선택</h2>
+      <div>
+        <PassengerItem passengerType="성인" minNumber={1} maxNumber={3} />
+        <PassengerItem passengerType="소아" minNumber={0} maxNumber={3} />
+        <PassengerItem passengerType="유아" minNumber={0} maxNumber={3} />
+      </div>
+    </section>
   );
 };
 
