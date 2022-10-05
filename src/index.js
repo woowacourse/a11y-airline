091280c57b8dd738e19cf2e28549e93c918b7650
InKyoJeong <viapolar@naver.com>
2022-10-05 20:47:59 +0900

    feat: 승객 숫자 input 핸들러 추가

--- src/index.js
+++ src/index.js
@@ -6,9 +6,16 @@ const passengerPlusButton = document.querySelector(
   ".passenger-count__plus-button"
 );
 
+passengerInput.addEventListener("input", (e) => {
+  if (e.target.value > 3) {
+    passengerInput.value = 3;
+    return;
+  }
+});
+
 passengerMinusButton.addEventListener("click", () => {
   const passengerCount = Number(passengerInput.value);
-  if (passengerCount <= 0) return;
+  if (passengerCount <= 1) return;
   passengerInput.value = passengerCount - 1;
 });
 
