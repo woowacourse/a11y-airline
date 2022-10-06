5a13ea5709ac3f765b11588adf9d26669e8b97fc
jhy979 <jhy979@gmail.com>
2022-10-06 11:29:09 +0900

    feat: 탑승자 선택 뷰 렌더링

---
+++ src/App.styles.ts
@@ -0,0 +1,14 @@
+import { css } from '@emotion/react';
+
+const layoutStyle = css`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  width: 120px;
+  padding: 20px;
+  border: 1px solid #ced4da;
+  border-radius: 8px;
+`;
+
+export { layoutStyle };
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+/** @jsxImportSource @emotion/react */
+
+import { layoutStyle } from './App.styles';
+import { PASSENGER_TYPE } from './constants';
+import HelpTooltip from './HelpTooltip/HelpTooltip';
+import SpinButton from './SpinButton/SpinButton';
+
 function App() {
-  return <div>a11y-airline</div>;
+  return (
+    <div css={layoutStyle}>
+      <h2>승객 선택</h2>
+      <HelpTooltip
+        baseInfo={PASSENGER_TYPE.ADULT}
+        passengerType={PASSENGER_TYPE.ADULT}
+        tooltipMessage="성인은 만 18세 이상 탑승자로 최대 3명까지 추가할 수 있어요."
+      />
+      <SpinButton min={0} max={3} passengerType={PASSENGER_TYPE.ADULT} />
+    </div>
+  );
 }
 
 export default App;
