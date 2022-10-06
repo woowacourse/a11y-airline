66f5ac546f226ec674ed0143991d9b944af2660c
Daye Lee <508yeah@gmail.com>
2022-10-06 16:37:06 +0900

    refactor: 성인 승객 수 조절 기능을 커스텀 훅으로 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,14 @@
-import { ChangeEvent, useState } from 'react';
 import './App.css';
-import { MESSAGE, PASSENGER } from './constants';
+import { PASSENGER } from './constants';
+import usePassengerSelect from './hooks/usePassengerSelect';
 
 function App() {
-  const [adultCount, setAdultCount] = useState<number | ''>(0);
-
-  const handleClickDecreaseAdultCount = () => {
-    if (adultCount === PASSENGER.ADULT.MIN_COUNT) {
-      alert(MESSAGE.ERROR.LESS_THAN_MIN_ADULT_COUNT);
-
-      return;
-    }
-
-    setAdultCount((prev) => {
-      if (prev === '') return PASSENGER.ADULT.MIN_COUNT;
-      return prev - 1;
-    });
-  };
-
-  const handleChangeAdultCount = ({
-    target,
-  }: ChangeEvent<HTMLInputElement>) => {
-    if (target.value === '') {
-      setAdultCount('');
-
-      return;
-    }
-
-    const value = Number(target.value);
-
-    if (value < PASSENGER.ADULT.MIN_COUNT) {
-      alert(MESSAGE.ERROR.LESS_THAN_MIN_ADULT_COUNT);
-
-      return;
-    }
-
-    if (value > PASSENGER.ADULT.MAX_COUNT) {
-      alert(MESSAGE.ERROR.EXCEED_MAX_ADULT_COUNT);
-
-      return;
-    }
-
-    setAdultCount(value);
-  };
-
-  const handleClickIncreaseAdultCount = () => {
-    if (adultCount === PASSENGER.ADULT.MAX_COUNT) {
-      alert(MESSAGE.ERROR.EXCEED_MAX_ADULT_COUNT);
-
-      return;
-    }
-
-    setAdultCount((prev) => {
-      if (prev === '') return PASSENGER.ADULT.MIN_COUNT + 1;
-      return prev + 1;
-    });
-  };
+  const {
+    adultCount,
+    handleChangeAdultCount,
+    handleClickDecreaseAdultCount,
+    handleClickIncreaseAdultCount,
+  } = usePassengerSelect();
 
   return (
     <div className='App'>
---
+++ src/hooks/usePassengerSelect.ts
@@ -0,0 +1,67 @@
+import { ChangeEvent, useState } from 'react';
+import { MESSAGE, PASSENGER } from '../constants';
+
+function usePassengerSelect() {
+  const [adultCount, setAdultCount] = useState<number | ''>(0);
+
+  const handleClickDecreaseAdultCount = () => {
+    if (adultCount === PASSENGER.ADULT.MIN_COUNT) {
+      alert(MESSAGE.ERROR.LESS_THAN_MIN_ADULT_COUNT);
+
+      return;
+    }
+
+    setAdultCount((prev) => {
+      if (prev === '') return PASSENGER.ADULT.MIN_COUNT;
+      return prev - 1;
+    });
+  };
+
+  const handleChangeAdultCount = ({
+    target,
+  }: ChangeEvent<HTMLInputElement>) => {
+    if (target.value === '') {
+      setAdultCount('');
+
+      return;
+    }
+
+    const value = Number(target.value);
+
+    if (value < PASSENGER.ADULT.MIN_COUNT) {
+      alert(MESSAGE.ERROR.LESS_THAN_MIN_ADULT_COUNT);
+
+      return;
+    }
+
+    if (value > PASSENGER.ADULT.MAX_COUNT) {
+      alert(MESSAGE.ERROR.EXCEED_MAX_ADULT_COUNT);
+
+      return;
+    }
+
+    setAdultCount(value);
+  };
+
+  const handleClickIncreaseAdultCount = () => {
+    if (adultCount === PASSENGER.ADULT.MAX_COUNT) {
+      alert(MESSAGE.ERROR.EXCEED_MAX_ADULT_COUNT);
+
+      return;
+    }
+
+    setAdultCount((prev) => {
+      if (prev === '') return PASSENGER.ADULT.MIN_COUNT + 1;
+      return prev + 1;
+    });
+  };
+
+  return {
+    adultCount,
+    handleChangeAdultCount,
+    handleClickDecreaseAdultCount,
+    handleClickIncreaseAdultCount,
+  };
+}
+
+export default usePassengerSelect;
