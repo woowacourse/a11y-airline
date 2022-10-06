01eb9a7451bdd0bfe06639cdb338cf1cce4c8f06
Daye Lee <508yeah@gmail.com>
2022-10-06 09:51:06 +0900

    feat: 성인 승객 선택 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
 import './App.css';
 
 function App() {
-  return <div></div>;
+  return (
+    <div className='App'>
+      <h1 className='passenger-select-menu'>승객 선택</h1>
+      <div className='passenger-select-option'>
+        <label className='adult-passenger-select-label' htmlFor='adult-count'>
+          성인
+        </label>
+        <div
+          className='passenger-select-tooltip'
+          aria-describedby='passenger-select-tooltip-text'
+        >
+          ?
+          <span id='passenger-select-tooltip-text' role='tooltip'>
+            성인은 최대 3명까지 탑승할 수 있습니다.
+          </span>
+        </div>
+      </div>
+      <div className='passenger-select-control'>
+        <button className='passenger-count-control-button'>-</button>
+        <input
+          type='number'
+          id='adult-count'
+          className='passenger-count-input'
+        />
+        <button className='passenger-count-control-button'>+</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
