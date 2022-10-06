16fde3aedeb5432ef32bbef7e7a220c5436b77bb
airman5573 <rpf5573@kakao.com>
2022-10-06 18:53:02 +0900

    feat: 숫자가 바뀔때 스크린 리더가 숫자를 읽도록 설정

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const snackbarManager = new SnackbarManager();
   const $adultPassengerSpin = $('.adult-passenger-spin', $adultPassengerField);
   const $adultPassengerSpinInput = $('input', $adultPassengerSpin);
+  const $adultPassengerSpinAriaLiveMessage = $('.spin-input-aria-live-message', $adultPassengerSpin);
 
   $adultPassengerSpin.addEventListener('click', e => {
     if (!e.target) return;
@@ -30,6 +31,8 @@ document.addEventListener('DOMContentLoaded', () => {
       return;
     }
     $adultPassengerSpinInput.value = Math.max(0, Math.min(MAX_RESERVABLE_ADULT_PASSENGER_COUNT, nextValue));
+    $adultPassengerSpinAriaLiveMessage.textContent = `성인 승객 추가 ${$adultPassengerSpinInput.value}`;
+    $adultPassengerSpinAriaLiveMessage.setAttribute('aria-live', 'assertive');
   });
 
   $adultPassengerSpinInput.addEventListener('input', e => {
@@ -39,19 +42,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
     target.value = Number(target.value.replace(/[^0-9]/g, ''));
 
+    let isProperRange = true;
+
     const min = target.getAttribute('min');
     const max = target.getAttribute('max');
     if (min !== null) {
       if (target.value < Number(min)) {
+        isPropertRange = false;
         target.value = min;
         snackbarManager.show('인원수는 0명에서 3명까지만 가능합니다');
       }
     }
     if (max !== null) {
       if (target.value > Number(max)) {
+        isPropertRange = false;
         snackbarManager.show('인원수는 0명에서 3명까지만 가능합니다');
         target.value = max;
       }
+    }
+
+    if (isProperRange) {
+      $adultPassengerSpinAriaLiveMessage.textContent = `성인 승객 추가 ${target.value}`;
+      $adultPassengerSpinAriaLiveMessage.setAttribute('aria-live', 'assertive');
     }
   });
 });
