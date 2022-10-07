5f40b3ab04fc60cc56db991f71b3a9e345d87a94
jswith <typingjs@gmail.com>
2022-10-07 15:31:41 +0900

    refactor: step2 리팩토링

--- src/javascript/step2.js
+++ src/javascript/step2.js
@@ -33,66 +33,68 @@ class Step2 {
       </div>`;
   }
 
-  handleClickPrevButton = (e) => {
+  handleClickPrevButton = () => {
     this.currentIndex--;
 
-    if (this.currentIndex < 2) {
+    if (this.currentIndex < 1) {
       this.currentIndex = 1;
 
       this.prevButton.setAttribute("aria-disabled", "true");
       this.prevButton.setAttribute("aria-label", "이전 버튼 (사용 중지)");
-      this.slides.forEach(
-        (slide) =>
-          (slide.style.transform = `translateX(-${
-            this.SLIDE_SIZE * (this.currentIndex - 1)
-          }px)`)
-      );
+      this.moveSlide();
+
       return;
     }
 
-    this.nextButton.setAttribute("aria-disabled", "false");
-    this.prevButton.setAttribute("aria-label", "이전 버튼");
-    this.nextButton.setAttribute("aria-label", "다음 버튼");
-
-    this.slides.forEach(
-      (slide) =>
-        (slide.style.transform = `translateX(-${
-          this.SLIDE_SIZE * (this.currentIndex - 1)
-        }px)`)
-    );
+    this.enableButton("prev");
+    this.moveSlide();
   };
 
-  handleClickNextButton = (e) => {
+  handleClickNextButton = () => {
     this.currentIndex++;
 
-    if (this.currentIndex > 6) {
+    if (this.currentIndex > 7) {
       this.currentIndex = 7;
 
       this.nextButton.setAttribute("aria-disabled", "true");
       this.nextButton.setAttribute("aria-label", "다음 버튼 (사용 중지)");
-      this.slides.forEach(
-        (slide) =>
-          (slide.style.transform = `translateX(-${
-            this.SLIDE_SIZE * (this.currentIndex - 1)
-          }px)`)
-      );
+      this.moveSlide();
+
       return;
     }
 
-    this.prevButton.setAttribute("aria-disabled", "false");
+    this.enableButton("next");
+    this.moveSlide();
+  };
+
+  handleAnchorFocus = (e) => {
+    console.log(e.target.dataset.id);
+  };
+
+  enableButton = (role) => {
+    if (!(role === "next" || role === "prev")) return;
+
     this.prevButton.setAttribute("aria-label", "이전 버튼");
     this.nextButton.setAttribute("aria-label", "다음 버튼");
 
+    if (role === "next") {
+      this.prevButton.setAttribute("aria-disabled", "false");
+      return;
+    }
+
+    if (role === "prev") {
+      this.nextButton.setAttribute("aria-disabled", "false");
+      return;
+    }
+  };
+
+  moveSlide = () => {
     this.slides.forEach(
       (slide) =>
         (slide.style.transform = `translateX(-${
           this.SLIDE_SIZE * (this.currentIndex - 1)
         }px)`)
     );
-  };
-
-  handleAnchorFocus = (e) => {
-    console.log(e.target.dataset.id);
   };
 }
 
