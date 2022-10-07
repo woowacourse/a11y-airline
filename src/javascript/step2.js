a10ef2036b54d327f939100be72601c837b9798e
jswith <typingjs@gmail.com>
2022-10-07 14:33:18 +0900

    feat: 슬라이드 방식 수정

--- src/javascript/step2.js
+++ src/javascript/step2.js
@@ -1,16 +1,20 @@
-import { $ } from "./util.js";
+import { $, $$ } from "./util.js";
 import slideList from "./slide.js";
 
 class Step2 {
   constructor() {
     this.slideList = slideList;
+    this.currentIndex = 1;
+    this.SLIDE_SIZE = 260;
 
     $("#app").innerHTML = this.template();
 
-    this.carousel = $(".carousel");
+    this.prevButton = $(".prev");
+    this.nextButton = $(".next");
+    this.slides = $$(".slide");
 
-    $(".prev").addEventListener("click", this.handleClickPrevButton);
-    $(".next").addEventListener("click", this.handleClickNextButton);
+    this.prevButton.addEventListener("click", this.handleClickPrevButton);
+    this.nextButton.addEventListener("click", this.handleClickNextButton);
   }
 
   template() {
@@ -20,27 +24,67 @@ class Step2 {
         <section class="carousel" role="list">
           ${this.slideList.map((slide) => `${slide}`).join("")}
         </section>
-        <button class="prev" aria-label="이전 버튼">&lt;</button>
-        <button class="next" aria-label="다음 버튼">&gt;</button>
+        <button class="prev" aria-label="이전 버튼 (사용 중지)" aria-disabled="true">&lt;</button>
+        <button class="next" aria-label="다음 버튼" aria-disabled="false">&gt;</button>
       </div>`;
   }
 
-  render() {
-    const template = this.slideList.map((slide) => slide).join("");
+  handleClickPrevButton = (e) => {
+    this.currentIndex--;
 
-    this.carousel.replaceChildren();
-    this.carousel.insertAdjacentHTML("afterbegin", template);
-  }
+    if (this.currentIndex < 2) {
+      this.currentIndex = 1;
 
-  handleClickPrevButton = () => {
-    this.slideList.unshift(this.slideList.pop());
+      this.prevButton.setAttribute("aria-disabled", "true");
+      this.prevButton.setAttribute("aria-label", "이전 버튼 (사용 중지)");
+      this.slides.forEach(
+        (slide) =>
+          (slide.style.transform = `translateX(-${
+            this.SLIDE_SIZE * (this.currentIndex - 1)
+          }px)`)
+      );
+      return;
+    }
 
-    this.render();
+    this.nextButton.setAttribute("aria-disabled", "false");
+    this.prevButton.setAttribute("aria-label", "이전 버튼");
+    this.nextButton.setAttribute("aria-label", "다음 버튼");
+
+    this.slides.forEach(
+      (slide) =>
+        (slide.style.transform = `translateX(-${
+          this.SLIDE_SIZE * (this.currentIndex - 1)
+        }px)`)
+    );
   };
 
-  handleClickNextButton = () => {
-    this.slideList.push(this.slideList.shift());
-    this.render();
+  handleClickNextButton = (e) => {
+    this.currentIndex++;
+
+    if (this.currentIndex > 6) {
+      this.currentIndex = 7;
+
+      this.nextButton.setAttribute("aria-disabled", "true");
+      this.nextButton.setAttribute("aria-label", "다음 버튼 (사용 중지)");
+      this.slides.forEach(
+        (slide) =>
+          (slide.style.transform = `translateX(-${
+            this.SLIDE_SIZE * (this.currentIndex - 1)
+          }px)`)
+      );
+      return;
+    }
+
+    this.prevButton.setAttribute("aria-disabled", "false");
+    this.prevButton.setAttribute("aria-label", "이전 버튼");
+    this.nextButton.setAttribute("aria-label", "다음 버튼");
+
+    this.slides.forEach(
+      (slide) =>
+        (slide.style.transform = `translateX(-${
+          this.SLIDE_SIZE * (this.currentIndex - 1)
+        }px)`)
+    );
   };
 }
 
