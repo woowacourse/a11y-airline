97fc3d3def8fb0359162c394774efc7d3739575e
JASUN LEE <liswktjs@gmail.com>
2022-10-08 17:35:29 +0900

    fix: role과 id를 부여해 tooltip안내메시지가 읽히도록 한다

--- src/common/ChipButton/ChipButton.tsx
+++ src/common/ChipButton/ChipButton.tsx
@@ -8,10 +8,15 @@ const ChipButton = () => {
 	};
 	return (
 		<S.Container>
-			<S.ChipButton type="button" aria-label="안내메세지 보기" onClick={showMessage}>
+			<S.ChipButton
+				type="button"
+				aria-label="안내메세지 보기"
+				onClick={showMessage}
+				aria-describedby="passenger-description"
+			>
 				?
 			</S.ChipButton>
-			<S.Message isOpen={isOpen}>
+			<S.Message isOpen={isOpen} id="passenger-description" role={'tooltip'}>
 				성인인 승객의 인원 수를 아래의 +, - 버튼 또는 직접입력을 통해 기입해주세요
 			</S.Message>
 		</S.Container>
