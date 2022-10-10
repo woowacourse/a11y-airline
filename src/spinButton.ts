f5a2e82dab7b95a5e135e83753fac0ca6432f8ce
Yo Wook Kim <uk960214@gmail.com>
2022-10-10 14:32:44 +0900

    [항공사 웹사이트의 컴포넌트 접근성 높이기 - 1단계] 블링(김요욱) 미션 제출합니다. (#29)
    * chore: 웹팩과 기본 프로젝트 설정
    
    * chore: css 설정
    
    * feat: html, css 구현
    
    * feat: input에 min, max 값 추가
    
    * style: 버튼 컨테이너에 덮개 after 요소 추가
    
    * feat: spin button 기능 구현
    
    * feat: input 상태 변화 시 스크린 리더에게 전달하도록 설정
    
    * chore: gh-page로 배포 설정
    
    * style: ios 버튼 기본 색상 추가
    
    * feat: 1초 후 안내 메시지가 삭제되도록 설정
    
    * feat: 툴팁과 상태바를 ios, android 등 모든 환경에서 정상 작동하도록 설정
    
    * style: ios input 기본 스타일 설정
    
    * refactor: css 모듈로 분리 및 일부 클래스명, 아이디명 변경
    
    * refactor: ts 리팩토링
    
    * feat: 직접 input 값을 변경 시 검증 기능 추가
    
    * style: 회색 색상 변경
    
    * fix: 입력란이 빈 칸을 허용하지 않는 오류 해결
    
    * feat: 직접 값을 입력했을 때에도 변경된 결과를 스크린리더가 읽도록 설정
    
    * refactor: 시맨틱 태그 변경
    
    * refactor: 이벤트 위임 함수에서 타입 단언을 타입 가드로 대체
    
    * feat: native alert 대신 snackbar를 사용하도록 수정
    
    * feat: tooltip 구현
    
    * refactor: 여러번 사용되는 gray 색상 css 변수로 저장
    
    * style: 페이지 형식이 깨지지 않도록 툴팁 스타일 변경
    
    * fix: 입력값을 지울 수 있도록 수정
    
    * fix: 툴팁 오픈 여부에 따라 aria expanded 속성 변경되도록 수정
    
    * fix: 툴팁을 닫으면 툴팁 열기 버튼으로 돌아가도록 수정
    
    * fix: aria expanded 속성 부여 위치 변경

---
+++ src/spinButton.ts
@@ -0,0 +1,135 @@
+import { MESSAGES, SPIN_INPUT_RANGE } from "./constants";
+import { alertWithSnackbar } from "./snackbar";
+
+const spinButtonForm = document.querySelector(".spin-input-form");
+const tooltip = document.querySelector("[role=tooltip]");
+const tooltipOpenButton =
+  document.querySelector<HTMLButtonElement>(".tooltip-button");
+
+const spinButtonInput =
+  spinButtonForm.querySelector<HTMLButtonElement>("#spin-input-value");
+const spinInputStatus = document.querySelector("#spin-input-status");
+
+const handleSRMessage = (message: string) => {
+  spinInputStatus.textContent = message;
+
+  setTimeout(() => {
+    spinInputStatus.textContent = "";
+  }, 500);
+};
+
+const setSpinInputValue = (to: number) => {
+  spinButtonInput.value = String(to);
+  handleSRMessage(MESSAGES.현재_승객_수_안내(to));
+};
+
+const handleValueChange = (to: number) => {
+  if (typeof to !== "number") {
+    throw new Error(MESSAGES.유효하지_않은_입력값_오류);
+  }
+  if (to > SPIN_INPUT_RANGE.MAX || to < SPIN_INPUT_RANGE.MIN) {
+    throw new Error(MESSAGES.승객_수_범위_오류);
+  }
+
+  setSpinInputValue(to);
+};
+
+const getChangeAmountFromClassList = (classList: DOMTokenList) =>
+  classList.contains("increment")
+    ? 1
+    : classList.contains("decrement")
+    ? -1
+    : 0;
+
+const handleSpinButtonClick = (classList: DOMTokenList) => {
+  const amount = getChangeAmountFromClassList(classList);
+  const currentValue = Number(spinButtonInput.value);
+
+  try {
+    handleValueChange(currentValue + amount);
+  } catch (e) {
+    if (e instanceof Error) {
+      alertWithSnackbar(e.message);
+      return;
+    }
+
+    alertWithSnackbar(MESSAGES.알_수_없는_오류);
+  }
+};
+
+const handleSpinInputValue = (target: HTMLInputElement) => {
+  console.log(target.value === "");
+  if (target.value === "") {
+    target.value = "";
+    return;
+  }
+  const inputValue = Number(target.value);
+  if (
+    inputValue <= SPIN_INPUT_RANGE.MAX &&
+    inputValue >= SPIN_INPUT_RANGE.MIN
+  ) {
+    return;
+  }
+
+  if (inputValue > SPIN_INPUT_RANGE.MAX) {
+    target.value = String(SPIN_INPUT_RANGE.MAX);
+  } else {
+    target.value = String(SPIN_INPUT_RANGE.MIN);
+  }
+  throw new Error(MESSAGES.승객_수_범위_오류);
+};
+
+const handleTooltipButtonClick = (e: Event) => {
+  e.stopPropagation();
+
+  tooltip.classList.remove("hide");
+  tooltipOpenButton.ariaExpanded = "true";
+};
+
+const handleTooltipClose = () => {
+  tooltip.classList.add("hide");
+  tooltipOpenButton.focus();
+};
+
+window.addEventListener("click", (e) => {
+  if (!(e.target instanceof HTMLElement)) return;
+  if (tooltip.classList.contains("hide") || tooltip.contains(e.target)) return;
+  handleTooltipClose();
+});
+
+spinButtonForm.addEventListener("click", (e) => {
+  if (!(e.target instanceof HTMLButtonElement)) return;
+
+  const { classList } = e.target;
+  if (classList.contains("spin-button")) {
+    handleSpinButtonClick(classList);
+  }
+  if (classList.contains("tooltip-button")) {
+    handleTooltipButtonClick(e);
+  }
+  if (classList.contains("tooltip-close-button")) {
+    handleTooltipClose();
+  }
+});
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && !tooltip.classList.contains("hide")) {
+    tooltip.classList.add("hide");
+    tooltipOpenButton.ariaExpanded = "false";
+  }
+});
+
+spinButtonInput.addEventListener("input", (e) => {
+  if (!(e.target instanceof HTMLInputElement)) return;
+
+  try {
+    handleSpinInputValue(e.target);
+  } catch (exception) {
+    if (exception instanceof Error) {
+      alertWithSnackbar(exception.message);
+      return;
+    }
+
+    alertWithSnackbar(MESSAGES.알_수_없는_오류);
+  }
+});
