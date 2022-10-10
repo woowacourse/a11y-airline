8ee3b284dae58c7fa372262a97011ab63ca59f46
hwangjungmin <hwangstar156@gmail.com>
2022-10-10 18:41:31 +0900

    feat: carousel 애니메이션 추가

--- src/ts/carousel.ts
+++ src/ts/carousel.ts
@@ -1,3 +1,40 @@
 import "../css/carousel.css";
 
-console.log("dd");
+const $prevArrowButton = document.querySelector(
+  ".prev-arrow-button"
+) as HTMLButtonElement;
+const $nextArrowButton = document.querySelector(
+  ".next-arrow-button"
+) as HTMLButtonElement;
+const $carouselList = document.querySelector(
+  ".carousel-list"
+) as HTMLUListElement;
+
+let currentCarouselIndex = 0;
+
+const animateCarousel = (currentCarouselIndex: number) => {
+  $carouselList.animate(
+    [{ transform: `translateX(calc((-270px) * ${currentCarouselIndex}))` }],
+    {
+      // timing options
+      duration: 200,
+      fill: "forwards",
+    }
+  );
+};
+
+$prevArrowButton.addEventListener("click", () => {
+  if (currentCarouselIndex === 0) {
+    return;
+  }
+  currentCarouselIndex -= 1;
+  animateCarousel(currentCarouselIndex);
+});
+
+$nextArrowButton.addEventListener("click", () => {
+  if (currentCarouselIndex === 3) {
+    return;
+  }
+  currentCarouselIndex += 1;
+  animateCarousel(currentCarouselIndex);
+});
