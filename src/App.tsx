fdbe538395fa7bf57d9e6b1e1013c24cf57de8c7
soyi47 <soyi.dev@gmail.com>
2022-10-11 20:26:08 +0900

    feat:  App.tsx에서 step1 관련 요소 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
 import React from "react";
 import "./style/App.css";
 
-import PassengerController from "./components/PassengerController";
-
 function App() {
-  return (
-    <div className="App" tabIndex={0}>
-      <h2>승객 선택</h2>
-      <PassengerController
-        passengerType="성인"
-        defaultValue={1}
-        step={1}
-        min={0}
-        max={3}
-      />
-    </div>
-  );
+  return <div className="App" tabIndex={0}></div>;
 }
 
 export default App;
