2993c725c45aa5cf11a43f1aebc2693e6494fcda
wonsss <bigdatajang@gmail.com>
2022-10-12 00:21:54 +0900

    feat: 컨트롤 버튼 추가

--- src/Pages/CarouselPage/data.ts
+++ src/Pages/CarouselPage/data.ts
@@ -1,11 +1,11 @@
-import image0 from 'images/0.jpg';
-import image1 from 'images/1.jpg';
-import image2 from 'images/2.jpg';
-import image3 from 'images/3.jpg';
-import image4 from 'images/4.jpg';
-import image5 from 'images/5.jpg';
-import image6 from 'images/6.jpg';
-import image7 from 'images/7.jpg';
+import image0 from 'assets/0.jpg';
+import image1 from 'assets/1.jpg';
+import image2 from 'assets/2.jpg';
+import image3 from 'assets/3.jpg';
+import image4 from 'assets/4.jpg';
+import image5 from 'assets/5.jpg';
+import image6 from 'assets/6.jpg';
+import image7 from 'assets/7.jpg';
 
 export const carouselData = [
   {
