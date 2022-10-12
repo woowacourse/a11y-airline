f7df4d1d6e51647c3981d0948fd0127657d1c58f
2019015414 <2019015414@hanyang.ac.kr>
2022-10-12 10:36:00 +0900

    feat: travelList constants 생성

---
+++ a11y-airline/src/constants/index.ts
@@ -0,0 +1,67 @@
+export const travelList = [
+  {
+    place: "서울/인천 - 두바이",
+    seat: "일반석 왕복",
+    minCost: 1121600,
+    link: "https://www.koreanair.com/booking/best-prices?departureCode=ICN&destinationCode=DXB&duration=7&cabin=Y",
+    placeImg:
+      "https://www.koreanair.com/content/dam/koreanair/ko/airport-img/DXB-list-pc.jpg",
+  },
+  {
+    place: "서울/인천 - 후쿠오카",
+    seat: "일반석 왕복",
+    minCost: 340400,
+    link: "https://www.koreanair.com/booking/best-prices?departureCode=ICN&destinationCode=FUK&cabin=Y&tripType=RT&duration=7",
+    placeImg:
+      "	https://www.koreanair.com/content/dam/koreanair/ko/airport-img/FUK-list-pc.jpg",
+  },
+
+  {
+    place: "서울/인천 - 푸껫",
+    seat: "일반석 왕복",
+    minCost: 704200,
+    link: "https://www.koreanair.com/booking/best-prices?departureCode=ICN&destinationCode=HKT&cabin=Y&tripType=RT&duration=7",
+    placeImg:
+      "https://www.koreanair.com/content/dam/koreanair/ko/airport-img/HKT-list-pc.jpg",
+  },
+  {
+    place: "서울/인천 - 치앙마이",
+    seat: "일반석 왕복",
+    minCost: 839200,
+    link: "https://www.koreanair.com/booking/best-prices?departureCode=ICN&destinationCode=CNX&cabin=Y&tripType=RT&duration=7",
+    placeImg:
+      "https://www.koreanair.com/content/dam/koreanair/ko/airport-img/CNX-list-pc.jpg",
+  },
+  {
+    place: "서울/인천 - 바르셀로",
+    seat: "일반석 왕복",
+    minCost: 1546300,
+    link: "https://www.koreanair.com/booking/best-prices?departureCode=ICN&destinationCode=BCN&cabin=Y&tripType=RT&duration=7",
+    placeImg:
+      "https://www.koreanair.com/content/dam/koreanair/ko/airport-img/BCN-list-pc.jpg",
+  },
+  {
+    place: "서울/인천 - 하노이",
+    seat: "일반석 왕복",
+    minCost: 1121600,
+    link: "https://www.koreanair.com/booking/best-prices?departureCode=ICN&destinationCode=HAN&cabin=Y&tripType=RT&duration=7",
+    placeImg:
+      "https://www.koreanair.com/content/dam/koreanair/ko/airport-img/HAN-list-pc.jpg",
+  },
+  {
+    place: "서울/인천-로마/레오나르도 다빈치",
+    seat: "일반석 왕복",
+    minCost: 1454300,
+    link: "https://www.koreanair.com/booking/best-prices?departureCode=ICN&destinationCode=FCO&cabin=Y&tripType=RT&duration=7",
+    placeImg:
+      "https://www.koreanair.com/content/dam/koreanair/ko/airport-img/FCO-list-pc.jpg",
+  },
+  {
+    place: "서울/인천-호놀룰루 (하와이)",
+    seat: "일반석 왕복",
+    minCost: 1244900,
+    link: "https://www.koreanair.com/booking/best-prices?departureCode=ICN&destinationCode=HNL&cabin=Y&tripType=RT&duration=7",
+    placeImg:
+      "https://www.koreanair.com/content/dam/koreanair/ko/airport-img/HNL-list-pc.jpg",
+  },
+];
