c9811de653d1e104654c569535bf44cae13afb5b
prefer2 <sunho6200@gmail.com>
2022-10-12 16:32:20 +0900

    feat: CarouselItem 구현

---
+++ src/pages/CarouselPage/components/CarouselItem/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./style.css";
+
+interface CarouselItemProps {
+  href: string;
+  image: string;
+  location: string;
+  seat: string;
+  price: number;
+}
+const CarouselItem = ({
+  href,
+  image,
+  location,
+  seat,
+  price,
+}: CarouselItemProps) => {
+  const formattedPrice = new Intl.NumberFormat("ko-KR").format(price);
+
+  return (
+    <div className="carousel__item">
+      <a href={href}>
+        <img className="link__img" src={image} />
+        <div className="description">
+          <p className="location">서울/인천 - {location}</p>
+          <p className="seat">{seat} 왕복</p>
+          <p className="price">KRW {formattedPrice} ~</p>
+        </div>
+      </a>
+    </div>
+  );
+};
+
+export default CarouselItem;
