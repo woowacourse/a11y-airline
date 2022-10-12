1bfe0cefa7e0ccc1ecb7157557abf0760fa5949d
cks3066 <cks3066@naver.com>
2022-10-12 16:16:25 +0900

    chore: 데이터 추가

---
+++ data/index.ts
@@ -0,0 +1,62 @@
+export const TRAVEL_DATA = [
+  {
+    id: 1,
+    link: 'https://www.koreanair.com/booking/best-prices?departureCode=ICN&destinationCode=CNX&duration=7&cabin=Y',
+    imageUrl:
+      'https://www.koreanair.com/content/dam/koreanair/ko/airport-img/DXB-list-pc.jpg',
+    departure: '서울/인천',
+    arrival: '두바이',
+    type: '일반석 왕복',
+    price: 1158000,
+  },
+  {
+    id: 2,
+    link: 'https://www.koreanair.com/booking/best-prices?departureCode=ICN&destinationCode=FUK&duration=7&cabin=Y',
+    imageUrl:
+      'https://www.koreanair.com/content/dam/koreanair/ko/airport-img/FUK-list-pc.jpg',
+    departure: '서울/인천',
+    arrival: '후쿠오카',
+    type: '일반석 왕복',
+    price: 340400,
+  },
+  {
+    id: 3,
+    link: 'https://www.koreanair.com/booking/best-prices?departureCode=ICN&destinationCode=HKT&duration=7&cabin=Y',
+    imageUrl:
+      'https://www.koreanair.com/content/dam/koreanair/ko/airport-img/HKT-list-pc.jpg',
+    departure: '서울/인천',
+    arrival: '푸껫',
+    type: '일반석 왕복',
+    price: 704200,
+  },
+  {
+    id: 4,
+    link: 'https://www.koreanair.com/booking/best-prices?departureCode=ICN&destinationCode=CNX&duration=7&cabin=Y',
+    imageUrl:
+      'https://www.koreanair.com/content/dam/koreanair/ko/airport-img/BCN-list-pc.jpg',
+    departure: '서울/인천',
+    arrival: '치앙마이',
+    type: '일반석 왕복',
+    price: 839200,
+  },
+  {
+    id: 5,
+    link: 'https://www.koreanair.com/booking/best-prices?departureCode=ICN&destinationCode=BCN&duration=7&cabin=Y',
+    imageUrl:
+      'https://www.koreanair.com/content/dam/koreanair/ko/airport-img/HAN-list-pc.jpg',
+    departure: '서울/인천',
+    arrival: '바르셀로나',
+    type: '일반석 왕복',
+    price: 1546300,
+  },
+  {
+    id: 6,
+    link: 'https://www.koreanair.com/booking/best-prices?departureCode=ICN&destinationCode=HAN&duration=7&cabin=Y',
+    imageUrl:
+      'https://www.koreanair.com/content/dam/koreanair/ko/airport-img/FCO-list-pc.jpg',
+    departure: '서울/인천',
+    arrival: '하노이',
+    type: '일반석 왕복',
+    price: 527400,
+  },
+];
