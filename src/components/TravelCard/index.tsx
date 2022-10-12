ed6cbb5a6c5105e05b541ee5904d61b321eebbb6
moonheekim0118 <hellomooneekim@gmail.com>
2022-10-13 01:56:24 +0900

    feat: TravelCard 컴포넌트의 웹접근성을 개선한다.

--- src/components/TravelCard/index.tsx
+++ src/components/TravelCard/index.tsx
@@ -27,15 +27,25 @@ const TravelCard = ({
           className="card__image"
           src={imageSrc}
           alt={`여행지 ${destination} 사진`}
+          aria-hidden={true}
         />
         <div className="card__description">
           <p className="destination">
-            {departure} - {destination}
+            <span aria-label={`출발지 ${departure}`}> {departure} </span>
+            <span aria-hidden={true}> ~ </span>
+            <span aria-label={`도착지 ${destination}`}> {destination} </span>
           </p>
-          <p className="seat">
-            {seat} {tripType}
+          <p className="trip-information">
+            <span aria-label={`좌석 등급 ${seat}`}> {seat} </span>
+            <span aria-label={`티켓 종류 ${tripType}`}>{tripType}</span>
           </p>
-          <p className="price">KWR {parseToKWRPrice(price)} ~</p>
+          <p className="price">
+            <span aria-hidden={true}>KWR</span>
+            <span className="sr-only">한화 </span>
+            {parseToKWRPrice(price)}
+            <span className="sr-only">원 부터 </span>
+            <span aria-hidden={true}> ~</span>
+          </p>
         </div>
       </a>
     </div>
