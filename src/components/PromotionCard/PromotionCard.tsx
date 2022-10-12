6846b0ad4fb49bb621d2785f59dea6b1e07e3528
Daye Lee <508yeah@gmail.com>
2022-10-13 07:33:57 +0900

    feat: 프로모션 아이템 컴포넌트 구현

---
+++ src/components/PromotionCard/PromotionCard.tsx
@@ -0,0 +1,45 @@
+import './PromotionCard.css';
+
+import { addThousandUnitComma } from '../../utils';
+
+interface PromotionCardProps {
+  link: string;
+  image: string;
+  destination: string;
+  lowestPrice: number;
+  departure?: '서울/인천' | string;
+  seat?: '일반석' | '프레스티지석';
+  trip?: '왕복' | '편도';
+}
+
+function PromotionCard({
+  link,
+  image,
+  destination,
+  lowestPrice,
+  departure = '서울/인천',
+  seat = '일반석',
+  trip = '왕복',
+}: PromotionCardProps) {
+  const handleClickPromotionCard = () => {
+    window.open(link);
+  };
+
+  return (
+    <div
+      className='promotion-card'
+      style={{ backgroundImage: `url(${image})` }}
+      onClick={handleClickPromotionCard}
+    >
+      <div className='promotion-info'>
+        <p className='promotion-route'>{`${departure} - ${destination}`}</p>
+        <p className='promotion-flight'>{`${seat} ${trip}`}</p>
+        <p className='promotion-price'>{`KRW ${addThousandUnitComma(
+          lowestPrice
+        )} ~`}</p>
+      </div>
+    </div>
+  );
+}
+
+export default PromotionCard;
---
+++ src/utils.ts
@@ -0,0 +1,5 @@
+const addThousandUnitComma = (number: number) => {
+  return number.toLocaleString('ko-KR');
+};
+
+export { addThousandUnitComma };
