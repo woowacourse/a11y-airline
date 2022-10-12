91c978c5b558bdcfd918aa8a06a84aa82d14c2cc
InKyoJeong <viapolar@naver.com>
2022-10-12 20:54:03 +0900

    fix: 버튼 태그 위치 수정 - ul 태그 밖으로 수정

--- src/carousel.js
+++ src/carousel.js
@@ -3,11 +3,11 @@ const slideLinks = document.querySelectorAll(".slide-link");
 const btnLeft = document.querySelector(".slider-btn__left");
 const btnRight = document.querySelector(".slider-btn__right");
 
-let curSlide = 0;
-const maxSlide = slides.length - 1;
-
 const addDisabled = (element) => element.classList.add("disabled");
 const removeDisabled = (element) => element.classList.remove("disabled");
+
+let curSlide = 0;
+const maxSlide = slides.length - 1;
 
 if (curSlide === 0) {
   addDisabled(btnLeft);
@@ -22,10 +22,9 @@ const goToSlide = (slideIndex) => {
 const nextSlide = () => {
   if (curSlide !== maxSlide - 1) {
     curSlide++;
-  }
-
-  if (curSlide === maxSlide - 1) {
-    addDisabled(btnRight);
+    if (curSlide === maxSlide - 1) {
+      addDisabled(btnRight);
+    }
   }
 
   removeDisabled(btnLeft);
@@ -35,20 +34,19 @@ const nextSlide = () => {
 const prevSlide = () => {
   if (curSlide !== 0) {
     curSlide--;
-  }
-
-  if (curSlide === 0) {
-    addDisabled(btnLeft);
+    if (curSlide === 0) {
+      addDisabled(btnLeft);
+    }
   }
 
   removeDisabled(btnRight);
   goToSlide(curSlide);
 };
 
-slideLinks.forEach((link, i) => {
+slideLinks.forEach((link, index) => {
   link.addEventListener("focus", () => {
-    if (i === maxSlide) return;
-    goToSlide(i);
+    if (index === maxSlide) return;
+    goToSlide(index);
   });
 });
 
