2526fd00cd36db9a45544c1789328174c44fde13
KIM YEJI <rladpwl0512@gmail.com>
2022-10-12 13:47:18 +0900

    feat: 제목에 대한 설명 추가
    - aria-describedby 를  사용하여, 제목에 대한 부가 설명을 작성

--- src/pages/PromotionList/PromotionList.jsx
+++ src/pages/PromotionList/PromotionList.jsx
@@ -27,88 +27,94 @@ function PromotionList() {
   };
 
   return (
-    <div className="layout-container">
-      <div className="container">
-        <h2>지금 떠나기 좋은 여행</h2>
+    <>
+      <div className="layout-container">
+        <section className="container">
+          <h2 aria-describedby="title">지금 떠나기 좋은 여행</h2>
 
-        <div className="card-container">
-          <ul className="cards">
-            <PromotionListItem
-              img="https://www.koreanair.com/content/dam/koreanair/ko/airport-img/DXB-list-pc.jpg"
-              departure="서울/인천"
-              arrival="두바이"
-              seat="일반석 왕복"
-              price="KRW 1,157,500 ~"
-            />
-            <PromotionListItem
-              img="https://www.koreanair.com/content/dam/koreanair/ko/airport-img/FUK-list-pc.jpg"
-              departure="서울/인천"
-              arrival="후쿠오카"
-              seat="일반석 왕복"
-              price="KRW 340,200 ~"
-            />
-            <PromotionListItem
-              img="https://www.koreanair.com/content/dam/koreanair/ko/airport-img/HKT-list-pc.jpg"
-              departure="서울/인천"
-              arrival="푸껫"
-              seat="일반석 왕복"
-              price="KRW 704,100 ~"
-            />
-            <PromotionListItem
-              img="https://www.koreanair.com/content/dam/koreanair/ko/airport-img/CNX-list-pc.jpg"
-              departure="서울/인천"
-              arrival="치앙마이"
-              seat="일반석 왕복"
-              price="KRW 839,100 ~"
-            />
-            <PromotionListItem
-              img="https://www.koreanair.com/content/dam/koreanair/ko/airport-img/BCN-list-pc.jpg"
-              departure="서울/인천"
-              arrival="바르셀로나"
-              seat="일반석 왕복"
-              price="KRW 1,546,300 ~"
-            />
-            <PromotionListItem
-              img="https://www.koreanair.com/content/dam/koreanair/ko/airport-img/HAN-list-pc.jpg"
-              departure="서울/인천"
-              arrival="하노이"
-              seat="일반석 왕복"
-              price="KRW 486,900 ~"
-            />
-            <PromotionListItem
-              img="https://www.koreanair.com/content/dam/koreanair/ko/airport-img/FCO-list-pc.jpg"
-              departure="서울/인천"
-              arrival="로마/레오나르도"
-              seat="일반석 왕복"
-              price="KRW 1,454,200 ~"
-            />
-            <PromotionListItem
-              img="https://www.koreanair.com/content/dam/koreanair/ko/airport-img/HNL-list-pc.jpg"
-              departure="서울/인천"
-              arrival="호놀룰루 (하와이)"
-              seat="일반석 왕복"
-              price="KRW 1,243,800 ~"
-            />
-          </ul>
-        </div>
-        <div className="buttons">
-          <button
-            className="gallery-button prev"
-            onClick={handleShowPrevCard}
-            disabled={slideIdx === 1}
-          >
-            <span className="invisible">이전</span>
-          </button>
-          <button
-            className="gallery-button next"
-            onClick={handleShowNextCard}
-            disabled={slideIdx === endSlideIdx}
-          >
-            <span className="invisible">다음</span>
-          </button>
-        </div>
+          <div className="buttons">
+            <button
+              className="gallery-button prev"
+              onClick={handleShowPrevCard}
+              disabled={slideIdx === 1}
+            >
+              <span className="invisible">이전 프로모션</span>
+            </button>
+            <button
+              className="gallery-button next"
+              onClick={handleShowNextCard}
+              disabled={slideIdx === endSlideIdx}
+            >
+              <span className="invisible">다음 프로모션</span>
+            </button>
+          </div>
+
+          <div className="card-container" aria-label="리스트 목록">
+            <ul className="cards">
+              <PromotionListItem
+                img="https://www.koreanair.com/content/dam/koreanair/ko/airport-img/DXB-list-pc.jpg"
+                departure="서울"
+                arrival="두바이"
+                seat="일반석 왕복"
+                price="KRW 1,157,500 ~"
+              />
+              <PromotionListItem
+                img="https://www.koreanair.com/content/dam/koreanair/ko/airport-img/FUK-list-pc.jpg"
+                departure="서울/인천"
+                arrival="후쿠오카"
+                seat="일반석 왕복"
+                price="KRW 340,200 ~"
+              />
+              <PromotionListItem
+                img="https://www.koreanair.com/content/dam/koreanair/ko/airport-img/HKT-list-pc.jpg"
+                departure="서울/인천"
+                arrival="푸껫"
+                seat="일반석 왕복"
+                price="KRW 704,100 ~"
+              />
+              <PromotionListItem
+                img="https://www.koreanair.com/content/dam/koreanair/ko/airport-img/CNX-list-pc.jpg"
+                departure="서울/인천"
+                arrival="치앙마이"
+                seat="일반석 왕복"
+                price="KRW 839,100 ~"
+              />
+              <PromotionListItem
+                img="https://www.koreanair.com/content/dam/koreanair/ko/airport-img/BCN-list-pc.jpg"
+                departure="서울/인천"
+                arrival="바르셀로나"
+                seat="일반석 왕복"
+                price="KRW 1,546,300 ~"
+              />
+              <PromotionListItem
+                img="https://www.koreanair.com/content/dam/koreanair/ko/airport-img/HAN-list-pc.jpg"
+                departure="서울/인천"
+                arrival="하노이"
+                seat="일반석 왕복"
+                price="KRW 486,900 ~"
+              />
+              <PromotionListItem
+                img="https://www.koreanair.com/content/dam/koreanair/ko/airport-img/FCO-list-pc.jpg"
+                departure="서울/인천"
+                arrival="로마/레오나르도"
+                seat="일반석 왕복"
+                price="KRW 1,454,200 ~"
+              />
+              <PromotionListItem
+                img="https://www.koreanair.com/content/dam/koreanair/ko/airport-img/HNL-list-pc.jpg"
+                departure="서울/인천"
+                arrival="호놀룰루 (하와이)"
+                seat="일반석 왕복"
+                price="KRW 1,243,800 ~"
+              />
+            </ul>
+          </div>
+        </section>
       </div>
-    </div>
+      <p id="title" className="invisible">
+        현재 떠날 수 있는 여행지 프로모션 목록입니다.
+      </p>
+    </>
   );
 }
 
