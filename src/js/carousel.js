d975ce65fa3cee6001848be5c574fbfeeeab5e23
Ahn <tn3748@naver.com>
2022-10-12 18:55:33 +0900

    feat: carousel 슬라이드 로직 추가

---
+++ src/js/carousel.js
@@ -0,0 +1,42 @@
+import { $, $$ } from "./dom.js";
+
+let itemIndex = 0;
+
+const transformItemList = () => {
+  $$(".item").forEach((item) => {
+    item.animate([{ transform: `translateX(calc((-270px) * ${itemIndex}))` }], {
+      duration: 300,
+      fill: "forwards",
+    });
+  });
+};
+
+$(".prev-button").addEventListener("click", () => {
+  if (itemIndex === 0) return;
+
+  if (itemIndex === 1) $(".prev-button").ariaDisabled = "true";
+  if (itemIndex === 6) $(".next-button").ariaDisabled = "false";
+
+  itemIndex -= 1;
+  transformItemList();
+});
+
+$(".next-button").addEventListener("click", () => {
+  if (itemIndex === 6) return;
+
+  if (itemIndex === 5) $(".next-button").ariaDisabled = "true";
+  if (itemIndex === 0) $(".prev-button").ariaDisabled = "false";
+
+  itemIndex += 1;
+  transformItemList();
+});
+
+$$(".item-link").forEach((item) => {
+  item.addEventListener("focus", () => {
+    const itemId = Number(item.closest("li").dataset.id);
+    if (itemId === 7) return;
+
+    itemIndex = itemId;
+    if (itemId > 0) transformItemList();
+  });
+});
