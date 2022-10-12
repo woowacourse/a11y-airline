b01cb5bff4e2fa731145c98c5a61d47b4d9d2dba
InKyoJeong <viapolar@naver.com>
2022-10-12 19:23:23 +0900

    feat: 사진 설명 추가

--- src/carousel.js
+++ src/carousel.js
@@ -1,7 +1,7 @@
 const slides = document.querySelectorAll(".slide");
-const slideLinks = document.querySelectorAll(".slide__link");
-const btnLeft = document.querySelector(".slider__btn--left");
-const btnRight = document.querySelector(".slider__btn--right");
+const slideLinks = document.querySelectorAll(".slide-link");
+const btnLeft = document.querySelector(".slider-btn__left");
+const btnRight = document.querySelector(".slider-btn__right");
 
 let curSlide = 0;
 const maxSlide = slides.length - 1;
