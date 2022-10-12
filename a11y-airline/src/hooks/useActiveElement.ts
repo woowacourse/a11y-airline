a6aa504fcf3e4c0ef91faa2665cf30360716af0b
2019015414 <2019015414@hanyang.ac.kr>
2022-10-12 11:23:48 +0900

    feat: useActiveElement hook에 dependencyArray 추가

--- a11y-airline/src/hooks/useActiveElement.ts
+++ a11y-airline/src/hooks/useActiveElement.ts
@@ -1,21 +1,21 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 const useActiveElement = (selectors: string) => {
   const [active, setActive] = useState(document.activeElement);
 
-  const handleFocusIn = () => {
+  const handleFocusIn = useCallback(() => {
     const focusElement = document.activeElement?.closest(selectors);
     if (focusElement) {
       setActive(focusElement);
     }
-  };
+  }, [selectors]);
 
   useEffect(() => {
     document.addEventListener("focusin", handleFocusIn);
     return () => {
       document.removeEventListener("focusin", handleFocusIn);
     };
-  }, []);
+  }, [handleFocusIn]);
 
   return active;
 };
