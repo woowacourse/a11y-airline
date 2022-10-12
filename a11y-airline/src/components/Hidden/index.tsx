f557689593a4923d4521121d5858416a14d022fa
2019015414 <2019015414@hanyang.ac.kr>
2022-10-12 10:36:22 +0900

    feat: Hidden component 분리

---
+++ a11y-airline/src/components/Hidden/index.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Hidden = styled.p`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  clip: rect(0 0 0 0);
+  margin: -1px;
+  overflow: hidden;
+`;
