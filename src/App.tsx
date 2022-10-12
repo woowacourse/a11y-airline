da2f19bf419a1ed13710e05e27a5d2103b1d8daf
JASUN LEE <liswktjs@gmail.com>
2022-10-13 01:02:59 +0900

    feat: Home에서 페이지 이동하는 링크 분리

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ const App = () => {
 	return (
 		<main>
 			<section>
-				<a href={URL.INSERTING_PASSENGER}>승객수 입력하기 페이지로 이동하기</a>
+				<div>
+					<a href={URL.INSERTING_PASSENGER}>승객수 입력하기 페이지로 이동하기</a>
+				</div>
 				<a href={URL.CAROUSEL_TRAVEL}>carousel 페이지로 이동하기</a>
 			</section>
 		</main>
