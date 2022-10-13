83a779a4026b5ccec3d7afe47fabb6472d0052ca
kamwoo <rkadndud06@naver.com>
2022-10-13 16:14:42 +0900

    feat: dummy data 수정

--- src/data/dummy.ts
+++ src/data/dummy.ts
@@ -13,46 +13,45 @@ export const infoList: CardInfo[] = [
   {
     id: 2,
     departure: '서울/인천',
-    arrival: '두바이',
+    arrival: '로스엔젤레스',
     type: '일반석 왕복',
-    price: 1121600,
+    price: 1721600,
     imageLink:
-      'https://res.klook.com/image/upload/c_crop,g_custom/w_1160,h_652/v1540285601/activities/gdf3czrtb4xx4qhfzvez.webp',
+      'https://d2ur7st6jjikze.cloudfront.net/offer_photos/32949/206415_large_1521658778.jpg?1521658778',
   },
   {
     id: 3,
     departure: '서울/인천',
-    arrival: '두바이',
+    arrival: '호롤룰루',
     type: '일반석 왕복',
-    price: 1121600,
+    price: 1341600,
     imageLink:
-      'https://res.klook.com/image/upload/c_crop,g_custom/w_1160,h_652/v1540285601/activities/gdf3czrtb4xx4qhfzvez.webp',
+      'https://media.triple.guide/triple-cms/c_limit,f_auto,h_1024,w_1024/1bed56a8-e298-4c8d-a2eb-3c8307efe904.jpeg',
   },
   {
     id: 4,
     departure: '서울/인천',
-    arrival: '두바이',
+    arrival: '괌',
     type: '일반석 왕복',
-    price: 1121600,
+    price: 1241600,
     imageLink:
-      'https://res.klook.com/image/upload/c_crop,g_custom/w_1160,h_652/v1540285601/activities/gdf3czrtb4xx4qhfzvez.webp',
+      'https://www.lottehotelmagazine.com/resources/4ceaf51f-8225-435b-b233-d79ecc5dc68d_img_travel_guam_detail01.jpg',
   },
   {
     id: 5,
     departure: '서울/인천',
-    arrival: '두바이',
+    arrival: '스위스',
     type: '일반석 왕복',
     price: 1121600,
     imageLink:
-      'https://res.klook.com/image/upload/c_crop,g_custom/w_1160,h_652/v1540285601/activities/gdf3czrtb4xx4qhfzvez.webp',
+      'https://post-phinf.pstatic.net/MjAxOTAyMjZfMTU0/MDAxNTUxMTQ4NjY0MjE4.7napanJWu5a44xN90w46MyB6nh_YBhssOg9B3XbgJRYg.YPWrrB5OkxxSZbCmKw-cbxZREVPBZUnOWSPAiPktM7gg.JPEG/%EC%8A%A4%EC%9C%84%EC%8A%A4%EC%97%AC%ED%96%89%EC%BD%94%EC%8A%A43.jpg?type=w1200',
   },
   {
     id: 6,
     departure: '서울/인천',
-    arrival: '두바이',
+    arrival: '영국',
     type: '일반석 왕복',
-    price: 1121600,
-    imageLink:
-      'https://res.klook.com/image/upload/c_crop,g_custom/w_1160,h_652/v1540285601/activities/gdf3czrtb4xx4qhfzvez.webp',
+    price: 2121600,
+    imageLink: 'http://www.hellotravel.kr/data/local/list/150753123438.jpg',
   },
 ];
