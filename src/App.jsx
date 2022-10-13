78105b5f1206557806809aa55d2ba4b8ef117456
KIM YEJI <rladpwl0512@gmail.com>
2022-10-13 11:28:55 +0900

    refactor: 페이지 들어갔을 때 보이는 컴포넌트 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import PassengerCountInput from "./pages/PassengerCountInput/PassengerCountInput";
 import PromotionList from "./pages/PromotionList/PromotionList";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="a11y-airline">
-          <Route path="step1" element={<PassengerCountInput />} />
-          <Route path="step2" element={<PromotionList />} />
-        </Route>
-      </Routes>
-    </Router>
-  );
+  return <PromotionList />;
 }
 
 export default App;
