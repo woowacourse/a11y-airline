bd8e7294def248fa75ddcc83c084a3dd8cfdd5d2
airman5573 <rpf5573@kakao.com>
2022-10-13 16:13:05 +0900

    feat: crousel 개선

--- src/airline-product.js
+++ src/airline-product.js
@@ -1,22 +1,23 @@
-import debounce from './debounce';
-
 document.addEventListener('DOMContentLoaded', () => {
   const $carousel = document.querySelector('.carousel');
+  const $carouselInner = $carousel.querySelector('.carousel-inner');
+  const itemCount = $carousel.querySelectorAll('li').length / 2; // @TODO: 홀수 처리 필요
   const $prevBtn = $carousel.querySelector('.carousel-control.left');
   const $nextBtn = $carousel.querySelector('.carousel-control.right');
-
-  const debouncer = debounce(200);
+  let currentIndex = 0;
 
   $prevBtn.addEventListener('click', () => {
     const width = $carousel.clientWidth;
-    debouncer(() => {
-      $carousel.scrollBy({ left: -1 * width, behavior: 'smooth' });
-    });
+    currentIndex = -1 * Math.max(0, currentIndex - 1);
+    const nextX = currentIndex * width;
+    const translate = `translateX(${nextX}px)`;
+    $carouselInner.style.transform = translate;
   });
   $nextBtn.addEventListener('click', () => {
     const width = $carousel.clientWidth;
-    debouncer(() => {
-      $carousel.scrollBy({ left: width, behavior: 'smooth' });
-    });
+    currentIndex = Math.min(itemCount - 1, currentIndex + 1);
+    const nextX = -1 * currentIndex * width;
+    const translate = `translateX(${nextX}px)`;
+    $carouselInner.style.transform = translate;
   });
 });
