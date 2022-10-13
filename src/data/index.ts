4745b357723c3f0634ee91ccf71ecdacb989dc71
jhy979 <jhy979@gmail.com>
2022-10-13 09:48:16 +0900

    chore: 데이터 변경

--- src/data/index.ts
+++ src/data/index.ts
@@ -4,8 +4,7 @@ export const data = [
     location: '서울/인천 - 두바이',
     seat: '일반석 왕복',
     price: 1158000,
-    image:
-      'https://wttc.org/DesktopModules/MVC/NewsArticleList/images/141_20201013185512_Consumer%20Survey%20Finds%2070%20Percent%20of%20Travelers%20Plan%20to%20Holiday%20in%202021.jpg',
+    image: 'https://www.koreanair.com/content/dam/koreanair/ko/airport-img/DXB-list-pc.jpg',
     href: 'https://www.koreanair.com/booking/best-prices?departureCode=ICN&destinationCode=DXB&duration=7&cabin=Y',
   },
   {
@@ -13,8 +12,7 @@ export const data = [
     location: '서울/인천 - 후쿠오카',
     seat: '일반석 왕복',
     price: 340400,
-    image:
-      'https://wttc.org/DesktopModules/MVC/NewsArticleList/images/141_20201013185512_Consumer%20Survey%20Finds%2070%20Percent%20of%20Travelers%20Plan%20to%20Holiday%20in%202021.jpg',
+    image: 'https://www.koreanair.com/content/dam/koreanair/ko/airport-img/FUK-list-pc.jpg',
     href: 'https://www.koreanair.com/booking/best-prices?departureCode=ICN&destinationCode=FUK&duration=7&cabin=Y',
   },
   {
@@ -22,8 +20,7 @@ export const data = [
     location: '서울/인천 - 푸껫',
     seat: '일반석 왕복',
     price: 704200,
-    image:
-      'https://wttc.org/DesktopModules/MVC/NewsArticleList/images/141_20201013185512_Consumer%20Survey%20Finds%2070%20Percent%20of%20Travelers%20Plan%20to%20Holiday%20in%202021.jpg',
+    image: 'https://www.koreanair.com/content/dam/koreanair/ko/airport-img/HKT-list-pc.jpg',
     href: 'https://www.koreanair.com/booking/best-prices?departureCode=ICN&destinationCode=HKT&cabin=Y&tripType=RT&duration=7',
   },
   {
@@ -31,8 +28,7 @@ export const data = [
     location: '서울/인천 - 치앙마이',
     seat: '일반석 왕복',
     price: 839200,
-    image:
-      'https://wttc.org/DesktopModules/MVC/NewsArticleList/images/141_20201013185512_Consumer%20Survey%20Finds%2070%20Percent%20of%20Travelers%20Plan%20to%20Holiday%20in%202021.jpg',
+    image: 'https://www.koreanair.com/content/dam/koreanair/ko/airport-img/CNX-list-pc.jpg',
     href: 'https://www.koreanair.com/booking/best-prices?departureCode=ICN&destinationCode=CNX&cabin=Y&tripType=RT&duration=7',
   },
   {
@@ -40,8 +36,7 @@ export const data = [
     location: '서울/인천 - 바르셀로나',
     seat: '일반석 왕복',
     price: 1546300,
-    image:
-      'https://wttc.org/DesktopModules/MVC/NewsArticleList/images/141_20201013185512_Consumer%20Survey%20Finds%2070%20Percent%20of%20Travelers%20Plan%20to%20Holiday%20in%202021.jpg',
+    image: 'https://www.koreanair.com/content/dam/koreanair/ko/airport-img/BCN-list-pc.jpg',
     href: 'https://www.koreanair.com/booking/best-prices?departureCode=ICN&destinationCode=BCN&cabin=Y&tripType=RT&duration=7',
   },
   {
@@ -49,8 +44,7 @@ export const data = [
     location: '서울/인천 - 하노이',
     seat: '일반석 왕복',
     price: 527400,
-    image:
-      'https://wttc.org/DesktopModules/MVC/NewsArticleList/images/141_20201013185512_Consumer%20Survey%20Finds%2070%20Percent%20of%20Travelers%20Plan%20to%20Holiday%20in%202021.jpg',
+    image: 'https://www.koreanair.com/content/dam/koreanair/ko/airport-img/HAN-list-pc.jpg',
     href: 'https://www.koreanair.com/booking/best-prices?departureCode=ICN&destinationCode=HAN&cabin=Y&tripType=RT&duration=7',
   },
   {
@@ -58,8 +52,7 @@ export const data = [
     location: '서울/인천 - 로마/레오나르도 다빈치',
     seat: '일반석 왕복',
     price: 1454300,
-    image:
-      'https://wttc.org/DesktopModules/MVC/NewsArticleList/images/141_20201013185512_Consumer%20Survey%20Finds%2070%20Percent%20of%20Travelers%20Plan%20to%20Holiday%20in%202021.jpg',
+    image: 'https://www.koreanair.com/content/dam/koreanair/ko/airport-img/FCO-list-pc.jpg',
     href: 'https://www.koreanair.com/booking/best-prices?departureCode=ICN&destinationCode=HNL&cabin=Y&tripType=RT&duration=7',
   },
   {
@@ -67,8 +60,7 @@ export const data = [
     location: '서울/인천 - 호놀룰루(하와이)',
     seat: '일반석 왕복',
     price: 1244900,
-    image:
-      'https://wttc.org/DesktopModules/MVC/NewsArticleList/images/141_20201013185512_Consumer%20Survey%20Finds%2070%20Percent%20of%20Travelers%20Plan%20to%20Holiday%20in%202021.jpg',
+    image: 'https://www.koreanair.com/content/dam/koreanair/ko/airport-img/HNL-list-pc.jpg',
     href: 'https://www.koreanair.com/booking/best-prices?departureCode=ICN&destinationCode=HNL&cabin=Y&tripType=RT&duration=7',
   },
 ];
