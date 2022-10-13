61f2c69e412d6390f7dc2a7f67a52eef27be72fd
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-10-13 16:59:31 +0900

    feat: Carousel 접근성 향상

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -1,56 +1,7 @@
 import { useRef, useState } from "react";
+import { travels } from "./travels";
+import HiddenButAriaRead from "../HiddenButAriaRead";
 import "./carousel.css";
-
-const travels = [
-  {
-    id: 1,
-    alt: "서울 인천 치앙마이",
-    src: "https://www.koreanair.com/content/dam/koreanair/ko/airport-img/DXB-list-pc.jpg",
-    location: "서울/인천 치앙마이",
-    seat: "일반석 왕복",
-    price: 839200,
-  },
-  {
-    id: 2,
-    alt: "서울 인천 바르셀로나",
-    src: "https://www.koreanair.com/content/dam/koreanair/ko/airport-img/HAN-list-pc.jpg",
-    location: "서울/인천 바르셀로나",
-    seat: "일반석 왕복",
-    price: 1546300,
-  },
-  {
-    id: 3,
-    alt: "서울 인천 하노이",
-    src: "https://www.koreanair.com/content/dam/koreanair/ko/airport-img/FUK-list-pc.jpg",
-    location: "서울/인천 하노이",
-    seat: "일반석 왕복",
-    price: 1546300,
-  },
-  {
-    id: 4,
-    alt: "서울 인천 푸껫",
-    src: "https://www.koreanair.com/content/dam/koreanair/ko/airport-img/HKT-list-pc.jpg",
-    location: "서울/인천 푸껫",
-    seat: "일반석 왕복",
-    price: 704200,
-  },
-  {
-    id: 5,
-    alt: "서울 인천 호놀룰루",
-    src: "https://www.koreanair.com/content/dam/koreanair/ko/airport-img/CNX-list-pc.jpg",
-    location: "서울/인천 호놀룰루",
-    seat: "일반석 왕복",
-    price: 1824600,
-  },
-  {
-    id: 6,
-    alt: "서울 인천 후쿠오카",
-    src: "https://www.koreanair.com/content/dam/koreanair/ko/airport-img/BCN-list-pc.jpg",
-    location: "서울/인천 후쿠오카",
-    seat: "일반석 왕복",
-    price: 1481800,
-  },
-];
 
 const TRANSLATE_X = 300;
 const DEBOUNCE_TIME = 100;
@@ -58,6 +9,7 @@ const DEBOUNCE_TIME = 100;
 const Carousel: React.FC = () => {
   const cardListRef = useRef<HTMLUListElement | null>(null);
   const debounceRef = useRef<NodeJS.Timeout | null>(null);
+
   const [scrollPos, setScrollPos] = useState<"start" | "end" | null>("start");
 
   const handleLeftSlideButtonClick = () => {
@@ -104,7 +56,7 @@ const Carousel: React.FC = () => {
   };
 
   return (
-    <div>
+    <section>
       <h2>지금 떠나기 좋은 여행</h2>
       <section className="carousel-section">
         <CarouselButton
@@ -112,16 +64,21 @@ const Carousel: React.FC = () => {
           disabled={scrollPos === "start"}
           onClick={handleLeftSlideButtonClick}
         />
+        <CarouselButton
+          direction="right"
+          disabled={scrollPos === "end"}
+          onClick={handleRightSlideButtonClick}
+        />
         <ul
           className="card-list"
           ref={cardListRef}
           onScroll={handleCardListScroll}
         >
+          <HiddenButAriaRead>{`추천 여행 목록 ${travels.length}개 포함`}</HiddenButAriaRead>
           {travels.map((travel) => (
             <li key={travel.id}>
               <a className="card-link" href=".">
                 <TravelCard
-                  alt={travel.alt}
                   src={travel.src}
                   location={travel.location}
                   seat={travel.seat}
@@ -131,40 +88,40 @@ const Carousel: React.FC = () => {
             </li>
           ))}
         </ul>
-        <CarouselButton
-          direction="right"
-          disabled={scrollPos === "end"}
-          onClick={handleRightSlideButtonClick}
-        />
       </section>
-    </div>
+    </section>
   );
 };
 
 export default Carousel;
 
 type CardProps = {
-  alt: string;
   src: string;
-  location: string;
+  location: {
+    departure: string;
+    arrival: string;
+  };
   seat: string;
   price: number;
 };
 
-const TravelCard: React.FC<CardProps> = ({
-  alt,
-  src,
-  location,
-  seat,
-  price,
-}) => {
+const TravelCard: React.FC<CardProps> = ({ src, location, seat, price }) => {
   return (
     <div className="card">
-      <img className="card-image" alt={alt} src={src} />
+      <img className="card-image" alt="" src={src} />
       <div className="card-description">
-        <p className="location">{location}</p>
+        <p className="location">
+          <span>{location.departure}</span>
+          <span aria-hidden> - </span>
+          <span>{location.arrival}</span>
+        </p>
         <p className="seat">{seat}</p>
-        <span className="price">KRW {price.toLocaleString()} ~</span>
+        <span className="price">
+          <span aria-hidden>KRW {price.toLocaleString()} ~</span>
+          <HiddenButAriaRead>
+            {`${price.toLocaleString()} 대한민국 원`}
+          </HiddenButAriaRead>
+        </span>
       </div>
     </div>
   );
@@ -186,6 +143,7 @@ const CarouselButton: React.FC<CarouselButtonProps> = ({
       className={`carousel-button ${direction}`}
       type="button"
       disabled={disabled}
+      aria-disabled={disabled}
       aria-label={direction === "right" ? "다음" : "이전"}
       onClick={handleClick}
     >
---
+++ src/components/carousel/travels.ts
@@ -0,0 +1,82 @@
+export const travels = [
+  {
+    id: 1,
+    src: "https://www.koreanair.com/content/dam/koreanair/ko/airport-img/DXB-list-pc.jpg",
+    location: {
+      departure: "서울/인천",
+      arrival: "치앙마이",
+    },
+    seat: "일반석 왕복",
+    price: 839200,
+  },
+  {
+    id: 2,
+    src: "https://www.koreanair.com/content/dam/koreanair/ko/airport-img/HAN-list-pc.jpg",
+    location: {
+      departure: "서울/인천",
+      arrival: "바르셀로나",
+    },
+    seat: "일반석 왕복",
+    price: 1546300,
+  },
+  {
+    id: 3,
+    src: "https://www.koreanair.com/content/dam/koreanair/ko/airport-img/FUK-list-pc.jpg",
+    location: {
+      departure: "서울/인천",
+      arrival: "하노이",
+    },
+    seat: "일반석 왕복",
+    price: 1546300,
+  },
+  {
+    id: 4,
+    src: "https://www.koreanair.com/content/dam/koreanair/ko/airport-img/HKT-list-pc.jpg",
+    location: {
+      departure: "서울/인천",
+      arrival: "푸껫",
+    },
+    seat: "일반석 왕복",
+    price: 704200,
+  },
+  {
+    id: 5,
+    src: "https://www.koreanair.com/content/dam/koreanair/ko/airport-img/CNX-list-pc.jpg",
+    location: {
+      departure: "서울/인천",
+      arrival: "호놀룰루",
+    },
+    seat: "일반석 왕복",
+    price: 1824600,
+  },
+  {
+    id: 6,
+    src: "https://www.koreanair.com/content/dam/koreanair/ko/airport-img/BCN-list-pc.jpg",
+    location: {
+      departure: "서울/인천",
+      arrival: "후쿠오카",
+    },
+    seat: "일반석 왕복",
+    price: 1481800,
+  },
+  {
+    id: 7,
+    src: "https://www.koreanair.com/content/dam/koreanair/ko/airport-img/DXB-list-pc.jpg",
+    location: {
+      departure: "서울/인천",
+      arrival: "후쿠오카",
+    },
+    seat: "일반석 왕복",
+    price: 1481800,
+  },
+  {
+    id: 8,
+    src: "https://www.koreanair.com/content/dam/koreanair/ko/airport-img/HKT-list-pc.jpg",
+    location: {
+      departure: "서울/인천",
+      arrival: "후쿠오카",
+    },
+    seat: "일반석 왕복",
+    price: 1481800,
+  },
+];
