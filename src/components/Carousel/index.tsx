5a673155fb0586c0a3fab6454130205a13852213
kwannee <yleh1126@naver.com>
2022-10-13 15:58:02 +0900

    feat: next,prev 버튼에 aria-disabled 추가

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -6,6 +6,10 @@ import { ReactNode } from "react";
 
 const Carousel = ({ children }: { children: ReactNode[] }) => {
   const carouselRef = useRef<HTMLUListElement>(null);
+  const [movable, setMovable] = useState({
+    prev: false,
+    next: true,
+  });
 
   function scrollToNextPage() {
     carouselRef.current?.scrollBy({ left: 1, top: 0, behavior: "smooth" });
@@ -14,15 +18,42 @@ const Carousel = ({ children }: { children: ReactNode[] }) => {
     carouselRef.current?.scrollBy({ left: -1, top: 0, behavior: "smooth" });
   }
 
+  const handleXScrollMove = () => {
+    const { scrollLeft, scrollWidth, offsetWidth } = carouselRef.current!;
+
+    setMovable({
+      prev: scrollLeft !== 0,
+      next: scrollWidth !== scrollLeft + offsetWidth,
+    });
+  };
+
+  useEffect(() => {
+    carouselRef.current?.addEventListener("scroll", handleXScrollMove);
+
+    return () => {
+      carouselRef.current?.removeEventListener("scroll", handleXScrollMove);
+    };
+  }, [carouselRef.current]);
+
   return (
     <S.Slider>
       <S.Carousel ref={carouselRef} aria-label="항공권들">
         {children}
       </S.Carousel>
-      <S.PrevButton type="button" onClick={scrollToPrevPage} aria-label="이전">
+      <S.PrevButton
+        type="button"
+        onClick={scrollToPrevPage}
+        aria-label="이전"
+        aria-disabled={!movable.prev}
+      >
         <ArrowLeft />
       </S.PrevButton>
-      <S.NextButton type="button" onClick={scrollToNextPage} aria-label="다음">
+      <S.NextButton
+        type="button"
+        onClick={scrollToNextPage}
+        aria-label="다음"
+        aria-disabled={!movable.next}
+      >
         <ArrowRight />
       </S.NextButton>
     </S.Slider>
@@ -87,9 +118,6 @@ const S = {
     top: 50%;
     transform: translateY(-50%);
     right: 0;
-    [aria-disabled="true"] {
-      cursor: not-allowed;
-    }
   `,
   PrevButton: styled.button`
     z-index: 100000;
@@ -97,8 +125,5 @@ const S = {
     top: 50%;
     transform: translateY(-50%);
     left: 0;
-    :disabled {
-      cursor: not-allowed;
-    }
   `,
 };
