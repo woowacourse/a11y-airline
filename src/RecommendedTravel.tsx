1a4cb239331fb263435494ee0b38e5f55ed980b0
gytks4 <gytks4@naver.com>
2022-10-13 16:14:59 +0900

    feat: h2에 tab index 부여

--- src/RecommendedTravel.tsx
+++ src/RecommendedTravel.tsx
@@ -3,7 +3,7 @@ import TravelCarousel from './Carousel';
 const RecommendedTravel = () => {
   return (
     <section>
-      <h2>지금 떠나기 좋은 여행</h2>
+      <h2 tabIndex={0}>지금 떠나기 좋은 여행</h2>
       <TravelCarousel />
     </section>
   );
