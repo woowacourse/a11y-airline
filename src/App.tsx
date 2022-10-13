9fd699d67186dd56cd2bd4924c8dc036024b380f
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-10-13 15:59:31 +0900

    feat : App 스탭 1, 2 페이지 구분

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,48 @@
-import Passengers from 'pages/Passengers'
+import cn from 'classnames'
+
+import FlexContainer from 'components/FlexContainer'
+import CarouselPage from 'pages/CarouselPage'
+import PassengersPage from 'pages/PassengersPage'
+import { useState } from 'react'
 
 import 'styles/global.scss'
+import styles from 'styles/app.module.scss'
+
+type StepList = 1 | 2
 
 function App() {
-  return <Passengers />
+  const [step, setStep] = useState<StepList>(2)
+
+  const handleChangeStep = (step: StepList) => () => {
+    setStep(step)
+  }
+
+  return (
+    <>
+      <FlexContainer className={styles.header} direction="row" justify="center" gap="large">
+        <button
+          type="button"
+          className={cn(styles.button, { [styles.focus]: step === 1 })}
+          onClick={handleChangeStep(1)}
+        >
+          <p>🦖</p>
+          <p>Step 1</p>
+        </button>
+
+        <button
+          type="button"
+          className={cn(styles.button, { [styles.focus]: step === 2 })}
+          onClick={handleChangeStep(2)}
+        >
+          <p>🦕</p>
+          <p>Step 2</p>
+        </button>
+      </FlexContainer>
+
+      {step === 1 && <PassengersPage />}
+      {step === 2 && <CarouselPage />}
+    </>
+  )
 }
 
 export default App
