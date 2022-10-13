0f81e592aa2a829ddc1eac742dbbe411ae3b90d5
kwannee <yleh1126@naver.com>
2022-10-13 14:51:27 +0900

    feat: carousel 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,120 +1,132 @@
-import { ChangeEvent, useState } from "react";
-import "./app.css";
-import QuestionMarkTooltip from "./components/Tooltip";
-
-const MAX_PASSENGER_COUNT = 3;
-const MIN_PASSENGER_COUNT = 1;
-
-const PASSENGER_CHANGE_MESSAGE = "성인 탑승 인원을 변경합니다. ";
-const PASSENGER_INCREASE_MESSAGE = "성인 탑승 인원 추가";
-const PASSENGER_DECREASE_MESSAGE = "성인 탑승 인원 감소";
-const PASSENGER_COUNT_INPUT_ALERT_MESSAGE = (count: number) =>
-  `승객 ${count}명`;
-
-const isValidPassengerCount = (count: number) => {
-  if (count < MIN_PASSENGER_COUNT) {
-    return false;
-  }
-
-  if (count > MAX_PASSENGER_COUNT) {
-    return false;
-  }
-
-  return true;
-};
+import { useRef } from "react";
+import { ReactComponent as ArrowLeft } from "./images/arrow_left.svg";
+import { ReactComponent as ArrowRight } from "./images/arrow_right.svg";
+import styled from "styled-components";
+import { TravelInformation } from "./data";
 
 const App = () => {
-  const [passengerCount, setPassengerCount] =
-    useState<number>(MIN_PASSENGER_COUNT);
-  const [message, setMessage] = useState<string>("");
+  const carouselRef = useRef<HTMLUListElement>(null);
 
-  const changePassengerCount = (e: ChangeEvent<HTMLInputElement>) => {
-    const count = e.target.valueAsNumber;
-
-    if (!isValidPassengerCount(count)) {
-      return;
-    }
-
-    setPassengerCount(count);
-    setMessage(
-      `${PASSENGER_CHANGE_MESSAGE} ${PASSENGER_COUNT_INPUT_ALERT_MESSAGE(
-        count
-      )}`
-    );
-  };
-
-  const increasePassengerCount = () => {
-    const currentPassengerCount = passengerCount;
-
-    if (!isValidPassengerCount(passengerCount + 1)) {
-      return;
-    }
-
-    setPassengerCount((prev) => prev + 1);
-    setMessage(
-      `${PASSENGER_INCREASE_MESSAGE}
-        ${PASSENGER_COUNT_INPUT_ALERT_MESSAGE(currentPassengerCount + 1)}`
-    );
-  };
-
-  const decreasePassengerCount = () => {
-    const currentPassengerCount = passengerCount;
-
-    if (!isValidPassengerCount(passengerCount - 1)) {
-      return;
-    }
-
-    setPassengerCount((prev) => prev - 1);
-    setMessage(
-      `${PASSENGER_DECREASE_MESSAGE}
-        ${PASSENGER_COUNT_INPUT_ALERT_MESSAGE(currentPassengerCount - 1)}`
-    );
-  };
+  function scrollToNextPage() {
+    carouselRef.current?.scrollBy({ left: 312, top: 0, behavior: "smooth" });
+  }
+  function scrollToPrevPage() {
+    carouselRef.current?.scrollBy({ left: -312, top: 0, behavior: "smooth" });
+  }
 
   return (
     <main>
-      <h1>웹 접근성 좋은 항공사</h1>
-      <h2>승객 선택</h2>
-      <section className="passenger-count-control-container">
-        <div className="flex g7 align-center">
-          <label htmlFor="passenger-count-input">성인</label>
-          <QuestionMarkTooltip description="승객은 최소 1명 최대 3명입니다." />
-        </div>
-        <div className="passenger-count-controller">
-          <button
-            className="passenger-count-control-button"
+      <h2>지금 떠나기 좋은 여행</h2>
+      <S.Container>
+        <S.Slider>
+          <S.Carousel ref={carouselRef}>
+            {TravelInformation.map(
+              ({ route, href, id, image, price, seat }) => (
+                <li key={id}>
+                  <a href={href}>
+                    <S.TravelItem>
+                      <S.TravelItemDescription>
+                        <S.TravelItemRoute>{route}</S.TravelItemRoute>
+                        <S.TravelItemSeat>{seat}</S.TravelItemSeat>
+                        <S.TravelItemPrice>
+                          KRW {price.toLocaleString("ko-KR")} ~
+                        </S.TravelItemPrice>
+                      </S.TravelItemDescription>
+                      <S.TravelItemImage src={image} alt={route} />
+                    </S.TravelItem>
+                  </a>
+                </li>
+              )
+            )}
+          </S.Carousel>
+          <S.PrevButton
             type="button"
-            aria-disabled={passengerCount === MIN_PASSENGER_COUNT}
-            aria-label="성인 승객 한명 줄이기"
-            onClick={decreasePassengerCount}
+            onClick={scrollToPrevPage}
+            aria-label="이전"
+            aria-disabled={false}
           >
-            -
-          </button>
-          <input
-            className="passenger-count-control-input"
-            id="passenger-count-input"
-            type="number"
-            min={MIN_PASSENGER_COUNT}
-            max={MAX_PASSENGER_COUNT}
-            value={passengerCount}
-            onChange={changePassengerCount}
-          />
-          <button
-            className="passenger-count-control-button"
+            <ArrowLeft />
+          </S.PrevButton>
+          <S.NextButton
             type="button"
-            aria-disabled={passengerCount === MAX_PASSENGER_COUNT}
-            aria-label="성인 승객 한명 늘이기"
-            onClick={increasePassengerCount}
+            onClick={scrollToNextPage}
+            aria-label="다음"
+            aria-disabled={false}
           >
-            +
-          </button>
-        </div>
-        <div className="user-action-message-box" role="status">
-          {message}
-        </div>
-      </section>
+            <ArrowRight />
+          </S.NextButton>
+        </S.Slider>
+      </S.Container>
     </main>
   );
 };
 
 export default App;
+
+const S = {
+  NextButton: styled.button`
+    z-index: 100000;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    right: 0;
+  `,
+  PrevButton: styled.button`
+    z-index: 100000;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    left: 0;
+  `,
+  Container: styled.section`
+    position: relative;
+  `,
+  Slider: styled.section`
+    position: relative;
+  `,
+  Carousel: styled.ul`
+    display: grid;
+    grid-auto-flow: column;
+    scroll-behavior: auto;
+    gap: 1rem;
+    overflow-y: auto;
+    overscroll-behavior-x: contain;
+    scroll-snap-type: x mandatory;
+    scrollbar-width: none;
+    ::-webkit-scrollbar {
+      display: none;
+    }
+  `,
+  TravelItem: styled.section`
+    position: relative;
+  `,
+  TravelItemDescription: styled.section`
+    display: flex;
+    flex-flow: column;
+    position: absolute;
+    top: 10%;
+    left: 10%;
+    color: black;
+  `,
+  TravelItemImage: styled.img`
+    scroll-snap-align: start;
+    display: flex;
+    align-items: center;
+    max-height: 300px;
+    justify-content: center;
+  `,
+  TravelItemRoute: styled.span`
+    font-size: 1rem;
+    font-weight: bold;
+    margin-bottom: 7px;
+  `,
+  TravelItemSeat: styled.span`
+    color: #242424;
+    font-size: 0.8rem;
+  `,
+  TravelItemPrice: styled.span`
+    color: #11277b;
+    font-size: 1rem;
+    font-weight: bold;
+  `,
+};
