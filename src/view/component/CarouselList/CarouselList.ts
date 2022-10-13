a914405eb61462e5a39cf50cebb83d93aab8b563
JUDONGHYEOK <judonghyeok@gmail.com>
2022-10-13 11:38:34 +0900

    refactor: ariaLabel 템플릿 리터럴 사용

--- src/view/component/CarouselList/CarouselList.ts
+++ src/view/component/CarouselList/CarouselList.ts
@@ -14,25 +14,30 @@ export default class CarouselList extends Component {
   #itemList;
 
   constructor() {
-    super();
+    super('div', 'carousel__list__container');
     this.#itemList = ITEM_LIST.map((item) => new CarouselItem(item).target);
 
     this.#listContainer = createElement('ul', 'carousel__list');
-    this.#listContainer.ariaLabel =
-      '목록 8개 항목 포함 서울/인천 로스앤젤레스 일반석 왕복 1,481,800 대한민국 원 링크 목록 항목';
+    this.#listContainer.ariaLabel = `목록 ${ITEM_LIST.length}개 항목 포함 ${
+      ITEM_LIST[0]?.startingPoint
+    } ${
+      ITEM_LIST[0]?.departure
+    } 일반석 왕복 ${ITEM_LIST[0]?.price.toLocaleString(
+      'ko-KR'
+    )} 대한민국 원 목록`;
     this.#listContainer.append(...this.#itemList);
 
     this.#minusButton = new CarouselButton({
       id: 'minus__button',
       onClick: () => {
-        this.element.scrollLeft -= this.#itemList[0].clientWidth;
+        this.element.scrollLeft -= this.#itemList[0]?.clientWidth;
       },
       ariaLabel: '이전',
     });
     this.#plusButton = new CarouselButton({
       id: 'plus__button',
       onClick: () => {
-        this.element.scrollLeft += this.#itemList[0].clientWidth;
+        this.element.scrollLeft += this.#itemList[0]?.clientWidth;
       },
       ariaLabel: '다음',
     });
@@ -43,7 +48,6 @@ export default class CarouselList extends Component {
       this.#plusButton.target
     );
 
-    this.element.classList.add('carousel__list__container');
     this.element.append(this.#listContainer, this.#buttonContainer);
     this.element.addEventListener('scroll', this.handleScroll);
   }
@@ -51,7 +55,7 @@ export default class CarouselList extends Component {
   handleScroll = () => {
     this.#plusButton.disabled =
       this.element.scrollLeft >
-      this.#itemList[0].clientWidth * (this.#itemList.length - 2);
+      this.#itemList[0]?.clientWidth * (this.#itemList.length - 2);
     this.#minusButton.disabled = this.element.scrollLeft === 0;
   };
 }
