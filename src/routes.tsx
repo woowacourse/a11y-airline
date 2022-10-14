b128f0c9f40560bf6ffbae4cad7ff338b3050cc2
kkojae <jaejeung3210@gmail.com>
2022-10-14 20:46:06 +0900

    chore: router 설정 및 home page 연결

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,19 @@
+import Carousel from "./pages/Carousel";
+import Home from "./pages/Home";
 import Passenger from "./pages/Passenger";
 
 const routes = [
   {
+    path: "/",
+    element: <Home />,
+  },
+  {
     path: "/passenger",
     element: <Passenger />,
+  },
+  {
+    path: "/carousel",
+    element: <Carousel />,
   },
 ];
 
