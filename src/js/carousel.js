ad302b9d6e9ce7060cfda8882b920771eee1d8b4
Ahn <tn3748@naver.com>
2022-10-17 13:33:50 +0900

    feat: 버튼 상태 메시지 추가

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -3,7 +3,7 @@ import { $, $$ } from "./dom.js";
 let itemIndex = 0;
 
 const transformItemList = () => {
-  let translateSize = window.innerWidth > 400 ? -270 : -210;
+  const translateSize = window.innerWidth > 400 ? -270 : -210;
 
   $$(".item").forEach((item) => {
     item.animate(
@@ -17,8 +17,10 @@ const transformItemList = () => {
 };
 
 $(".prev-button").addEventListener("click", () => {
-  if (itemIndex === 0) return;
-
+  if (itemIndex === 0) {
+    $("#button-status").textContent = "처음 목록입니다.";
+    return;
+  }
   if (itemIndex === 1) $(".prev-button").ariaDisabled = "true";
   if (itemIndex === 6) $(".next-button").ariaDisabled = "false";
 
@@ -27,8 +29,10 @@ $(".prev-button").addEventListener("click", () => {
 });
 
 $(".next-button").addEventListener("click", () => {
-  if (itemIndex === 6) return;
-
+  if (itemIndex === 6) {
+    $("#button-status").textContent = "마지막 목록입니다.";
+    return;
+  }
   if (itemIndex === 5) $(".next-button").ariaDisabled = "true";
   if (itemIndex === 0) $(".prev-button").ariaDisabled = "false";
 
