bc2adf0c047877b3a04baed8f7e107489e30b292
Daye Lee <508yeah@gmail.com>
2022-10-17 21:55:56 +0900

    feat: carousel 이동 버튼 비활성화 기능 구현

--- src/pages/PromotionPage/PromotionPage.tsx
+++ src/pages/PromotionPage/PromotionPage.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 import './PromotionPage.css';
 
@@ -10,6 +10,24 @@ import PromotionCard from '../../components/PromotionCard/PromotionCard';
 
 function PromotionPage() {
   const promotionListRef = useRef<HTMLUListElement>(null);
+
+  const [buttonDisabled, setButtonDisabled] = useState({
+    prev: true,
+    next: false,
+  });
+
+  const handleScrollPromotionList = () => {
+    const promotionList = promotionListRef.current;
+
+    if (!(promotionList instanceof HTMLUListElement)) return;
+
+    setButtonDisabled({
+      prev: !promotionList.scrollLeft,
+      next:
+        promotionList.scrollLeft ===
+        promotionList.scrollWidth - promotionList.offsetWidth,
+    });
+  };
 
   const handleClickControlButton = (direction: string) => {
     const promotionList = promotionListRef.current;
@@ -23,25 +41,38 @@ function PromotionPage() {
       left: offset,
       behavior: 'smooth',
     });
+
+    setButtonDisabled({
+      prev: !(promotionList.scrollLeft + offset),
+      next:
+        promotionList.scrollLeft + offset ===
+        promotionList.scrollWidth - promotionList.offsetWidth,
+    });
   };
 
   return (
     <div className='promotion-page'>
       <h1 className='promotion-header'>지금 떠나기 좋은 여행</h1>
       <div className='promotion-list-container'>
-        <ul className='promotion-list' ref={promotionListRef}>
+        <ul
+          className='promotion-list'
+          onScroll={handleScrollPromotionList}
+          ref={promotionListRef}
+        >
           {promotions.map((promotion) => (
             <PromotionCard key={promotion.id} {...promotion} />
           ))}
         </ul>
         <button
           className='control-button prev-button'
+          disabled={buttonDisabled.prev}
           onClick={() => handleClickControlButton('prev')}
         >
           ＜
         </button>
         <button
           className='control-button next-button'
+          disabled={buttonDisabled.next}
           onClick={() => handleClickControlButton('next')}
         >
           ＞
