8131cd5a9779c86d789fc10b37371b45ccd8760e
JASUN LEE <liswktjs@gmail.com>
2022-10-17 15:29:46 +0900

    fix: 맨 첫 번째 요소이거나, 마지막 요소일 때에 안내문구 읽게 하기

--- src/hooks/useHandleCarouselEvent.tsx
+++ src/hooks/useHandleCarouselEvent.tsx
@@ -1,25 +1,48 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const useHandleCarouselEvent = () => {
 	const carouselListRef = useRef<HTMLUListElement>(null);
 	const carouselImgItem = useRef<HTMLImageElement>(null);
+	const title = useRef<HTMLHeadingElement>(null);
+
 	const [travelDescription, setTravelDescription] = useState('');
+	const [buttonState, setButtonState] = useState('');
+
+	useEffect(() => {
+		if (title.current) {
+			title.current.focus();
+		}
+	}, []);
 
 	const onLeftSlideButtonClick = () => {
 		if (!carouselListRef.current || !carouselImgItem.current) {
 			return;
 		}
+		if (carouselListRef.current.scrollLeft === 0) {
+			setButtonState('이전 버튼 (사용중지) ');
+			return;
+		}
 		const elementWidth = carouselImgItem.current.clientWidth + 20;
-		console.log(elementWidth);
 		carouselListRef.current.scrollBy({ top: 0, left: -elementWidth, behavior: 'smooth' });
+		setButtonState('이전 버튼');
 	};
 
 	const onRightSlideButtonClick = () => {
 		if (!carouselListRef.current || !carouselImgItem.current) {
 			return;
 		}
+		if (
+			carouselListRef.current.scrollWidth -
+				carouselListRef.current.clientWidth -
+				carouselListRef.current.scrollLeft <=
+			1
+		) {
+			setButtonState('다음 버튼 (사용중지)');
+			return;
+		}
 		const elementWidth = carouselImgItem.current.clientWidth + 20;
 		carouselListRef.current.scrollBy({ top: 0, left: elementWidth, behavior: 'smooth' });
+		setButtonState('다음 버튼');
 	};
 
 	const onHoverItem = (item: { title: string; price: number; image: string; link: string }) => {
@@ -31,7 +54,9 @@ const useHandleCarouselEvent = () => {
 	return {
 		carouselListRef,
 		carouselImgItem,
+		title,
 		travelDescription,
+		buttonState,
 		onLeftSlideButtonClick,
 		onRightSlideButtonClick,
 		onHoverItem,
