a5ff20d5a1e82a857e15c415a888cab9e060e451
woowapark <shinyoung.park@woowahan.com>
2024-09-22 21:13:17 +0900

    feat: update to 6th-level4 version

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import React from "react";
-import "./App.css";
-import SpinButton from "./components/SpinButton";
+import styles from './App.module.css';
+import Navigation from './components/Navigation';
+import FlightBooking from './components/FlightBooking';
+import TravelSection from './components/TravelSection';
+// import PromotionModal from './components/PromotionModal';
 
 function App() {
   return (
-    <div className="App">
-      <SpinButton />
+    <div className={styles.app}>
+      <Navigation />
+      <div className={styles.header}>
+        <h1 className={`${styles.title} heading-1-text`}>A11Y AIRLINE</h1>
+        <p className="body-text">
+          A11Y AIRLINE은 고객 여러분의 안전하고 쾌적한 여행을 위해 최선을 다하고 있습니다.
+        </p>
+      </div>
+      <div id="main-content" className={styles.main}>
+        <div className={styles.flightBooking}>
+          <FlightBooking />
+        </div>
+        <div className={styles.travelSection}>
+          <h1 className={`${styles.travelTitle} heading-2-text`}>지금 떠나기 좋은 여행</h1>
+          <TravelSection />
+        </div>
+      </div>
+      <div className={styles.footer}>
+        <p className="body-text">&copy; A11Y AIRLINE</p>
+      </div>
+      {/* 추가 CHALLENGE: 모달 포커스 트랩 */}
+      {/* <PromotionModal /> */}
     </div>
   );
 }
---
+++ src/components/TravelSection.tsx
@@ -0,0 +1,92 @@
+import { useState } from 'react';
+
+import travelItem01 from '../assets/travel-item-01.png';
+import travelItem02 from '../assets/travel-item-02.png';
+import travelItem03 from '../assets/travel-item-03.png';
+import chevronLeft from '../assets/chevron-left.svg';
+import chevronRight from '../assets/chevron-right.svg';
+
+import styles from './TravelSection.module.css';
+
+interface TravelOption {
+  departure: string;
+  destination: string;
+  type: string;
+  price: number;
+  image: string;
+  link: string;
+}
+
+const travelOptions: TravelOption[] = [
+  {
+    departure: '서울/인천',
+    destination: '두바이',
+    type: '일반석 왕복',
+    price: 1121600,
+    image: travelItem01,
+    link: 'https://koreanairkp.kaltour.com/ProductOverseas/OverseasList?TOURTYP=KALPAK&PKGBRA=KP&PKGARE=E5&REGNB1=%uC720%uB7FD&REGNB2=%uC911%uB3D9&REGTOP=1'
+  },
+  {
+    departure: '서울/인천',
+    destination: '바르셀로나',
+    type: '일반석 왕복',
+    price: 1515200,
+    image: travelItem02,
+    link: 'https://koreanairkp.kaltour.com/ProductOverseas/OverseasView?pkgpnh=KP44129'
+  },
+  {
+    departure: '서울/인천',
+    destination: '로마',
+    type: '일반석 왕복',
+    price: 1415800,
+    image: travelItem03,
+    link: 'https://koreanairkp.kaltour.com/ProductOverseas/OverseasView?pkgpnh=KP41216'
+  }
+];
+
+const TravelSection = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextTravel = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % travelOptions.length);
+  };
+
+  const prevTravel = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + travelOptions.length) % travelOptions.length);
+  };
+
+  const handleCardClick = (link: string) => {
+    window.open(link, '_blank', 'noopener,noreferrer');
+  };
+
+  return (
+    <div className={styles.travelSection}>
+      <button className={`${styles.navButton} ${styles.navButtonPrev}`} onClick={prevTravel}>
+        <img src={chevronLeft} className={styles.navButtonIcon} />
+      </button>
+      <div className={styles.carousel}>
+        {travelOptions.map((option, index) => (
+          <div
+            key={index}
+            className={`${styles.card} ${index === currentIndex ? styles.cardActive : ''}`}
+            onClick={() => handleCardClick(option.link)}
+          >
+            <img src={option.image} className={styles.cardImage} />
+            <div className={styles.cardContent}>
+              <p className={`${styles.cardTitle} heading-3-text`}>
+                {option.departure} - {option.destination}
+              </p>
+              <p className={`${styles.cardType} body-text`}>{option.type}</p>
+              <p className={`${styles.cardPrice} body-text`}>KRW {option.price.toLocaleString()}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <button className={`${styles.navButton} ${styles.navButtonNext}`} onClick={nextTravel}>
+        <img src={chevronRight} className={styles.navButtonIcon} />
+      </button>
+    </div>
+  );
+};
+
+export default TravelSection;
