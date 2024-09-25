d26d24e3b71818ac93095ea244ca81e17d1516ac
badahertz52 <badahertz52@gmail.com>
2024-09-25 14:03:46 +0900

    feat: TravelSection에서 캐러셀에 대한 정보를 스크린 리더기에서 읽을 수 있도록 함

--- src/App.tsx
+++ src/App.tsx
@@ -18,10 +18,8 @@ function App() {
         <section className={styles.flightBooking}>
           <FlightBooking />
         </section>
-        <section className={styles.travelSection}>
-          <h2 className={`${styles.travelTitle} heading-2-text`}>지금 떠나기 좋은 여행</h2>
-          <TravelSection />
-        </section>
+
+        <TravelSection />
       </main>
       <footer className={styles.footer}>
         <p className="body-text">&copy; A11Y AIRLINE</p>
--- src/components/TravelSection.tsx
+++ src/components/TravelSection.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useId, useState } from 'react';
 
 import travelItem01 from '../assets/travel-item-01.png';
 import travelItem02 from '../assets/travel-item-02.png';
@@ -46,6 +46,14 @@ const travelOptions: TravelOption[] = [
 
 const TravelSection = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const travelSectionId = useId();
+
+  const makeElementId = (id: string) => `${travelSectionId}__${id}`;
+
+  const ELEMENT_ID = {
+    carouselHeading: makeElementId('carousel-heading'),
+    carouselDescription: makeElementId('carousel-description')
+  };
 
   const nextTravel = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % travelOptions.length);
@@ -59,33 +67,65 @@ const TravelSection = () => {
     window.open(link, '_blank', 'noopener,noreferrer');
   };
 
+  const getOptionDescription = (option: TravelOption, index: number) => {
+    const { departure, destination, type, price } = option;
+
+    return `${travelOptions.length}개 추천 상품 중 ${
+      index + 1
+    }번째 상품: ${departure}에서 출발해 ${destination}에 도착하는 ${price}원 ${type}좌석`;
+  };
+
   return (
-    <div className={styles.travelSection}>
-      <button className={`${styles.navButton} ${styles.navButtonPrev}`} onClick={prevTravel}>
-        <img src={chevronLeft} className={styles.navButtonIcon} />
-      </button>
-      <div className={styles.carousel}>
-        {travelOptions.map((option, index) => (
-          <div
-            key={index}
-            className={`${styles.card} ${index === currentIndex ? styles.cardActive : ''}`}
-            onClick={() => handleCardClick(option.link)}
-          >
-            <img src={option.image} className={styles.cardImage} />
-            <div className={styles.cardContent}>
-              <p className={`${styles.cardTitle} heading-3-text`}>
-                {option.departure} - {option.destination}
-              </p>
-              <p className={`${styles.cardType} body-text`}>{option.type}</p>
-              <p className={`${styles.cardPrice} body-text`}>KRW {option.price.toLocaleString()}</p>
-            </div>
-          </div>
-        ))}
+    <section
+      className={styles.travelSection}
+      aria-labelledby={ELEMENT_ID.carouselHeading}
+      aria-describedby={ELEMENT_ID.carouselDescription}
+    >
+      <h2 id={ELEMENT_ID.carouselHeading} className={`${styles.travelTitle} heading-2-text`}>
+        지금 떠나기 좋은 여행
+      </h2>
+      <div className={styles.carouselContainer}>
+        <button
+          className={`${styles.navButton} ${styles.navButtonPrev}`}
+          onClick={prevTravel}
+          type="button"
+        >
+          <span className="screen-only">왼쪽 이동</span>
+          <img src={chevronLeft} className={styles.navButtonIcon} alt="" />
+        </button>
+        <p id={ELEMENT_ID.carouselDescription} aria-live="polite" className="screen-only">
+          {getOptionDescription(travelOptions[currentIndex], currentIndex)}
+        </p>
+        <ol className={styles.carousel}>
+          {travelOptions.map((option, index) => (
+            <li
+              key={index}
+              className={`${styles.card} ${index === currentIndex ? styles.cardActive : ''}`}
+              onClick={() => handleCardClick(option.link)}
+            >
+              <img src={option.image} className={styles.cardImage} alt="" />
+              <div className={styles.cardContent}>
+                <h3 className={`${styles.cardTitle} heading-3-text`}>
+                  {option.departure} - {option.destination}
+                </h3>
+                <p className={`${styles.cardType} body-text`}>{option.type}</p>
+                <p className={`${styles.cardPrice} body-text`}>
+                  KRW {option.price.toLocaleString()}
+                </p>
+              </div>
+            </li>
+          ))}
+        </ol>
+        <button
+          className={`${styles.navButton} ${styles.navButtonNext}`}
+          onClick={nextTravel}
+          type="button"
+        >
+          <span className="screen-only">오른쪽 이동</span>
+          <img src={chevronRight} className={styles.navButtonIcon} alt="" />
+        </button>
       </div>
-      <button className={`${styles.navButton} ${styles.navButtonNext}`} onClick={nextTravel}>
-        <img src={chevronRight} className={styles.navButtonIcon} />
-      </button>
-    </div>
+    </section>
   );
 };
 
