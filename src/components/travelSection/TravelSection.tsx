8f385628e9c11a5b99e6c5c009853a1710453351
useon <roqkftjs@gmail.com>
2024-09-27 21:04:08 +0900

    refactor: 시맨틱태그로 수정

--- src/components/travelSection/TravelSection.tsx
+++ src/components/travelSection/TravelSection.tsx
@@ -60,7 +60,7 @@ const TravelSection = () => {
   };
 
   return (
-    <div className={styles.travelSection}>
+    <article className={styles.travelSection}>
       <button className={`${styles.navButton} ${styles.navButtonPrev}`} onClick={prevTravel}>
         <img src={chevronLeft} className={styles.navButtonIcon} />
       </button>
@@ -85,7 +85,7 @@ const TravelSection = () => {
       <button className={`${styles.navButton} ${styles.navButtonNext}`} onClick={nextTravel}>
         <img src={chevronRight} className={styles.navButtonIcon} />
       </button>
-    </div>
+    </article>
   );
 };
 
