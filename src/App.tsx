344efd9fe0e6a0d89187609d7629137be915717b
chysis <chysiss@naver.com>
2024-09-27 22:05:29 +0900

    style: helmet 태그 위치 조정

--- src/App.tsx
+++ src/App.tsx
@@ -7,32 +7,34 @@ import { Helmet } from 'react-helmet';
 
 function App() {
   return (
-    <div className={styles.app}>
+    <>
       <Helmet>
         <title>A11Y AIRLINE - 안전하고 쾌적한 여행을 위한 항공사</title>
       </Helmet>
-      <Navigation />
-      <div className={styles.header}>
-        <h1 className={`${styles.title} heading-1-text`}>A11Y AIRLINE</h1>
-        <p className="body-text">
-          A11Y AIRLINE은 고객 여러분의 안전하고 쾌적한 여행을 위해 최선을 다하고 있습니다.
-        </p>
-      </div>
-      <div id="main-content" className={styles.main}>
-        <div className={styles.flightBooking}>
-          <FlightBooking />
+      <div className={styles.app}>
+        <Navigation />
+        <div className={styles.header}>
+          <h1 className={`${styles.title} heading-1-text`}>A11Y AIRLINE</h1>
+          <p className="body-text">
+            A11Y AIRLINE은 고객 여러분의 안전하고 쾌적한 여행을 위해 최선을 다하고 있습니다.
+          </p>
         </div>
-        <div className={styles.travelSection}>
-          <h1 className={`${styles.travelTitle} heading-2-text`}>지금 떠나기 좋은 여행</h1>
-          <TravelSection />
+        <div id="main-content" className={styles.main}>
+          <div className={styles.flightBooking}>
+            <FlightBooking />
+          </div>
+          <div className={styles.travelSection}>
+            <h1 className={`${styles.travelTitle} heading-2-text`}>지금 떠나기 좋은 여행</h1>
+            <TravelSection />
+          </div>
         </div>
+        <div className={styles.footer}>
+          <p className="body-text">&copy; A11Y AIRLINE</p>
+        </div>
+        {/* 추가 CHALLENGE: 모달 포커스 트랩 */}
+        {/* <PromotionModal /> */}
       </div>
-      <div className={styles.footer}>
-        <p className="body-text">&copy; A11Y AIRLINE</p>
-      </div>
-      {/* 추가 CHALLENGE: 모달 포커스 트랩 */}
-      {/* <PromotionModal /> */}
-    </div>
+    </>
   );
 }
 
