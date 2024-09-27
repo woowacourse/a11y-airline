6eef573e776e38368b9cc23482c41bea67049ed4
rbgksqkr <rbgks1937@naver.com>
2024-09-27 19:05:04 +0900

    refactor: 스크린 리더가 이미지 캐로셀 내 각 아이템 정보 읽기 기능 & 현재 보이는 아이템 정보 읽기 기능 구현

--- src/components/TravelSection.tsx
+++ src/components/TravelSection.tsx
@@ -73,8 +73,14 @@ const TravelSection = () => {
             key={index}
             className={`${styles.card} ${index === currentIndex ? styles.cardActive : ''}`}
             onClick={() => handleCardClick(option.link)}
+            role="button"
+            aria-live="polite"
+            aria-label={` ${option.departure} 출발 ${option.destination} 도착. ${
+              option.type
+            } 가격 ${option.price.toLocaleString()}원. 선택하면 예약 페이지로 이동합니다.`}
           >
             <img src={option.image} className={styles.cardImage} />
+
             <div className={styles.cardContent}>
               <p className={`${styles.cardTitle} heading-3-text`}>
                 {option.departure} - {option.destination}
