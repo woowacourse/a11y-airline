96285ede365bab4f275af92dbd70d625f9974b34
jinsil <jinsil378900@gmail.com>
2024-09-28 16:52:21 +0900

    refactor: button에 aria-label 추가

--- src/components/TravelSection.tsx
+++ src/components/TravelSection.tsx
@@ -61,10 +61,7 @@ const TravelSection = () => {
 
   return (
     <div className={styles.travelSection}>
-      <button className={`${styles.navButton} ${styles.navButtonPrev}`} onClick={prevTravel}>
-        <img src={chevronLeft} className={styles.navButtonIcon} />
-      </button>
-      <div className={styles.carousel}>
+      <div className={styles.carousel} aria-live="polite" aria-atomic="true">
         {travelOptions.map((option, index) => (
           <div
             key={index}
@@ -82,7 +79,18 @@ const TravelSection = () => {
           </div>
         ))}
       </div>
-      <button className={`${styles.navButton} ${styles.navButtonNext}`} onClick={nextTravel}>
+      <button
+        className={`${styles.navButton} ${styles.navButtonPrev}`}
+        onClick={prevTravel}
+        aria-label="이전 여행 상품"
+      >
+        <img src={chevronLeft} className={styles.navButtonIcon} />
+      </button>
+      <button
+        className={`${styles.navButton} ${styles.navButtonNext}`}
+        onClick={nextTravel}
+        aria-label="다음 여행 상품"
+      >
         <img src={chevronRight} className={styles.navButtonIcon} />
       </button>
     </div>
