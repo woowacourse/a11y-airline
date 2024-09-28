df1cd18e75fdf61bd697f03148a90903e52c577a
손진영 <sm099@naver.com>
2024-09-28 14:54:42 +0900

    refactor(TravelSection): 스크린 리더가 이미지 캐로셀 내 각 아이템 정보를 읽을 수 있도록 접근성 개선

--- src/components/TravelSection.tsx
+++ src/components/TravelSection.tsx
@@ -63,31 +63,48 @@ const TravelSection = () => {
 
   return (
     <div className={styles.travelSection}>
-      <button className={`${styles.navButton} ${styles.navButtonPrev}`} onClick={prevTravel}>
+      <button
+        aria-label="이전"
+        className={`${styles.navButton} ${styles.navButtonPrev}`}
+        onClick={prevTravel}
+      >
         <img src={chevronLeft} className={styles.navButtonIcon} />
       </button>
       <div className={styles.carousel}>
-        {travelOptions.map((option, index) => (
-          <div
-            key={index}
-            className={`${styles.card} ${index === currentIndex ? styles.cardActive : ''}`}
-            onClick={() => handleCardClick(option.link)}
-          >
-            <p className="visually-hidden">
-              {`${travelOptions.length}개의 여행 상품 중 ${currentIndex + 1}번째 상품`}
-            </p>
-            <img src={option.image} className={styles.cardImage} />
-            <div className={styles.cardContent}>
-              <p className={`${styles.cardTitle} heading-3-text`}>
-                {option.departure} - {option.destination}
+        {travelOptions.map((option, index) => {
+          return (
+            <button
+              key={index}
+              role="alert"
+              aria-live="polite"
+              aria-label={`${option.departure} 출발 ${option.destination} 도착 ${
+                option.type
+              } 가격 ${option.price.toLocaleString()}원 선택 하면 예약 페이지로 이동합니다`}
+              className={`${styles.card} ${index === currentIndex ? styles.cardActive : ''}`}
+              onClick={() => handleCardClick(option.link)}
+            >
+              <p className="visually-hidden">
+                {`${travelOptions.length}개의 여행 상품 중 ${currentIndex + 1}번째 상품`}
               </p>
-              <p className={`${styles.cardType} body-text`}>{option.type}</p>
-              <p className={`${styles.cardPrice} body-text`}>KRW {option.price.toLocaleString()}</p>
-            </div>
-          </div>
-        ))}
+              <img src={option.image} className={styles.cardImage} />
+              <div className={styles.cardContent}>
+                <p className={`${styles.cardTitle} heading-3-text`}>
+                  {option.departure} - {option.destination}
+                </p>
+                <p className={`${styles.cardType} body-text`}>{option.type}</p>
+                <p className={`${styles.cardPrice} body-text`}>
+                  KRW {option.price.toLocaleString()}
+                </p>
+              </div>
+            </button>
+          );
+        })}
       </div>
-      <button className={`${styles.navButton} ${styles.navButtonNext}`} onClick={nextTravel}>
+      <button
+        aria-label="다음"
+        className={`${styles.navButton} ${styles.navButtonNext}`}
+        onClick={nextTravel}
+      >
         <img src={chevronRight} className={styles.navButtonIcon} />
       </button>
     </div>
