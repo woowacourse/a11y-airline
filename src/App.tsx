7ca4c1a96488f92205dc09f3c5fa718717aa53e4
Lee sang Yeop <mma7710@naver.com>
2024-09-28 15:03:56 +0900

    적절한 시멘틱 태그로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -8,26 +8,28 @@ function App() {
   return (
     <div className={styles.app}>
       <Navigation />
-      <div className={styles.header}>
-        <h1 className={`${styles.title} heading-1-text`}>A11Y AIRLINE</h1>
-        <p className="body-text">
-          A11Y AIRLINE은 고객 여러분의 안전하고 쾌적한 여행을 위해 최선을 다하고 있습니다.
-        </p>
-      </div>
-      <div id="main-content" className={styles.main}>
-        <div className={styles.flightBooking}>
-          <FlightBooking />
-        </div>
-        <div className={styles.travelSection}>
-          <h2 className={`${styles.travelTitle} heading-2-text`}>지금 떠나기 좋은 여행</h2>
-          <TravelSection />
-        </div>
-      </div>
-      <div className={styles.footer}>
-        <p className="body-text">&copy; A11Y AIRLINE</p>
-      </div>
-      {/* 추가 CHALLENGE: 모달 포커스 트랩 */}
-      {/* <PromotionModal /> */}
+      <main>
+        <header className={styles.header}>
+          <h1 className={`${styles.title} heading-1-text`}>A11Y AIRLINE</h1>
+          <p className="body-text">
+            A11Y AIRLINE은 고객 여러분의 안전하고 쾌적한 여행을 위해 최선을 다하고 있습니다.
+          </p>
+        </header>
+        <section id="main-content" className={styles.main}>
+          <div className={styles.flightBooking}>
+            <FlightBooking />
+          </div>
+          <div className={styles.travelSection}>
+            <h2 className={`${styles.travelTitle} heading-2-text`}>지금 떠나기 좋은 여행</h2>
+            <TravelSection />
+          </div>
+        </section>
+        <footer className={styles.footer}>
+          <p className="body-text">&copy; A11Y AIRLINE</p>
+        </footer>
+        {/* 추가 CHALLENGE: 모달 포커스 트랩 */}
+        {/* <PromotionModal /> */}
+      </main>
     </div>
   );
 }
