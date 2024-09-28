be8da96aa80fc8a7f28a249c4644e55b20dbf535
Lee sang Yeop <mma7710@naver.com>
2024-09-28 17:19:16 +0900

    모달이 열린경우 모달만 포커싱이 되도록 추가

--- src/components/PromotionModal.tsx
+++ src/components/PromotionModal.tsx
@@ -1,22 +1,42 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import close from '../assets/close.svg';
 
 import styles from './PromotionModal.module.css';
+import { createPortal } from 'react-dom';
+
+const portalElement = document.getElementById('modal') as HTMLElement;
 
 const PromotionModal = () => {
   const [isOpen, setIsOpen] = useState(true);
+  const dialogRef = useRef(null);
 
   const closeModal = () => {
     setIsOpen(false);
   };
 
+  useEffect(() => {
+    [...document.body.children].forEach((element) => {
+      if (element.id === 'modal') {
+        element.removeAttribute('aria-hidden');
+        return;
+      }
+
+      if (isOpen) {
+        element.setAttribute('aria-hidden', 'true');
+        return;
+      }
+
+      element.removeAttribute('aria-hidden');
+    });
+  }, [isOpen]);
+
   if (!isOpen) {
     return null;
   }
 
-  return (
-    <div className={styles.modal}>
+  return createPortal(
+    <div className={styles.modal} ref={dialogRef}>
       <div className={styles.modalBackdrop} onClick={closeModal}></div>
       <div className={styles.modalContainer}>
         <div className={styles.modalContent}>
@@ -27,11 +47,12 @@ const PromotionModal = () => {
           </p>
           <button className={`${styles.modalActionButton} button-text`}>앱에서 열기</button>
           <button className={`${styles.modalCloseButton} heading-2-text`} onClick={closeModal}>
-            <img src={close} />
+            <img src={close} alt="창 닫기" />
           </button>
         </div>
       </div>
-    </div>
+    </div>,
+    portalElement
   );
 };
 
