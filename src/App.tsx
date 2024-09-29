3c552b7f4ecd1fc06bd09199a26790af1867e2c9
pakxe <pigkill40@naver.com>
2024-09-29 21:06:40 +0900

    feat: 시멘틱 태그인 main, article, footer 사용

--- src/App.tsx
+++ src/App.tsx
@@ -14,18 +14,18 @@ function App() {
           A11Y AIRLINE은 고객 여러분의 안전하고 쾌적한 여행을 위해 최선을 다하고 있습니다.
         </p>
       </div>
-      <div id="main-content" className={styles.main}>
-        <div className={styles.flightBooking}>
+      <main id="main-content" className={styles.main} tabIndex={0}>
+        <article className={styles.flightBooking}>
           <FlightBooking />
-        </div>
-        <div className={styles.travelSection}>
-          <h1 className={`${styles.travelTitle} heading-2-text`}>지금 떠나기 좋은 여행</h1>
+        </article>
+        <article className={styles.travelSection}>
+          <h2 className={`${styles.travelTitle} heading-2-text`}>지금 떠나기 좋은 여행</h2>
           <TravelSection />
-        </div>
-      </div>
-      <div className={styles.footer}>
+        </article>
+      </main>
+      <footer className={styles.footer}>
         <p className="body-text">&copy; A11Y AIRLINE</p>
-      </div>
+      </footer>
       {/* 추가 CHALLENGE: 모달 포커스 트랩 */}
       {/* <PromotionModal /> */}
     </div>
