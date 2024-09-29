aa360bb2ddcc3929e8511b46c3d140ba8782f5bf
pakxe <pigkill40@naver.com>
2024-09-29 21:07:02 +0900

    feat: 페이지의 메인 컨텐츠로 바로 이동할 수 있는 a 태그 추가

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,10 @@ import TravelSection from './components/TravelSection';
 function App() {
   return (
     <div className={styles.app}>
+      <a href="#main-content" className="visually-hidden">
+        본문으로 바로가기
+      </a>
+
       <Navigation />
       <div className={styles.header}>
         <h1 className={`${styles.title} heading-1-text`}>A11Y AIRLINE</h1>
