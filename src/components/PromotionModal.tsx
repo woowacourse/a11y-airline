bb79eb62283c87a2743e00edc65595253073b11b
Seongjin Hong <hongseongjin.to@gmail.com>
2024-09-30 02:02:16 +0900

    feat(PromotionModal): 모달에 시멘틱 구조 및 음성 지원 정보 적용

--- src/components/PromotionModal.tsx
+++ src/components/PromotionModal.tsx
@@ -16,22 +16,39 @@ const PromotionModal = () => {
   }
 
   return (
-    <div className={styles.modal}>
-      <div className={styles.modalBackdrop} onClick={closeModal}></div>
+    <section
+      className={styles.modal}
+      role="dialog"
+      aria-modal="true"
+      aria-label="A11Y AIRLINE 서비스 안내 모달"
+    >
+      <div className={styles.modalBackdrop} onClick={closeModal} aria-hidden="true"></div>
       <div className={styles.modalContainer}>
         <div className={styles.modalContent}>
-          <h2 className={`${styles.modalTitle} heading-2-text`}>여행할 땐 A11Y AIRLINE 앱</h2>
-          <p className={`${styles.modalDescription} body-text`}>
-            체크인, 탑승권 저장, 수하물 알림까지
-            <br />- 앱으로 더욱 편하게 여행하세요!
-          </p>
-          <button className={`${styles.modalActionButton} button-text`}>앱에서 열기</button>
-          <button className={`${styles.modalCloseButton} heading-2-text`} onClick={closeModal}>
-            <img src={close} />
-          </button>
+          <header>
+            <h2 className={`${styles.modalTitle} heading-2-text`}>여행할 땐 A11Y AIRLINE 앱</h2>
+          </header>
+          <main>
+            <p className={`${styles.modalDescription} body-text`}>
+              체크인, 탑승권 저장, 수하물 알림까지
+              <br />- 앱으로 더욱 편하게 여행하세요!
+            </p>
+            <button type="button" className={`${styles.modalActionButton} button-text`}>
+              앱에서 열기
+            </button>
+          </main>
+          <footer>
+            <button
+              type="button"
+              className={`${styles.modalCloseButton} heading-2-text`}
+              onClick={closeModal}
+            >
+              <img src={close} alt="모달 닫기" />
+            </button>
+          </footer>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
