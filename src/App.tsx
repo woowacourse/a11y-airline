336b89e248882de20ddeffd2549c180c8dcc83f5
useon <roqkftjs@gmail.com>
2024-09-29 22:26:54 +0900

    refactor: 페이지 타이틀 수정

--- src/App.tsx
+++ src/App.tsx
@@ -20,9 +20,9 @@ function App() {
       <SkipToContent onSkipToContent={handleSkipToContent} />
       <Navigation />
       <header className={styles.header}>
-        <h1 className={`${styles.title} heading-1-text`}>A11Y AIRLINE</h1>
+        <h1 className={`${styles.title} heading-1-text`}>SUNDAY AIRLINE</h1>
         <p className="body-text">
-          A11Y AIRLINE은 고객 여러분의 안전하고 쾌적한 여행을 위해 최선을 다하고 있습니다.
+          SUNDAY AIRLINE은 고객 여러분의 안전하고 쾌적한 여행을 위해 최선을 다하고 있습니다.
         </p>
       </header>
       <main id="main-content" ref={mainContentRef} className={styles.main}>
@@ -35,7 +35,7 @@ function App() {
         </div>
       </main>
       <footer className={styles.footer}>
-        <p className="body-text">&copy; A11Y AIRLINE</p>
+        <p className="body-text">&copy; SUNDAY AIRLINE</p>
       </footer>
       {/* 추가 CHALLENGE: 모달 포커스 트랩 */}
       {/* <PromotionModal /> */}
