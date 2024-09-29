abf4946fa4c605e43b058afa4f04312dec0ca9e8
cys4585 <dev.youngsu@gmail.com>
2024-09-29 21:09:02 +0900

    페이지에 적절한 제목 제공

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,14 @@ function App() {
   return (
     <div className={styles.app}>
       <Navigation />
+
       <div className={styles.header}>
         <h1 className={`${styles.title} heading-1-text`}>A11Y AIRLINE</h1>
         <p className="body-text">
           A11Y AIRLINE은 고객 여러분의 안전하고 쾌적한 여행을 위해 최선을 다하고 있습니다.
         </p>
       </div>
+
       <div id="main-content" className={styles.main}>
         <div className={styles.flightBooking}>
           <FlightBooking />
@@ -23,9 +25,11 @@ function App() {
           <TravelSection />
         </div>
       </div>
+
       <div className={styles.footer}>
         <p className="body-text">&copy; A11Y AIRLINE</p>
       </div>
+
       {/* 추가 CHALLENGE: 모달 포커스 트랩 */}
       {/* <PromotionModal /> */}
     </div>
