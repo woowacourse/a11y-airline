7467e1067014e2637f33402da8264a07245b1c96
jaeml06 <jaeml0630@gmail.com>
2024-09-29 09:21:09 +0900

    feat: 키보드 사용자를 위해 skip link 구현

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ import TravelSection from './components/TravelSection';
 function App() {
   return (
     <div className={styles.app}>
+      <a href="#main-content" className={styles.skipLink}>
+        본문으로 바로가기
+      </a>
       <header>
         <Navigation />
         <div className={styles.header}>
