3452c643dbc94008cf5740e5a7e151e58442c999
useon <roqkftjs@gmail.com>
2024-09-29 14:36:08 +0900

    feat: 이전/다음 상품 버튼을 눌러 이동시 바뀐 상품 데이터를 스크린 리더기가 읽도록 하는 기능

--- src/components/travelSection/TravelSection.tsx
+++ src/components/travelSection/TravelSection.tsx
@@ -61,11 +61,15 @@ const TravelSection = () => {
 
   return (
     <article className={styles.travelSection}>
-      <button className={`${styles.navButton} ${styles.navButtonPrev}`} onClick={prevTravel}>
+      <button
+        className={`${styles.navButton} ${styles.navButtonPrev}`}
+        onClick={prevTravel}
+        aria-label="이전 여행 상품"
+      >
         <img src={chevronLeft} className={styles.navButtonIcon} />
       </button>
-      <div className={styles.carousel}>
-        <div className="visually-hidden" role="alert">
+      <div className={styles.carousel} aria-live="polite">
+        <div className="visually-hidden">
           {`세계 여행 상품 ${travelOptions.length} 중 ${currentIndex + 1}번째 상품`}
         </div>
         {travelOptions.map((option, index) => (
@@ -88,7 +92,11 @@ const TravelSection = () => {
           </button>
         ))}
       </div>
-      <button className={`${styles.navButton} ${styles.navButtonNext}`} onClick={nextTravel}>
+      <button
+        className={`${styles.navButton} ${styles.navButtonNext}`}
+        onClick={nextTravel}
+        aria-label="다음 여행 상품"
+      >
         <img src={chevronRight} className={styles.navButtonIcon} />
       </button>
     </article>
