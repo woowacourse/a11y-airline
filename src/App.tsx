ff2f646c0fa07d850f64a588e0adce3e8c9f3860
ss0526100 <ss0526100@gmail.com>
2024-09-29 18:30:17 +0900

    feat(travelSection): 캐러셀 전체 아이템수 읽게 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import styles from './App.module.css';
 import Navigation from './components/Navigation';
-import FlightBooking from './components/FlightBooking';
 import TravelSection from './components/TravelSection';
+import styles from './App.module.css';
+
+// import FlightBooking from './components/FlightBooking';
+
 // import PromotionModal from './components/PromotionModal';
 
 function App() {
@@ -9,7 +11,7 @@ function App() {
     <div className={styles.app}>
       <Navigation />
       <div className={styles.header}>
-        <h1 className={`${styles.title} heading-1-text`}>A11Y AIRLINE</h1>
+        {/* <h1 className={`${styles.title} heading-1-text`}>A11Y AIRLINE</h1>
         <p className="body-text">
           A11Y AIRLINE은 고객 여러분의 안전하고 쾌적한 여행을 위해 최선을 다하고 있습니다.
         </p>
@@ -17,7 +19,7 @@ function App() {
       <div id="main-content" className={styles.main}>
         <div className={styles.flightBooking}>
           <FlightBooking />
-        </div>
+        </div> */}
         <div className={styles.travelSection}>
           <h1 className={`${styles.travelTitle} heading-2-text`}>지금 떠나기 좋은 여행</h1>
           <TravelSection />
