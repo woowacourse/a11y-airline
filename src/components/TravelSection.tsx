b71f7ff58327eace52ddbabcd81cfa4803f92db8
rbgksqkr <rbgks1937@naver.com>
2024-09-30 17:13:00 +0900

    refactor: 시멘틱 태그로 수정

--- src/components/TravelSection.tsx
+++ src/components/TravelSection.tsx
@@ -61,30 +61,36 @@ const TravelSection = () => {
 
   return (
     <div className={styles.travelSection}>
-      <div className={styles.carousel}>
-        {travelOptions.map((option, index) => (
-          <button
-            key={index}
-            className={`${styles.card} ${index === currentIndex ? styles.cardActive : ''}`}
-            onClick={() => handleCardClick(option.link)}
-            aria-live="polite"
-            aria-label={`${travelOptions.length}개의 여행 상품 중 ${currentIndex + 1}번째 상품. ${
-              option.departure
-            } 출발 ${option.destination} 도착. ${
-              option.type
-            } 가격 ${option.price.toLocaleString()}원. 선택하면 예약 페이지로 이동합니다.`}
-          >
-            <img src={option.image} className={styles.cardImage} />
+      <div aria-live="polite">
+        <span className="visually-hidden" role="text">
+          {travelOptions.length}개의 여행 상품 중 {currentIndex + 1}번째 상품.
+        </span>
+        <ol className={styles.carousel}>
+          {travelOptions.map((option, index) => (
+            <li>
+              <button
+                key={index}
+                className={`${styles.card} ${index === currentIndex ? styles.cardActive : ''}`}
+                onClick={() => handleCardClick(option.link)}
+                aria-label={`${option.departure} 출발 ${option.destination} 도착. ${
+                  option.type
+                } 가격 ${option.price.toLocaleString()}원. 선택하면 예약 페이지로 이동합니다.`}
+              >
+                <img src={option.image} className={styles.cardImage} />
 
-            <div className={styles.cardContent}>
-              <p className={`${styles.cardTitle} heading-3-text`}>
-                {option.departure} - {option.destination}
-              </p>
-              <p className={`${styles.cardType} body-text`}>{option.type}</p>
-              <p className={`${styles.cardPrice} body-text`}>KRW {option.price.toLocaleString()}</p>
-            </div>
-          </button>
-        ))}
+                <div className={styles.cardContent}>
+                  <p className={`${styles.cardTitle} heading-3-text`}>
+                    {option.departure} - {option.destination}
+                  </p>
+                  <p className={`${styles.cardType} body-text`}>{option.type}</p>
+                  <p className={`${styles.cardPrice} body-text`}>
+                    KRW {option.price.toLocaleString()}
+                  </p>
+                </div>
+              </button>
+            </li>
+          ))}
+        </ol>
       </div>
       <button
         className={`${styles.navButton} ${styles.navButtonPrev}`}
