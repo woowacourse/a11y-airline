59644c68565e5fd8bdcf3a0ca309276fc4cd0a6b
chysis <chysiss@naver.com>
2024-09-30 16:11:10 +0900

    refactor: 정보 전달을 위한 요소에 포커스 되지 않도록 설정 및 캐러셀 버튼의 aria-control 속성 제거

--- src/components/TravelSection.tsx
+++ src/components/TravelSection.tsx
@@ -84,7 +84,7 @@ const TravelSection = () => {
   return (
     <div className={styles.travelSection} onFocus={handleFocus} onBlur={handleBlur}>
       <h2 className={`${styles.travelTitle} heading-2-text`}>지금 떠나기 좋은 여행</h2>
-      <div className="visually-hidden" ref={travelInfoRef}>
+      <div className="visually-hidden" ref={travelInfoRef} tabIndex={-1}>
         {generateTravelInfoLabel(travelOptions[currentIndex])}
       </div>
       <section
@@ -117,7 +117,6 @@ const TravelSection = () => {
       <button
         className={`${styles.navButton} ${styles.navButtonPrev}`}
         onClick={prevTravel}
-        aria-controls="carousel-button"
         aria-label="이전 여행 상품 보기"
       >
         <img src={chevronLeft} className={styles.navButtonIcon} alt="" />
@@ -125,7 +124,6 @@ const TravelSection = () => {
       <button
         className={`${styles.navButton} ${styles.navButtonNext}`}
         onClick={nextTravel}
-        aria-controls="carousel-button"
         aria-label="다음 여행 상품 보기"
       >
         <img src={chevronRight} className={styles.navButtonIcon} alt="" />
