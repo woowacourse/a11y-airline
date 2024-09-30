ab527826d8e7c7e5f4825ab9d57115f206ecb3b5
hwinkr <dnddl8280@naver.com>
2024-09-30 16:46:22 +0900

    feat: 시멘틱 구조를 사용하도록 개선, 키보드 사용자에게 본문으로 바로갈 수 있는 링크 제공

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,17 @@ import TravelSection from './components/TravelSection';
 function App() {
   return (
     <div className={styles.app}>
+      <a href="#main-content" className={styles.skipLink}>
+        본문으로 바로가기
+      </a>
       <Navigation />
-      <div className={styles.header}>
+      <header className={styles.header}>
         <h1 className={`${styles.title} heading-1-text`}>A11Y AIRLINE</h1>
         <p className="body-text">
           A11Y AIRLINE은 고객 여러분의 안전하고 쾌적한 여행을 위해 최선을 다하고 있습니다.
         </p>
-      </div>
-      <div id="main-content" className={styles.main}>
+      </header>
+      <main id="main-content" className={styles.main}>
         <div className={styles.flightBooking}>
           <FlightBooking />
         </div>
@@ -22,10 +25,10 @@ function App() {
           <h1 className={`${styles.travelTitle} heading-2-text`}>지금 떠나기 좋은 여행</h1>
           <TravelSection />
         </div>
-      </div>
-      <div className={styles.footer}>
-        <p className="body-text">&copy; A11Y AIRLINE</p>
-      </div>
+      </main>
+      <footer className={styles.footer}>
+        <p className="body-text">&copy; HARRY AIRLINE</p>
+      </footer>
       {/* 추가 CHALLENGE: 모달 포커스 트랩 */}
       {/* <PromotionModal /> */}
     </div>
