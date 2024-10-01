a4de6ff2eae0bbc7eba3dc565fcc502e02887bcd
00kang <kdb070970@gmail.com>
2024-10-01 16:36:30 +0900

    refactor: title을 app이 아니라 html로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,6 @@ import TravelSection from './components/TravelSection';
 function App() {
   return (
     <div className={styles.app}>
-      <title>A11Y AIRLINE - 안전하고 쾌적한 여행</title>
-
       <a href="#main-content" className={styles.skipLink}>
         본문으로 바로가기
       </a>
