e1f30ad35780b9a5ebd6b57e4fb53504ac45ff45
Lee sang Yeop <mma7710@naver.com>
2024-10-01 16:13:57 +0900

    skip navigation 이 main-content로 이동하도록 수정

--- src/components/SkipNavigation.tsx
+++ src/components/SkipNavigation.tsx
@@ -3,7 +3,7 @@ import styles from './SkipNavigation.module.css'; // CSS Module import
 
 const SkipNavigation = () => {
   return (
-    <a href="#main" className={styles.skipNavAnchor}>
+    <a href="#main-content" className={styles.skipNavAnchor}>
       Skip to main content
     </a>
   );
