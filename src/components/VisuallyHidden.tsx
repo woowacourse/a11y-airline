2e390ce3c5d64d225d1f6a18906504bf8d2fda47
mun-kyeong <horse6953@naver.com>
2025-09-22 10:13:20 +0900

    feat:  스크린 리더가 이미지 캐로셀 내 각 아이템 정보를 읽을 수 있도록 개선

--- src/components/VisuallyHidden.tsx
+++ src/components/VisuallyHidden.tsx
@@ -1,3 +1,5 @@
+import styles from './VisuallyHidden.module.css';
+
 export default function VisuallyHidden({ children }: { children: React.ReactNode }) {
-  return <p className="visually-hidden">{children}</p>;
+  return <p className={styles.visuallyHidden}>{children}</p>;
 }
