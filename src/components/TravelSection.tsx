40b605323605c04d5b6c54a47838a802bd455801
ShinjungOh <tlswjd9260@gmail.com>
2025-09-22 15:24:14 +0900

    feat: img 태그에 누락된 alt 속성 추가

--- src/components/TravelSection.tsx
+++ src/components/TravelSection.tsx
@@ -62,7 +62,7 @@ const TravelSection = () => {
   return (
     <div className={styles.travelSection}>
       <button className={`${styles.navButton} ${styles.navButtonPrev}`} onClick={prevTravel}>
-        <img src={chevronLeft} className={styles.navButtonIcon} />
+        <img src={chevronLeft} className={styles.navButtonIcon} alt="이전" />
       </button>
       <div className={styles.carousel}>
         {travelOptions.map((option, index) => (
@@ -71,7 +71,7 @@ const TravelSection = () => {
             className={`${styles.card} ${index === currentIndex ? styles.cardActive : ''}`}
             onClick={() => handleCardClick(option.link)}
           >
-            <img src={option.image} className={styles.cardImage} />
+            <img src={option.image} className={styles.cardImage} alt='배경 이미지' />
             <div className={styles.cardContent}>
               <p className={`${styles.cardTitle} heading-3-text`}>
                 {option.departure} - {option.destination}
@@ -83,7 +83,7 @@ const TravelSection = () => {
         ))}
       </div>
       <button className={`${styles.navButton} ${styles.navButtonNext}`} onClick={nextTravel}>
-        <img src={chevronRight} className={styles.navButtonIcon} />
+        <img src={chevronRight} className={styles.navButtonIcon} alt="다음" />
       </button>
     </div>
   );
