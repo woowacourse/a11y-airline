04419afb566668470943c231ff1a809a57cd8573
deokbin <deokbin@naver.com>
2025-09-27 21:48:52 +0900

    feat: PromotionModal 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import styles from './App.module.css';
 import Navigation from './components/Navigation';
 import FlightBooking from './components/FlightBooking';
 import TravelSection from './components/TravelSection';
-// import PromotionModal from './components/PromotionModal';
+import PromotionModal from './components/PromotionModal';
 
 function App() {
   return (
@@ -30,7 +30,7 @@ function App() {
       <footer>
         <p className="body-text">&copy; A11Y AIRLINE</p>
       </footer>
-      {/* <PromotionModal /> */}
+      <PromotionModal />
     </div>
   );
 }
