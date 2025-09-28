8377657954f1506e3a8627336cb08f42df897f60
AHHYUNJU <wndkgus0225@naver.com>
2025-09-29 07:21:52 +0900

    feat: 본문으로 바로가기 구현

--- src/App.tsx
+++ src/App.tsx
@@ -10,27 +10,33 @@ function App() {
   }, []);
 
   return (
-    <div className={styles.app}>
-      <header className={styles.header}>
-        <Navigation />
-        <h1 className={`${styles.title} heading-1-text`}>A11Y AIRLINE</h1>
-        <p className="body-text">
-          A11Y AIRLINE은 고객 여러분의 안전하고 쾌적한 여행을 위해 최선을 다하고 있습니다.
-        </p>
-      </header>
-      <main id="main-content" className={styles.main}>
-        <section className={styles.flightBooking}>
-          <FlightBooking />
-        </section>
-        <section className={styles.travelSection}>
-          <h2 className={`${styles.travelTitle} heading-2-text`}>지금 떠나기 좋은 여행</h2>
-          <TravelSection />
-        </section>
-      </main>
-      <footer className={styles.footer}>
-        <p className="body-text">&copy; A11Y AIRLINE</p>
-      </footer>
-    </div>
+    <>
+      <a href="#main-content" className="skip-link">
+        본문으로 바로가기
+      </a>
+
+      <div className={styles.app}>
+        <header className={styles.header}>
+          <Navigation />
+          <h1 className={`${styles.title} heading-1-text`}>A11Y AIRLINE</h1>
+          <p className="body-text">
+            A11Y AIRLINE은 고객 여러분의 안전하고 쾌적한 여행을 위해 최선을 다하고 있습니다.
+          </p>
+        </header>
+        <main id="main-content" className={styles.main}>
+          <section className={styles.flightBooking}>
+            <FlightBooking />
+          </section>
+          <section className={styles.travelSection}>
+            <h2 className={`${styles.travelTitle} heading-2-text`}>지금 떠나기 좋은 여행</h2>
+            <TravelSection />
+          </section>
+        </main>
+        <footer className={styles.footer}>
+          <p className="body-text">&copy; A11Y AIRLINE</p>
+        </footer>
+      </div>
+    </>
   );
 }
 
