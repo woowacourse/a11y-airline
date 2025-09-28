4ff0adbb44eb0d5a525aa2be39c8974975a7542b
aydenote <dslgpgh@gmail.com>
2025-09-28 21:26:48 +0900

    feat: 아이템으로 이동하고 현재 보이는 아이템의 정보를 읽도록 구현

--- src/components/TravelSection.tsx
+++ src/components/TravelSection.tsx
@@ -61,12 +61,14 @@ const TravelSection = () => {
 
   return (
     <div className={styles.travelSection}>
-      <button className={`${styles.navButton} ${styles.navButtonPrev}`} onClick={prevTravel}>
-        <img src={chevronLeft} className={styles.navButtonIcon} />
-      </button>
+      <h2 className="heading-2-text" tabIndex={0}>
+        지금 떠나기 좋은 여행
+      </h2>
+
       <p id="travel-pos" className={styles.visuallyHidden} tabIndex={0}>
         세계 여행 상품 {travelOptions.length}개
       </p>
+
       <div className={styles.carousel}>
         {travelOptions.map((option, index) => {
           const isActive = index === currentIndex;
@@ -83,12 +85,6 @@ const TravelSection = () => {
               aria-hidden={isActive ? 'false' : 'true'}
               tabIndex={isActive ? 0 : -1}
               onClick={() => handleCardClick(option.link)}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter' || e.key === ' ') {
-                  e.preventDefault();
-                  handleCardClick(option.link);
-                }
-              }}
             >
               <img src={option.image} className={styles.cardImage} alt="" aria-hidden="true" />
               <div className={styles.cardContent}>
@@ -103,9 +99,28 @@ const TravelSection = () => {
             </div>
           );
         })}
-      </div>{' '}
-      <button className={`${styles.navButton} ${styles.navButtonNext}`} onClick={nextTravel}>
-        <img src={chevronRight} className={styles.navButtonIcon} />
+      </div>
+      <p id="travel-live" className={styles.visuallyHidden} aria-live="polite" aria-atomic="true">
+        세계 여행 상품 {travelOptions.length}개 중 {currentIndex + 1}번째 상품,{' '}
+        {travelOptions[currentIndex].departure}출발,
+        {travelOptions[currentIndex].destination}도착, {travelOptions[currentIndex].type}, 가격
+        {travelOptions[currentIndex].price}원
+      </p>
+
+      <button
+        className={`${styles.navButton} ${styles.navButtonPrev}`}
+        onClick={prevTravel}
+        aria-label="이전 여행 상품"
+      >
+        <img src={chevronLeft} className={styles.navButtonIcon} alt="" aria-hidden="true" />
+      </button>
+
+      <button
+        className={`${styles.navButton} ${styles.navButtonNext}`}
+        onClick={nextTravel}
+        aria-label="다음 여행 상품"
+      >
+        <img src={chevronRight} className={styles.navButtonIcon} alt="" aria-hidden="true" />
       </button>
     </div>
   );
