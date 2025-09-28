44a0e1fccec083b94fb1127a6537e057a1630917
wo-o29 <woogur29@gmail.com>
2025-09-28 23:12:50 +0900

    refactor: VisuallyHidden 일반 props 받을 수 있게 설정

--- src/components/VisuallyHidden.tsx
+++ src/components/VisuallyHidden.tsx
@@ -6,9 +6,9 @@ interface VisuallyHiddenProps {
   as?: ElementType;
 }
 
-function VisuallyHidden({ as, children }: PropsWithChildren<VisuallyHiddenProps>) {
+function VisuallyHidden({ as, children, ...props }: PropsWithChildren<VisuallyHiddenProps>) {
   return (
-    <Polymorphic as={as} className={styles.visuallyHidden}>
+    <Polymorphic as={as} className={styles.visuallyHidden} {...props}>
       {children}
     </Polymorphic>
   );
