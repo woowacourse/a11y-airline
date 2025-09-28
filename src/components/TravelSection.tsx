e8eab8d189e4747e184ca568623816b4c9a78194
Minje Sung <minje813@dgu.ac.kr>
2025-09-29 01:18:31 +0900

    refactor: 시맨틱 태그 변경

--- src/components/TravelSection.tsx
+++ src/components/TravelSection.tsx
@@ -61,33 +61,35 @@ const TravelSection = () => {
   };
 
   return (
-    <div className={styles.travelSection}>
-      <div className={styles.carousel} aria-live="polite">
+    <section className={styles.travelSection}>
+      <ul className={styles.carousel} aria-live="polite">
         <span className="visually-hidden">
           {`${travelOptions.length}개의 여행 상품 중 ${currentIndex + 1}번째 상품`}
         </span>
         {travelOptions.map((option, index) => (
-          <button
-            key={index}
-            className={`${styles.card} ${index === currentIndex ? styles.cardActive : ''}`}
-            onClick={() => handleCardClick(option.link)}
-            aria-label={`${option.departure} 출발 ${option.destination} 도착. ${option.type}. 가격 ${option.price}원. 선택하면 예약 페이지로 이동합니다.`}
-          >
-            <AriaHidden>
-              <img src={option.image} className={styles.cardImage} />
-              <div className={styles.cardContent}>
-                <p className={`${styles.cardTitle} heading-3-text`}>
-                  {option.departure} - {option.destination}
-                </p>
-                <p className={`${styles.cardType} body-text`}>{option.type}</p>
-                <p className={`${styles.cardPrice} body-text`}>
-                  KRW {option.price.toLocaleString()}
-                </p>
-              </div>
-            </AriaHidden>
-          </button>
+          <li>
+            <button
+              key={index}
+              className={`${styles.card} ${index === currentIndex ? styles.cardActive : ''}`}
+              onClick={() => handleCardClick(option.link)}
+              aria-label={`${option.departure} 출발 ${option.destination} 도착. ${option.type}. 가격 ${option.price}원. 선택하면 예약 페이지로 이동합니다.`}
+            >
+              <AriaHidden>
+                <img src={option.image} className={styles.cardImage} />
+                <div className={styles.cardContent}>
+                  <p className={`${styles.cardTitle} heading-3-text`}>
+                    {option.departure} - {option.destination}
+                  </p>
+                  <p className={`${styles.cardType} body-text`}>{option.type}</p>
+                  <p className={`${styles.cardPrice} body-text`}>
+                    KRW {option.price.toLocaleString()}
+                  </p>
+                </div>
+              </AriaHidden>
+            </button>
+          </li>
         ))}
-      </div>
+      </ul>
       <button
         className={`${styles.navButton} ${styles.navButtonPrev}`}
         onClick={prevTravel}
@@ -102,7 +104,7 @@ const TravelSection = () => {
       >
         <img src={chevronRight} className={styles.navButtonIcon} />
       </button>
-    </div>
+    </section>
   );
 };
 
