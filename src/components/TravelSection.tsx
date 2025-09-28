62bba057a311ef098f3802396f763dc4a79c18f4
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-09-28 16:36:26 +0900

    refactor: button에 맥락 정보를 추가로 제공

--- src/components/TravelSection.tsx
+++ src/components/TravelSection.tsx
@@ -73,7 +73,12 @@ const TravelSection = () => {
               className={`${styles.card} ${index === currentIndex ? styles.cardActive : ''}`}
               aria-hidden={index !== currentIndex}
             >
-              <button onClick={() => handleCardClick(option.link)}>
+              <button
+                onClick={() => handleCardClick(option.link)}
+                aria-label={`${option.departure} 출발 ${option.destination} 도착, ${
+                  option.type
+                }, 가격 ${option.price.toLocaleString()} 원. 선택하면 예약 페이지로 이동합니다.`}
+              >
                 <img src={option.image} className={styles.cardImage} />
                 <div className={styles.cardContent}>
                   <p className={`${styles.cardTitle} heading-3-text`}>
