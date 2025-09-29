fcbf86b1c7e868bb734d90a7c4ec0b719396d27d
이상희 <sang6299@naver.com>
2025-09-30 06:18:26 +0900

    feat: 본문으로 바로가기 링크 추가

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ import TravelSection from './components/TravelSection';
 function App() {
   return (
     <div className={styles.app}>
+      <a href="#main-content" className="skip-link">
+        본문으로 바로가기
+      </a>
       <Navigation />
       <header className={styles.header}>
         <h1 className={`${styles.title} heading-1-text`}>A11Y AIRLINE</h1>
