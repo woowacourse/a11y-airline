56cb8ea004e403537f04633f05799a39b4bc6b96
sooyeoniya <tndus1306403@naver.com>
2025-09-29 15:32:31 +0900

    feat: 여행지, 좌석 유형, 가격 정보를 한번에 읽을 수 있도록 구현
    스크린 리더가 너무 빠르게 읽어서 숨쉴 틈이 없었음
    -> 쉼표나 마침표를 추가해서 스크린 리더가 자연스럽게 읽을 수 있도록 함
    
    "서울/인천 출발 두바이 도착. 일반석 왕복. 가격 112만 1600원. 선택하면 예약 페이지로 이동합니다. 버튼"

--- src/components/TravelSection.tsx
+++ src/components/TravelSection.tsx
@@ -73,6 +73,12 @@ const TravelSection = () => {
             key={index}
             className={`${styles.card} ${index === currentIndex ? styles.cardActive : ''}`}
             onClick={() => handleCardClick(option.link)}
+            role="button"
+            aria-label={`${option.departure} 출발 ${option.destination} 도착. ${
+              option.type
+            }. 가격 ${Math.floor(option.price / 10000)}만 ${(
+              option.price % 10000
+            ).toLocaleString()}원. 선택하면 예약 페이지로 이동합니다.`}
           >
             <img src={option.image} className={styles.cardImage} />
             <div className={styles.cardContent}>
