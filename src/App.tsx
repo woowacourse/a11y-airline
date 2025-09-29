5d4e9f3cd071338d7b4586cb9658d08d5efd00bd
kimyou1102 <yoosk5485@naver.com>
2025-09-30 01:35:41 +0900

    chore: 사용하지 않는 주석 제거

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import styles from './App.module.css';
 import Navigation from './components/Navigation';
 import FlightBooking from './components/FlightBooking';
 import TravelSection from './components/TravelSection';
-// import PromotionModal from './components/PromotionModal';
 
 function App() {
   return (
@@ -29,8 +28,6 @@ function App() {
       <footer className={styles.footer}>
         <p className="body-text">&copy; A11Y AIRLINE</p>
       </footer>
-      {/* 추가 CHALLENGE: 모달 포커스 트랩 */}
-      {/* <PromotionModal /> */}
     </div>
   );
 }
