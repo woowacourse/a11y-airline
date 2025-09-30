11adf2c0d35a4127e272a8702503bc2c7116f481
wo-o29 <woogur29@gmail.com>
2025-09-30 18:48:07 +0900

    fix: `display: none` 검증 getComputedStyle 방식으로 변경

--- src/hooks/useFocusTrap/tabbable.ts
+++ src/hooks/useFocusTrap/tabbable.ts
@@ -1,16 +1,15 @@
 const TABBABLE_NODES = /input|select|textarea|button|object/;
-export const FOCUS_SELECTOR =
-  "a, input, select, textarea, button, object, [tabindex]";
+export const FOCUS_SELECTOR = 'a, input, select, textarea, button, object, [tabindex]';
 
 const isDisplayHidden = (element: HTMLElement) => {
-  return element.style.display === "none";
+  return getComputedStyle(element).display === 'none';
 };
 
 const visible = (element: HTMLElement) => {
   const isHidden =
-    element.getAttribute("aria-hidden") ||
-    element.getAttribute("hidden") ||
-    element.getAttribute("type") === "hidden";
+    element.getAttribute('aria-hidden') ||
+    element.getAttribute('hidden') ||
+    element.getAttribute('type') === 'hidden';
 
   if (isHidden) {
     return false;
@@ -34,7 +33,7 @@ const visible = (element: HTMLElement) => {
 };
 
 const getElementTabIndex = (element: HTMLElement) => {
-  const tabIndex = element.getAttribute("tabindex");
+  const tabIndex = element.getAttribute('tabindex');
   if (tabIndex === null) {
     return NaN;
   }
@@ -48,11 +47,11 @@ const hasExplicitTabIndex = (element: HTMLElement) => {
 
 const isTabbableElement = (element: HTMLElement) => {
   const nodeName = element.nodeName.toLowerCase();
-  return TABBABLE_NODES.test(nodeName) && !element.hasAttribute("disabled");
+  return TABBABLE_NODES.test(nodeName) && !element.hasAttribute('disabled');
 };
 
 const isAnchorFocusable = (element: HTMLElement) => {
-  return element.hasAttribute("href") || hasExplicitTabIndex(element);
+  return element.hasAttribute('href') || hasExplicitTabIndex(element);
 };
 
 const isFocusableElement = (element: HTMLElement) => {
