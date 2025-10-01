7091d96ada92ab9675169b7fd83d770ebf6e9489
이상희 <sang6299@naver.com>
2025-10-01 20:51:41 +0900

    refactor: 목록 정보를 리스트 요소로 개선

--- src/components/TravelSection.tsx
+++ src/components/TravelSection.tsx
@@ -57,30 +57,34 @@ const TravelSection = () => {
 
   return (
     <div className={styles.travelSection}>
-      <div className={styles.carousel} aria-live="polite">
+      <ul className={styles.carousel} aria-live="polite">
         <div className="visually-hidden">{`세계의 여행상품 중 ${currentIndex + 1}번째 상품`}</div>
+
         {travelOptions.map((option, index) => (
-          <a
-            key={index}
-            href={option.link}
-            target="_blank"
-            rel="noopener noreferrer"
-            className={`${styles.card} ${index === currentIndex ? styles.cardActive : ''}`}
-            aria-label={`${option.departure} 출발 ${option.destination} 도착, ${
-              option.type
-            }, 가격 ${option.price.toLocaleString()}원, 선택하면 예약페이지로 이동합니다`}
-          >
-            <img src={option.image} className={styles.cardImage} alt="" />
-            <div className={styles.cardContent} aria-hidden="true">
-              <p className={`${styles.cardTitle} heading-3-text`}>
-                {option.departure} - {option.destination}
-              </p>
-              <p className={`${styles.cardType} body-text`}>{option.type}</p>
-              <p className={`${styles.cardPrice} body-text`}>KRW {option.price.toLocaleString()}</p>
-            </div>
-          </a>
+          <li key={index}>
+            <a
+              href={option.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={`${styles.card} ${index === currentIndex ? styles.cardActive : ''}`}
+              aria-label={`${option.departure} 출발 ${option.destination} 도착, ${
+                option.type
+              }, 가격 ${option.price.toLocaleString()}원, 선택하면 예약페이지로 이동합니다`}
+            >
+              <img src={option.image} className={styles.cardImage} alt="" />
+              <div className={styles.cardContent} aria-hidden="true">
+                <p className={`${styles.cardTitle} heading-3-text`}>
+                  {option.departure} - {option.destination}
+                </p>
+                <p className={`${styles.cardType} body-text`}>{option.type}</p>
+                <p className={`${styles.cardPrice} body-text`}>
+                  KRW {option.price.toLocaleString()}
+                </p>
+              </div>
+            </a>
+          </li>
         ))}
-      </div>
+      </ul>
       <button
         className={`${styles.navButton} ${styles.navButtonPrev}`}
         onClick={prevTravel}
