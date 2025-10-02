a6f3425631884754ae6d450e0db36e567f47f2de
Ko Daeun <drechsu@gmail.com>
2025-10-02 16:03:09 +0900

    refactor : announcement 를 한곳에서 부르도록 수정

--- src/components/FlightBooking.tsx
+++ src/components/FlightBooking.tsx
@@ -16,12 +16,12 @@ const FlightBooking = () => {
       setAdultCount(newCount);
 
       if (newCount === MAX_PASSENGERS) {
-        setAnnouncement('성인 승객 증가, 최대 승객 수에 도달했습니다.');
+        setAnnouncement(`성인 승객 ${newCount}명 선택되었습니다. 최대 승객 수에 도달했습니다.`);
       } else {
-        setAnnouncement('성인 승객 증가');
+        setAnnouncement(`성인 승객 ${newCount}명 선택되었습니다.`);
       }
     } else {
-      setAnnouncement('최대 승객수 입니다');
+      setAnnouncement('최대 승객 수입니다.');
     }
   };
 
@@ -29,16 +29,16 @@ const FlightBooking = () => {
     if (adultCount > MIN_PASSENGERS) {
       const newCount = adultCount - 1;
       setAdultCount(newCount);
-      setAnnouncement('성인 승객 감소');
+      setAnnouncement(`성인 승객 ${newCount}명 선택되었습니다.`);
     } else {
-      setAnnouncement('최소 승객수 입니다');
+      setAnnouncement('최소 승객 수입니다.');
     }
   };
 
   return (
     <div className={styles['flight-booking']}>
       <VisuallyHidden>
-        <div aria-live="assertive">{announcement}</div>
+        <div aria-live="polite">{announcement}</div>
       </VisuallyHidden>
       <h2 className="heading-2-text">항공권 예매</h2>
       <div className={styles['passenger-count']}>
@@ -47,7 +47,7 @@ const FlightBooking = () => {
           <button className="button-text" onClick={decrementCount} aria-label="성인 승객 감소">
             -
           </button>
-          <span aria-live="polite">{adultCount}</span>
+          <span>{adultCount}</span>
           <button className="button-text" onClick={incrementCount} aria-label="성인 승객 증가">
             +
           </button>
