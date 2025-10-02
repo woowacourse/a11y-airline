7562ef6f1824b3c7308bf7ebeac184b76424988a
dev-dino22 <wlsskdud02@naver.com>
2025-10-02 12:23:07 +0900

    chore: TravelSection button 이미지 alt 속성 빈 문자열 처리

--- src/components/TravelSection.tsx
+++ src/components/TravelSection.tsx
@@ -96,14 +96,14 @@ const TravelSection = () => {
         onClick={prevTravel}
         aria-label="이전 여행 상품"
       >
-        <img src={chevronLeft} className={styles.navButtonIcon} />
+        <img src={chevronLeft} className={styles.navButtonIcon} alt="" />
       </button>
       <button
         className={`${styles.navButton} ${styles.navButtonNext}`}
         onClick={nextTravel}
         aria-label="다음 여행 상품"
       >
-        <img src={chevronRight} className={styles.navButtonIcon} />
+        <img src={chevronRight} className={styles.navButtonIcon} alt="" />
       </button>
     </div>
   );
